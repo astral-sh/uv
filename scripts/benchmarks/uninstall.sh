f03c605bdef96d3a653f9a7fd017e045ae563997
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-10-09 16:59:15 -0400

    Add a script to benchmark uninstalls (#78)

---
+++ scripts/benchmarks/uninstall.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+###
+# Benchmark the uninstall command against `pip`.
+#
+# Example usage:
+#
+#   ./scripts/benchmarks/uninstall.sh numpy
+###
+
+set -euxo pipefail
+
+TARGET=${1}
+
+hyperfine --runs 20 --warmup 3 --prepare "rm -rf .venv && virtualenv .venv && source activate .venv/bin/activate && pip install ${TARGET}" \
+    "./target/release/puffin-cli uninstall ${TARGET}" \
+    "pip uninstall -y ${TARGET}"
