0d174eb2e18f99fe6e9df89b20cbafc464223031
konstin <konstin@mailbox.org>
2023-10-12 12:31:11 +0200

    Fix tempdir rename

--- crates/puffin-installer/src/cache.rs
+++ crates/puffin-installer/src/cache.rs
@@ -4,29 +4,34 @@ static WHEEL_CACHE: &str = "wheels-v0";
 
 #[derive(Debug)]
 pub(crate) struct WheelCache {
-    path: PathBuf,
+    root: PathBuf,
 }
 
 impl WheelCache {
     /// Create a handle to the wheel cache.
     pub(crate) fn new(root: &Path) -> Self {
         Self {
-            path: root.join(WHEEL_CACHE),
+            root: root.join(WHEEL_CACHE),
         }
     }
 
     /// Return the path at which a given wheel would be stored.
     pub(crate) fn entry(&self, id: &str) -> PathBuf {
-        self.path.join(id)
+        self.root.join(id)
     }
 
     /// Initialize the wheel cache.
     pub(crate) async fn init(&self) -> std::io::Result<()> {
-        tokio::fs::create_dir_all(&self.path).await
+        tokio::fs::create_dir_all(&self.root).await
     }
 
     /// Returns a handle to the wheel cache directory.
     pub(crate) async fn read_dir(&self) -> std::io::Result<tokio::fs::ReadDir> {
-        tokio::fs::read_dir(&self.path).await
+        tokio::fs::read_dir(&self.root).await
+    }
+
+    /// Returns the cache root.
+    pub(crate) fn root(&self) -> &Path {
+        &self.root
     }
 }
