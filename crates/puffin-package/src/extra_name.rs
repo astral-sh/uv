3d5f8249ef068972e48a5c4df132d7b332f11c0e
Zanie Blue <contact@zanie.dev>
2023-11-01 10:40:43 -0500

    Add validation of extra names (#257)
    Extends #254 
    
    Adds validation of extra names provided by users in `pip-compile` e.g. 
    
    ```
    error: invalid value 'foo!' for '--extra <EXTRA>': Extra names must start and end with a
    letter or digit and may only contain -, _, ., and alphanumeric characters
    ```
    
    We'll want to add something similar to `PackageName`. I'd be curious to
    improve the AP, making the unvalidated nature of `::normalize` clear?
    Perhaps worth pursuing later though as I don't have a better idea.

--- crates/puffin-package/src/extra_name.rs
+++ crates/puffin-package/src/extra_name.rs
@@ -1,6 +1,8 @@
 use std::fmt;
 use std::fmt::{Display, Formatter};
+use std::str::FromStr;
 
+use anyhow::{anyhow, Error, Result};
 use once_cell::sync::Lazy;
 use regex::Regex;
 
@@ -14,18 +16,35 @@ impl Display for ExtraName {
 }
 
 static NAME_NORMALIZE: Lazy<Regex> = Lazy::new(|| Regex::new(r"[-_.]+").unwrap());
+static NAME_VALIDATE: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"(?i)^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$").unwrap());
 
+/// An extra dependency group name.
+///
+/// See:
+/// - <https://peps.python.org/pep-0685/#specification/>
+/// - <https://packaging.python.org/en/latest/specifications/name-normalization/>
 impl ExtraName {
-    /// Collapses any run of the characters `-`, `_` and `.` down to a single `-`.
-    /// Ex) "---", ".", and "__" all get converted to just "."
+    /// Create a normalized extra name without validation.
     ///
-    /// See: <https://peps.python.org/pep-0685/#specification/>
-    ///      <https://packaging.python.org/en/latest/specifications/name-normalization/>
+    /// Converts the name to lowercase and collapses any run of the characters `-`, `_` and `.`
+    /// down to a single `-`, e.g., `---`, `.`, and `__` all get converted to just `-`.
     pub fn normalize(name: impl AsRef<str>) -> Self {
         // TODO(charlie): Avoid allocating in the common case (when no normalization is required).
         let mut normalized = NAME_NORMALIZE.replace_all(name.as_ref(), "-").to_string();
         normalized.make_ascii_lowercase();
         Self(normalized)
+    }
+
+    /// Create a validated, normalized extra name.
+    pub fn validate(name: impl AsRef<str>) -> Result<Self> {
+        if NAME_VALIDATE.is_match(name.as_ref()) {
+            Ok(Self::normalize(name))
+        } else {
+            Err(anyhow!(
+                "Extra names must start and end with a letter or digit and may only contain -, _, ., and alphanumeric characters"
+            ))
+        }
     }
 }
 
@@ -35,9 +54,11 @@ impl AsRef<str> for ExtraName {
     }
 }
 
-impl From<&str> for ExtraName {
-    fn from(name: &str) -> Self {
-        Self::normalize(name)
+impl FromStr for ExtraName {
+    type Err = Error;
+
+    fn from_str(name: &str) -> Result<Self> {
+        Self::validate(name)
     }
 }
 
@@ -75,5 +96,28 @@ mod tests {
             ExtraName::normalize("FrIeNdLy-._.-bArD").as_ref(),
             "friendly-bard"
         );
+    }
+
+    #[test]
+    fn validate() {
+        // Unchanged
+        assert_eq!(
+            ExtraName::validate("friendly-bard").unwrap().as_ref(),
+            "friendly-bard"
+        );
+        assert_eq!(ExtraName::validate("1okay").unwrap().as_ref(), "1okay");
+        assert_eq!(ExtraName::validate("okay2").unwrap().as_ref(), "okay2");
+        // Normalizes
+        assert_eq!(
+            ExtraName::validate("Friendly-Bard").unwrap().as_ref(),
+            "friendly-bard"
+        );
+        // Failures...
+        assert!(ExtraName::validate(" starts-with-space").is_err());
+        assert!(ExtraName::validate("-starts-with-dash").is_err());
+        assert!(ExtraName::validate("ends-with-dash-").is_err());
+        assert!(ExtraName::validate("ends-with-space ").is_err());
+        assert!(ExtraName::validate("includes!invalid-char").is_err());
+        assert!(ExtraName::validate("space in middle").is_err());
     }
 }
