63f7f651900cfa2d5b9898285d2a017bf393663d
Andrew Gallant <andrew@astral.sh>
2023-11-10 14:48:59 -0500

    change global allocator to jemalloc (and mimalloc on Windows) (#399)
    This copies the allocator configuration used in the Ruff project. In
    particular, this gives us an instant 10% win when resolving the top 1K
    PyPI packages:
    
        $ hyperfine \
    "./target/profiling/puffin-dev-main resolve-many --cache-dir
    cache-docker-no-build --no-build pypi_top_8k_flat.txt --limit 1000 2>
    /dev/null" \
    "./target/profiling/puffin-dev resolve-many --cache-dir
    cache-docker-no-build --no-build pypi_top_8k_flat.txt --limit 1000 2>
    /dev/null"
    Benchmark 1: ./target/profiling/puffin-dev-main resolve-many --cache-dir
    cache-docker-no-build --no-build pypi_top_8k_flat.txt --limit 1000 2>
    /dev/null
    Time (mean ± σ): 974.2 ms ± 26.4 ms [User: 17503.3 ms, System: 2205.3
    ms]
          Range (min … max):   943.5 ms … 1015.9 ms    10 runs
    
    Benchmark 2: ./target/profiling/puffin-dev resolve-many --cache-dir
    cache-docker-no-build --no-build pypi_top_8k_flat.txt --limit 1000 2>
    /dev/null
    Time (mean ± σ): 883.1 ms ± 23.3 ms [User: 14626.1 ms, System: 2542.2
    ms]
          Range (min … max):   849.5 ms … 916.9 ms    10 runs
    
        Summary
    './target/profiling/puffin-dev resolve-many --cache-dir
    cache-docker-no-build --no-build pypi_top_8k_flat.txt --limit 1000 2>
    /dev/null' ran
    1.10 ± 0.04 times faster than './target/profiling/puffin-dev-main
    resolve-many --cache-dir cache-docker-no-build --no-build
    pypi_top_8k_flat.txt --limit 1000 2> /dev/null'
    
    I was moved to do this because I noticed `malloc`/`free` taking up a
    fairly sizeable percentage of time during light profiling.
    
    As is becoming a pattern, it will be easier to review this
    commit-by-commit.
    
    Ref #396 (wouldn't call this issue fixed)
    
    -----
    
    I did also try adding a `smallvec` optimization to the
    `Version::release` field, but it didn't bare any fruit. I still think
    there is more to explore since the results I observed don't quite line
    up with what I expect. (So probably either my mental model is off or my
    measurement process is flawed.) You can see that attempt with a little
    more explanation here:
    https://github.com/astral-sh/puffin/commit/f9528b4ecd1b0c260df7e8ad57b9ddc4da09d273
    
    In the course of adding the `smallvec` optimization, I also shrunk the
    `Version` fields from a `usize` to a `u32`. They should at least be a
    fixed size integer since version numbers aren't used to index memory,
    and I shrunk it to `u32` since it seems reasonable to assume that all
    version numbers will be smaller than `2^32`.

--- crates/puffin-cli/src/main.rs
+++ crates/puffin-cli/src/main.rs
@@ -18,6 +18,22 @@ use crate::index_urls::IndexUrls;
 use crate::python_version::PythonVersion;
 use crate::requirements::RequirementsSource;
 
+#[cfg(target_os = "windows")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+#[cfg(all(
+    not(target_os = "windows"),
+    not(target_os = "openbsd"),
+    any(
+        target_arch = "x86_64",
+        target_arch = "aarch64",
+        target_arch = "powerpc64"
+    )
+))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 mod commands;
 mod index_urls;
 mod logging;
