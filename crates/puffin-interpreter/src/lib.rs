fc20d015934ceca2a3eaf0a45e161dc190bd6f5d
Charlie Marsh <charlie.r.marsh@gmail.com>
2023-12-03 23:53:26 -0500

    Ignore empty `VIRTUAL_ENV` variables (#536)
    I'm not sure how my interpreter gets into this state, but it's certainly
    wrong to respect these.

--- crates/puffin-interpreter/src/lib.rs
+++ crates/puffin-interpreter/src/lib.rs
@@ -15,17 +15,17 @@ mod virtual_env;
 pub enum Error {
     #[error("Expected {0} to be a virtual environment, but pyvenv.cfg is missing")]
     MissingPyVenvCfg(PathBuf),
-    #[error("Your virtualenv at {0} is broken. It contains a pyvenv.cfg but no python at {1}")]
+    #[error("Detected a broken virtualenv at: {0}. It contains a pyvenv.cfg but no Python binary at: {1}")]
     BrokenVenv(PathBuf, PathBuf),
     #[error("Both VIRTUAL_ENV and CONDA_PREFIX are set. Please unset one of them.")]
     Conflict,
-    #[error("Couldn't find a virtualenv or conda environment (Looked for VIRTUAL_ENV, CONDA_PREFIX and .venv)")]
+    #[error("Failed to locate a virtualenv or Conda environment (checked: VIRTUAL_ENV, CONDA_PREFIX, and .venv)")]
     NotFound,
     #[error(transparent)]
     Io(#[from] io::Error),
     #[error("Invalid modified date on {0}")]
     SystemTime(PathBuf, #[source] SystemTimeError),
-    #[error("Failed to query python interpreter at {interpreter}")]
+    #[error("Failed to query python interpreter at: {interpreter}")]
     PythonSubcommandLaunch {
         interpreter: PathBuf,
         #[source]
