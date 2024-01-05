f812a4ad9eed6ee3b3b368bb13eefb37c5705c8d
Zanie <contact@zanie.dev>
2024-01-05 10:40:55 -0600

    Lint

--- crates/puffin-cli/tests/pip_install_scenarios.rs
+++ crates/puffin-cli/tests/pip_install_scenarios.rs
@@ -1,7 +1,7 @@
 //! DO NOT EDIT
 //!
 //! Generated with ./scripts/scenarios/update.py
-//! Scenarios from https://github.com/zanieb/packse/tree/c448e24b336f3699407c50ed7a07b9b7442cb1a5/scenarios
+//! Scenarios from <https://github.com/zanieb/packse/tree/c448e24b336f3699407c50ed7a07b9b7442cb1a5/scenarios>
 //!
 #![cfg(all(feature = "python", feature = "pypi"))]
 
