f64d93fc49134de7e7efaaf63bdb24eff0e1fd2a
konstin <konstin@mailbox.org>
2024-02-08 16:29:01 -0500

    python

--- crates/puffin-interpreter/src/python_query.rs
+++ crates/puffin-interpreter/src/python_query.rs
@@ -297,7 +297,7 @@ mod tests {
         .ok_or(Error::NoSuchPython(request.to_string()));
         assert_display_snapshot!(
             format_err(result),
-            @"No Python python3.1000 In `PATH`. Is Python 3.1000 installed?"
+            @"No Python python3.1000 In `PATH`. Is Python python3.1000 installed?"
         );
     }
 
