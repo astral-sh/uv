ed9b0bdecb88cad3e2b7df2d235bbbbd96a070c4
Zanie <contact@zanie.dev>
2024-02-14 16:44:46 -0600

    Fix the trampoline
    These are easily missed due to the ignore

--- crates/uv-trampoline/src/bin/uv-trampoline-console.rs
+++ crates/uv-trampoline/src/bin/uv-trampoline-console.rs
@@ -5,5 +5,5 @@
 // build.rs passes a custom linker flag to make this the entrypoint to the executable
 #[no_mangle]
 pub extern "C" fn entry() -> ! {
-    puffin_trampoline::bounce::bounce(false)
+    uv_trampoline::bounce::bounce(false)
 }
--- crates/uv-trampoline/src/bin/uv-trampoline-gui.rs
+++ crates/uv-trampoline/src/bin/uv-trampoline-gui.rs
@@ -5,5 +5,5 @@
 // build.rs passes a custom linker flag to make this the entrypoint to the executable
 #[no_mangle]
 pub extern "C" fn entry() -> ! {
-    puffin_trampoline::bounce::bounce(true)
+    uv_trampoline::bounce::bounce(true)
 }
