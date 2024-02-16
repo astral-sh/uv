418d9ca9afeda7a6db4fe73a4bf6ace0cf8d8449
Zanie <contact@zanie.dev>
2024-02-15 20:37:16 -0600

    Add snapshot for `SimpleMetadata::from_files`

--- crates/uv-client/src/registry_client.rs
+++ crates/uv-client/src/registry_client.rs
@@ -705,10 +705,11 @@ pub enum Connectivity {
 mod tests {
     use std::str::FromStr;
 
-    use pypi_types::SimpleJson;
+    use pypi_types::{JoinRelativeError, SimpleJson};
+    use url::Url;
     use uv_normalize::PackageName;
 
-    use crate::{SimpleMetadata, SimpleMetadatum};
+    use crate::{html::SimpleHtml, SimpleMetadata, SimpleMetadatum};
 
     #[test]
     fn ignore_failing_files() {
@@ -757,5 +758,49 @@ mod tests {
             .map(|SimpleMetadatum { version, .. }| version.to_string())
             .collect();
         assert_eq!(versions, ["1.7.8".to_string()]);
+    }
+
+    /// Test for AWS Code Artifact registry
+    /// Regression coverage of https://github.com/astral-sh/uv/issues/1388
+    #[test]
+    fn relative_urls_code_artifact() -> Result<(), JoinRelativeError> {
+        let text = r#"
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Links for flask</title>
+            </head>
+            <body>
+                <h1>Links for flask</h1>
+                <a href="0.1/Flask-0.1.tar.gz#sha256=9da884457e910bf0847d396cb4b778ad9f3c3d17db1c5997cb861937bd284237"  data-gpg-sig="false" >Flask-0.1.tar.gz</a>
+                <br/>
+                <a href="0.10.1/Flask-0.10.1.tar.gz#sha256=4c83829ff83d408b5e1d4995472265411d2c414112298f2eb4b359d9e4563373"  data-gpg-sig="false" >Flask-0.10.1.tar.gz</a>
+                <br/>
+                <a href="3.0.1/flask-3.0.1.tar.gz#sha256=6489f51bb3666def6f314e15f19d50a1869a19ae0e8c9a3641ffe66c77d42403" data-requires-python="&gt;=3.8" data-gpg-sig="false" >flask-3.0.1.tar.gz</a>
+                <br/>
+            </body>
+            </html>
+        "#;
+
+        // Note the lack of a trailing `/` here is important for coverage of url-join behavior
+        let base = Url::parse("https://account.d.codeartifact.us-west-2.amazonaws.com/pypi/shared-packages-pypi/simple/flask")
+            .unwrap();
+        let SimpleHtml { base, files } = SimpleHtml::parse(text, &base).unwrap();
+
+        // Test parsing of the file urls
+        let urls = files
+            .iter()
+            .map(|file| pypi_types::base_url_join_relative(base.as_url().as_str(), &file.url))
+            .collect::<Result<Vec<_>, JoinRelativeError>>()?;
+        let urls = urls.iter().map(|url| url.as_str()).collect::<Vec<_>>();
+        insta::assert_debug_snapshot!(urls, @r###"
+        [
+            "https://account.d.codeartifact.us-west-2.amazonaws.com/pypi/shared-packages-pypi/simple/0.1/Flask-0.1.tar.gz#sha256=9da884457e910bf0847d396cb4b778ad9f3c3d17db1c5997cb861937bd284237",
+            "https://account.d.codeartifact.us-west-2.amazonaws.com/pypi/shared-packages-pypi/simple/0.10.1/Flask-0.10.1.tar.gz#sha256=4c83829ff83d408b5e1d4995472265411d2c414112298f2eb4b359d9e4563373",
+            "https://account.d.codeartifact.us-west-2.amazonaws.com/pypi/shared-packages-pypi/simple/3.0.1/flask-3.0.1.tar.gz#sha256=6489f51bb3666def6f314e15f19d50a1869a19ae0e8c9a3641ffe66c77d42403",
+        ]
+        "###);
+
+        Ok(())
     }
 }
