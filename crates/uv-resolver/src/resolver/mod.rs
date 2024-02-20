743abaec21a151046401636afe0166057b269343
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-02-19 21:40:45 -0500

    Support recursive extras for URL dependencies

--- crates/uv-resolver/src/resolver/mod.rs
+++ crates/uv-resolver/src/resolver/mod.rs
@@ -13,11 +13,25 @@ use pubgrub::range::Range;
 use pubgrub::solver::{Incompatibility, State};
 use pubgrub::type_aliases::DependencyConstraints;
 use rustc_hash::{FxHashMap, FxHashSet};
-
 use tokio::select;
 use tokio_stream::wrappers::ReceiverStream;
 use tracing::{debug, info_span, instrument, trace, warn, Instrument};
 use url::Url;
+
+use distribution_filename::WheelFilename;
+use distribution_types::{
+    BuiltDist, Dist, DistributionMetadata, IncompatibleWheel, LocalEditable, Name, RemoteSource,
+    SourceDist, VersionOrUrl,
+};
+use pep440_rs::{Version, VersionSpecifiers, MIN_VERSION};
+use pep508_rs::{MarkerEnvironment, Requirement};
+use platform_tags::{IncompatibleTag, Tags};
+use pypi_types::{Metadata21, Yanked};
+use uv_client::{FlatIndex, RegistryClient};
+use uv_distribution::DistributionDatabase;
+use uv_interpreter::Interpreter;
+use uv_normalize::PackageName;
+use uv_traits::BuildContext;
 
 use crate::candidate_selector::{CandidateDist, CandidateSelector};
 use crate::error::ResolveError;
@@ -39,20 +53,6 @@ use crate::resolver::reporter::Facade;
 pub use crate::resolver::reporter::{BuildId, Reporter};
 use crate::yanks::AllowedYanks;
 use crate::{DependencyMode, Options};
-use distribution_filename::WheelFilename;
-use distribution_types::{
-    BuiltDist, Dist, DistributionMetadata, IncompatibleWheel, LocalEditable, Name, RemoteSource,
-    SourceDist, VersionOrUrl,
-};
-use pep440_rs::{Version, VersionSpecifiers, MIN_VERSION};
-use pep508_rs::{MarkerEnvironment, Requirement};
-use platform_tags::{IncompatibleTag, Tags};
-use pypi_types::{Metadata21, Yanked};
-use uv_client::{FlatIndex, RegistryClient};
-use uv_distribution::DistributionDatabase;
-use uv_interpreter::Interpreter;
-use uv_normalize::PackageName;
-use uv_traits::BuildContext;
 
 mod allowed_urls;
 mod index;
@@ -784,6 +784,7 @@ impl<'a, Provider: ResolverProvider> Resolver<'a, Provider> {
                     &self.overrides,
                     None,
                     None,
+                    None,
                     self.markers,
                 );
                 if let Err(
@@ -841,6 +842,7 @@ impl<'a, Provider: ResolverProvider> Resolver<'a, Provider> {
                         &self.constraints,
                         &self.overrides,
                         Some(package_name),
+                        url.as_ref(),
                         extra.as_ref(),
                         self.markers,
                     )?;
@@ -896,6 +898,7 @@ impl<'a, Provider: ResolverProvider> Resolver<'a, Provider> {
                     &self.constraints,
                     &self.overrides,
                     Some(package_name),
+                    url.as_ref(),
                     extra.as_ref(),
                     self.markers,
                 )?;
@@ -911,7 +914,7 @@ impl<'a, Provider: ResolverProvider> Resolver<'a, Provider> {
                 // If a package has an extra, insert a constraint on the base package.
                 if extra.is_some() {
                     constraints.insert(
-                        PubGrubPackage::Package(package_name.clone(), None, None),
+                        PubGrubPackage::Package(package_name.clone(), None, url.clone()),
                         Range::singleton(version.clone()),
                     );
                 }
