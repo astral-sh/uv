7ac96ecd6084dc513d327b95ec77797bd99c0493
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-02-20 23:12:55 -0500

    Avoid enforcing URL correctness for installed distributions

--- crates/pypi-types/src/direct_url.rs
+++ crates/pypi-types/src/direct_url.rs
@@ -14,13 +14,13 @@ pub enum DirectUrl {
     /// ```json
     /// {"url": "file:///home/user/project", "dir_info": {}}
     /// ```
-    LocalDirectory { url: Url, dir_info: DirInfo },
+    LocalDirectory { url: String, dir_info: DirInfo },
     /// The direct URL is a path to an archive. For example:
     /// ```json
     /// {"archive_info": {"hash": "sha256=75909db2664838d015e3d9139004ee16711748a52c8f336b52882266540215d8", "hashes": {"sha256": "75909db2664838d015e3d9139004ee16711748a52c8f336b52882266540215d8"}}, "url": "https://files.pythonhosted.org/packages/b8/8b/31273bf66016be6ad22bb7345c37ff350276cfd46e389a0c2ac5da9d9073/wheel-0.41.2-py3-none-any.whl"}
     /// ```
     ArchiveUrl {
-        url: Url,
+        url: String,
         archive_info: ArchiveInfo,
         #[serde(skip_serializing_if = "Option::is_none")]
         subdirectory: Option<PathBuf>,
@@ -30,7 +30,7 @@ pub enum DirectUrl {
     /// {"url": "https://github.com/pallets/flask.git", "vcs_info": {"commit_id": "8d9519df093864ff90ca446d4af2dc8facd3c542", "vcs": "git"}}
     /// ```
     VcsUrl {
-        url: Url,
+        url: String,
         vcs_info: VcsInfo,
         #[serde(skip_serializing_if = "Option::is_none")]
         subdirectory: Option<PathBuf>,
@@ -83,36 +83,38 @@ impl std::fmt::Display for VcsKind {
     }
 }
 
-impl From<DirectUrl> for Url {
-    fn from(value: DirectUrl) -> Self {
+impl TryFrom<&DirectUrl> for Url {
+    type Error = url::ParseError;
+
+    fn try_from(value: &DirectUrl) -> Result<Self, Self::Error> {
         match value {
-            DirectUrl::LocalDirectory { url, .. } => url,
+            DirectUrl::LocalDirectory { url, .. } => Url::parse(url),
             DirectUrl::ArchiveUrl {
-                mut url,
+                url,
                 subdirectory,
                 archive_info: _,
             } => {
+                let mut url = Url::parse(url)?;
                 if let Some(subdirectory) = subdirectory {
                     url.set_fragment(Some(&format!("subdirectory={}", subdirectory.display())));
                 }
-                url
+                Ok(url)
             }
             DirectUrl::VcsUrl {
                 url,
                 vcs_info,
                 subdirectory,
             } => {
-                let mut url =
-                    Url::parse(&format!("{}+{}", vcs_info.vcs, url)).expect("VCS URL is invalid");
-                if let Some(commit_id) = vcs_info.commit_id {
+                let mut url = Url::parse(&format!("{}+{}", vcs_info.vcs, url))?;
+                if let Some(commit_id) = &vcs_info.commit_id {
                     url.set_path(&format!("{}@{commit_id}", url.path()));
-                } else if let Some(requested_revision) = vcs_info.requested_revision {
+                } else if let Some(requested_revision) = &vcs_info.requested_revision {
                     url.set_path(&format!("{}@{requested_revision}", url.path()));
                 }
                 if let Some(subdirectory) = subdirectory {
                     url.set_fragment(Some(&format!("subdirectory={}", subdirectory.display())));
                 }
-                url
+                Ok(url)
             }
         }
     }
