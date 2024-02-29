b983ff4fa70fdfc8974df4d41c460a34d203f800
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-02-29 10:06:29 -0500

    Prioritize `PATH` over `py --list-paths` in Windows selection (#2057)
    `uv --system` is failing in GitHub Actions, because `py --list-paths`
    returns all the pre-cached Pythons:
    
    ```
    -V:3.12 *        C:\hostedtoolcache\windows\Python\3.12.2\x64\python.exe
    -V:3.12-32       C:\hostedtoolcache\windows\Python\3.12.2\x86\python.exe
    -V:3.11          C:\hostedtoolcache\windows\Python\3.11.8\x64\python.exe
    -V:3.11-32       C:\hostedtoolcache\windows\Python\3.11.8\x86\python.exe
    -V:3.10          C:\hostedtoolcache\windows\Python\3.10.11\x64\python.exe
    -V:3.10-32       C:\hostedtoolcache\windows\Python\3.10.11\x86\python.exe
    -V:3.9           C:\hostedtoolcache\windows\Python\3.9.13\x64\python.exe
    -V:3.9-32        C:\hostedtoolcache\windows\Python\3.9.13\x86\python.exe
    -V:3.8           C:\hostedtoolcache\windows\Python\3.8.10\x64\python.exe
    -V:3.8-32        C:\hostedtoolcache\windows\Python\3.8.10\x86\python.exe
    -V:3.7           C:\hostedtoolcache\windows\Python\3.7.9\x64\python.exe
    -V:3.7-32        C:\hostedtoolcache\windows\Python\3.7.9\x86\python.exe
    ```
    
    So, our default selector returns the first entry here. But none of these
    are actually in `PATH` except the one that the user installed via
    `actions/setup-python@v5` -- that's the point of the action, that it
    puts the correct versions in `PATH`.
    
    It seems to me like we should prioritize `PATH` over `py --list-paths`.
    Is there a good reason not to do this?
    
    Closes: https://github.com/astral-sh/uv/issues/2056

--- crates/uv-interpreter/src/python_query.rs
+++ crates/uv-interpreter/src/python_query.rs
@@ -96,7 +96,6 @@ pub(crate) fn try_find_default_python(
 
 /// Finds a python version matching `selector`.
 /// It searches for an existing installation in the following order:
-/// * (windows): Discover installations using `py --list-paths` (PEP514). Continue if `py` is not installed.
 /// * Search for the python binary in `PATH` (or `UV_TEST_PYTHON_PATH` if set). Visits each path and for each path resolves the
 ///   files in the following order:
 ///   * Major.Minor.Patch: `pythonx.y.z`, `pythonx.y`, `python.x`, `python`
@@ -104,6 +103,7 @@ pub(crate) fn try_find_default_python(
 ///   * Major: `pythonx`, `python`
 ///   * Default: `python3`, `python`
 ///   * (windows): For each of the above, test for the existence of `python.bat` shim (pyenv-windows) last.
+/// * (windows): Discover installations using `py --list-paths` (PEP514). Continue if `py` is not installed.
 ///
 /// (Windows): Filter out the windows store shim (Enabled in Settings/Apps/Advanced app settings/App execution aliases).
 fn find_python(
@@ -114,23 +114,7 @@ fn find_python(
     #[allow(non_snake_case)]
     let UV_TEST_PYTHON_PATH = env::var_os("UV_TEST_PYTHON_PATH");
 
-    if cfg!(windows) && UV_TEST_PYTHON_PATH.is_none() {
-        // Use `py` to find the python installation on the system.
-        match windows::py_list_paths(selector, platform, cache) {
-            Ok(Some(interpreter)) => return Ok(Some(interpreter)),
-            Ok(None) => {
-                // No matching Python version found, continue searching PATH
-            }
-            Err(Error::PyList(error)) => {
-                if error.kind() == std::io::ErrorKind::NotFound {
-                    debug!("`py` is not installed. Falling back to searching Python on the path");
-                    // Continue searching for python installations on the path.
-                }
-            }
-            Err(error) => return Err(error),
-        }
-    }
-
+    let override_path = UV_TEST_PYTHON_PATH.is_some();
     let possible_names = selector.possible_names();
 
     #[allow(non_snake_case)]
@@ -194,6 +178,20 @@ fn find_python(
                     }
                 }
             }
+        }
+    }
+
+    if cfg!(windows) && !override_path {
+        // Use `py` to find the python installation on the system.
+        match windows::py_list_paths(selector, platform, cache) {
+            Ok(Some(interpreter)) => return Ok(Some(interpreter)),
+            Ok(None) => {}
+            Err(Error::PyList(error)) => {
+                if error.kind() == std::io::ErrorKind::NotFound {
+                    debug!("`py` is not installed");
+                }
+            }
+            Err(error) => return Err(error),
         }
     }
 
