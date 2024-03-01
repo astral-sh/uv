3131b14a9752e8dfebe3eea147774c4e3b1257ca
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-03-01 10:22:16 -0500

    Consider editable dependencies to be 'direct'

--- crates/uv-resolver/src/prerelease_mode.rs
+++ crates/uv-resolver/src/prerelease_mode.rs
@@ -1,7 +1,9 @@
 use rustc_hash::FxHashSet;
 
-use pep508_rs::{Requirement, VersionOrUrl};
+use pep508_rs::VersionOrUrl;
 use uv_normalize::PackageName;
+
+use crate::Manifest;
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 #[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
@@ -48,21 +50,23 @@ pub(crate) enum PreReleaseStrategy {
 }
 
 impl PreReleaseStrategy {
-    pub(crate) fn from_mode(
-        mode: PreReleaseMode,
-        requirements: &[Requirement],
-        constraints: &[Requirement],
-        overrides: &[Requirement],
-    ) -> Self {
+    pub(crate) fn from_mode(mode: PreReleaseMode, manifest: &Manifest) -> Self {
         match mode {
             PreReleaseMode::Disallow => Self::Disallow,
             PreReleaseMode::Allow => Self::Allow,
             PreReleaseMode::IfNecessary => Self::IfNecessary,
             PreReleaseMode::Explicit => Self::Explicit(
-                requirements
+                manifest
+                    .requirements
                     .iter()
-                    .chain(constraints.iter())
-                    .chain(overrides.iter())
+                    .chain(manifest.constraints.iter())
+                    .chain(manifest.overrides.iter())
+                    .chain(
+                        manifest
+                            .editables
+                            .iter()
+                            .flat_map(|(_editable, metadata)| metadata.requires_dist.iter()),
+                    )
                     .filter(|requirement| {
                         let Some(version_or_url) = &requirement.version_or_url else {
                             return false;
@@ -81,10 +85,17 @@ impl PreReleaseStrategy {
                     .collect(),
             ),
             PreReleaseMode::IfNecessaryOrExplicit => Self::IfNecessaryOrExplicit(
-                requirements
+                manifest
+                    .requirements
                     .iter()
-                    .chain(constraints.iter())
-                    .chain(overrides.iter())
+                    .chain(manifest.constraints.iter())
+                    .chain(manifest.overrides.iter())
+                    .chain(
+                        manifest
+                            .editables
+                            .iter()
+                            .flat_map(|(_editable, metadata)| metadata.requires_dist.iter()),
+                    )
                     .filter(|requirement| {
                         let Some(version_or_url) = &requirement.version_or_url else {
                             return false;
