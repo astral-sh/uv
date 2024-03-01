488c8affbfed7c5fac7087fbc2f7022fb4be593c
chan <chan.kang@enablemedicine.com>
2024-03-01 09:04:13 -0500

    add a test; need to fix run-dependent temp dir matching

--- crates/uv/tests/pip_show.rs
+++ crates/uv/tests/pip_show.rs
@@ -25,3 +25,75 @@ fn command(context: &TestContext) -> Command {
         .current_dir(&context.temp_dir);
     command
 }
+
+#[test]
+fn empty() {
+    let context = TestContext::new("3.12");
+
+    uv_snapshot!(Command::new(get_bin())
+        .arg("pip")
+        .arg("show")
+        .arg("--cache-dir")
+        .arg(context.cache_dir.path())
+        .env("VIRTUAL_ENV", context.venv.as_os_str())
+        .current_dir(&context.temp_dir), @r###"
+    success: false
+    exit_code: 1
+    ----- stdout -----
+
+    ----- stderr -----
+    Please provide a package name or names.
+    "###
+    );
+}
+
+#[test]
+fn found() -> Result<()> {
+    let context = TestContext::new("3.12");
+
+    let requirements_txt = context.temp_dir.child("requirements.txt");
+    requirements_txt.touch()?;
+    requirements_txt.write_str("MarkupSafe==2.1.3")?;
+
+    uv_snapshot!(command(&context)
+        .arg("-r")
+        .arg("requirements.txt")
+        .arg("--strict"), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Resolved 1 package in [TIME]
+    Downloaded 1 package in [TIME]
+    Installed 1 package in [TIME]
+     + markupsafe==2.1.3
+    "###
+    );
+
+    context.assert_command("import markupsafe").success();
+    println!("context: {:?}", context);
+    uv_snapshot!(Command::new(get_bin())
+        .arg("pip")
+        .arg("show")
+        .arg("markupsafe")
+        .arg("--cache-dir")
+        .arg(context.cache_dir.path())
+        .env("VIRTUAL_ENV", context.venv.as_os_str())
+        .current_dir(&context.temp_dir), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    Package    Version
+    ---------- -------
+    markupsafe 2.1.3
+
+    ----- stderr -----
+    Name: markupsafe
+    Version: 2.1.3
+    Location: [TEMP_DIR]/.venv/lib/python3.12/site-packages
+    "###
+    );
+
+    Ok(())
+}
