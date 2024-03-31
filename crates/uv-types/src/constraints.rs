9621b6229aaf4a09a13f65198f4f73697b7fbe68
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-03-31 11:52:22 -0400

    Respect overrides in all direct-dependency iterators

--- crates/uv-types/src/constraints.rs
+++ crates/uv-types/src/constraints.rs
@@ -32,4 +32,18 @@ impl Constraints {
     pub fn get(&self, name: &PackageName) -> Option<&Vec<Requirement>> {
         self.0.get(name)
     }
+
+    /// Apply the constraints to a set of requirements.
+    pub fn apply<'a>(
+        &'a self,
+        requirements: impl IntoIterator<Item = &'a Requirement>,
+    ) -> impl Iterator<Item = &Requirement> {
+        requirements.into_iter().flat_map(|requirement| {
+            std::iter::once(requirement).chain(
+                self.get(&requirement.name)
+                    .into_iter()
+                    .flat_map(|constraints| constraints.iter()),
+            )
+        })
+    }
 }
