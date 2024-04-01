1465ed6a9a76096b7a5e0ccecead0bf872c58953
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-04-01 17:32:00 -0400

    Rename resolver.rs to unnamed.rs in uv-requirements

--- crates/uv-requirements/src/lib.rs
+++ crates/uv-requirements/src/lib.rs
@@ -1,14 +1,14 @@
 pub use crate::lookahead::*;
-pub use crate::resolver::*;
 pub use crate::source_tree::*;
 pub use crate::sources::*;
 pub use crate::specification::*;
+pub use crate::unnamed::*;
 
 mod confirm;
 mod lookahead;
 mod pyproject;
-mod resolver;
 mod source_tree;
 mod sources;
 mod specification;
+mod unnamed;
 pub mod upgrade;
