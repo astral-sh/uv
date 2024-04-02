astral-sh/uv

commit e1878c83593638c7ca5298a2269cf140e9e994e5

Consider installed packages during resolution (#2596)
Previously, we did not consider installed distributions as candidates
while performing resolution. Here, we update the resolver to use
installed distributions that satisfy requirements instead of pulling new
distributions from the registry.

The implementation details are as follows:

- We now provide `SitePackages` to the `CandidateSelector`
- If an installed distribution satisfies the requirement, we prefer it
over remote distributions
- We do not want to allow installed distributions in some cases, i.e.,
upgrade and reinstall
- We address this by introducing an `Exclusions` type which tracks
installed packages to ignore during selection
- There's a new `ResolvedDist` wrapper with `Installed(InstalledDist)`
and `Installable(Dist)` variants
- This lets us pass already installed distributions throughout the
resolver

The user-facing behavior is thoroughly covered in the tests, but
briefly:

- Installing a package that depends on an already-installed package
prefers the local version over the index
- Installing a package with a name that matches an already-installed URL
package does not reinstall from the index
- Reinstalling (--reinstall) a package by name _will_ pull from the
index even if an already-installed URL package is present
- To reinstall the URL package, you must specify the URL in the request

Closes https://github.com/astral-sh/uv/issues/1661

Addresses:

- https://github.com/astral-sh/uv/issues/1476
- https://github.com/astral-sh/uv/issues/1856
- https://github.com/astral-sh/uv/issues/2093
- https://github.com/astral-sh/uv/issues/2282
- https://github.com/astral-sh/uv/issues/2383
- https://github.com/astral-sh/uv/issues/2560

## Test plan

- [x] Reproduction at `charlesnicholson/uv-pep420-bug` passes
- [x] Unit test for editable package
([#1476](https://github.com/astral-sh/uv/issues/1476))

crates/uv-installer/src/plan.rs
@@ -16,8 +16,7 @@ use uv_cache::{ArchiveTarget, ArchiveTimestamp, Cache, CacheBucket, WheelCache};
 use uv_distribution::{BuiltWheelIndex, RegistryWheelIndex};
 use uv_fs::Simplified;
 use uv_interpreter::PythonEnvironment;
-use uv_normalize::PackageName;
-use uv_types::NoBinary;
+use uv_types::{NoBinary, Reinstall};
 
 use crate::{ResolvedEditable, SitePackages};
 
@@ -68,9 +67,10 @@ impl<'a> Planner<'a> {
         // Index all the already-downloaded wheels in the cache.
         let mut registry_index = RegistryWheelIndex::new(cache, tags, index_locations);
 
-        let mut local = vec![];
+        let mut cached = vec![];
         let mut remote = vec![];
         let mut reinstalls = vec![];
+        let mut installed = vec![];
         let mut extraneous = vec![];
         let mut seen = FxHashMap::with_capacity_and_hasher(
             self.requirements.len(),
@@ -122,7 +122,7 @@ impl<'a> Planner<'a> {
                     let existing = site_packages.remove_packages(built.name());
                     reinstalls.extend(existing);
 
-                    local.push(built.wheel.clone());
+                    cached.push(built.wheel.clone());
                 }
             }
         }
@@ -166,53 +166,20 @@ impl<'a> Planner<'a> {
             };
 
             if reinstall {
-                let installed = site_packages.remove_packages(&requirement.name);
-                reinstalls.extend(installed);
+                let installed_dists = site_packages.remove_packages(&requirement.name);
+                reinstalls.extend(installed_dists);
             } else {
-                let installed = site_packages.remove_packages(&requirement.name);
-                match installed.as_slice() {
+                let installed_dists = site_packages.remove_packages(&requirement.name);
+                match installed_dists.as_slice() {
                     [] => {}
                     [distribution] => {
-                        // Filter out already-installed packages.
-                        match requirement.version_or_url.as_ref() {
-                            // Accept any version of the package.
-                            None => continue,
-
-                            // If the requirement comes from a registry, check by name.
-                            Some(VersionOrUrl::VersionSpecifier(version_specifier)) => {
-                                if version_specifier.contains(distribution.version()) {
-                                    debug!("Requirement already satisfied: {distribution}");
-                                    continue;
-                                }
-                            }
-
-                            // If the requirement comes from a direct URL, check by URL.
-                            Some(VersionOrUrl::Url(url)) => {
-                                if let InstalledDist::Url(installed) = &distribution {
-                                    if &installed.url == url.raw() {
-                                        // If the requirement came from a local path, check freshness.
-                                        if let Ok(archive) = url.to_file_path() {
-                                            if ArchiveTimestamp::up_to_date_with(
-                                                &archive,
-                                                ArchiveTarget::Install(distribution),
-                                            )? {
-                                                debug!("Requirement already satisfied (and up-to-date): {installed}");
-                                                continue;
-                                            }
-                                            debug!("Requirement already satisfied (but not up-to-date): {installed}");
-                                        } else {
-                                            // Otherwise, assume the requirement is up-to-date.
-                                            debug!("Requirement already satisfied (assumed up-to-date): {installed}");
-                                            continue;
-                                        }
-                                    }
-                                }
-                            }
+                        if installed_satisfies_requirement(distribution, requirement)? {
+                            installed.push(distribution.clone());
+                            continue;
                         }
-
                         reinstalls.push(distribution.clone());
                     }
-                    _ => reinstalls.extend(installed),
+                    _ => reinstalls.extend(installed_dists),
                 }
             }
 
@@ -222,14 +189,14 @@ impl<'a> Planner<'a> {
                 continue;
             }
 
-            // Identify any locally-available distributions that satisfy the requirement.
+            // Identify any cached distributions that satisfy the requirement.
             match requirement.version_or_url.as_ref() {
                 None => {
                     if let Some((_version, distribution)) =
                         registry_index.get(&requirement.name).next()
                     {
                         debug!("Requirement already cached: {distribution}");
-                        local.push(CachedDist::Registry(distribution.clone()));
+                        cached.push(CachedDist::Registry(distribution.clone()));
                         continue;
                     }
                 }
@@ -246,7 +213,7 @@ impl<'a> Planner<'a> {
                             })
                     {
                         debug!("Requirement already cached: {distribution}");
-                        local.push(CachedDist::Registry(distribution.clone()));
+                        cached.push(CachedDist::Registry(distribution.clone()));
                         continue;
                     }
                 }
@@ -291,7 +258,7 @@ impl<'a> Planner<'a> {
                                     );
 
                                     debug!("URL wheel requirement already cached: {cached_dist}");
-                                    local.push(CachedDist::Url(cached_dist));
+                                    cached.push(CachedDist::Url(cached_dist));
                                     continue;
                                 }
                                 Err(err) if err.kind() == io::ErrorKind::NotFound => {
@@ -339,7 +306,7 @@ impl<'a> Planner<'a> {
                                         debug!(
                                             "URL wheel requirement already cached: {cached_dist}"
                                         );
-                                        local.push(CachedDist::Url(cached_dist));
+                                        cached.push(CachedDist::Url(cached_dist));
                                         continue;
                                     }
                                 }
@@ -355,7 +322,7 @@ impl<'a> Planner<'a> {
                             if let Some(wheel) = BuiltWheelIndex::url(&sdist, cache, tags)? {
                                 let cached_dist = wheel.into_url_dist(url.clone());
                                 debug!("URL source requirement already cached: {cached_dist}");
-                                local.push(CachedDist::Url(cached_dist));
+                                cached.push(CachedDist::Url(cached_dist));
                                 continue;
                             }
                         }
@@ -365,7 +332,7 @@ impl<'a> Planner<'a> {
                             if let Some(wheel) = BuiltWheelIndex::path(&sdist, cache, tags)? {
                                 let cached_dist = wheel.into_url_dist(url.clone());
                                 debug!("Path source requirement already cached: {cached_dist}");
-                                local.push(CachedDist::Url(cached_dist));
+                                cached.push(CachedDist::Url(cached_dist));
                                 continue;
                             }
                         }
@@ -375,7 +342,7 @@ impl<'a> Planner<'a> {
                             if let Some(wheel) = BuiltWheelIndex::git(&sdist, cache, tags) {
                                 let cached_dist = wheel.into_url_dist(url.clone());
                                 debug!("Git source requirement already cached: {cached_dist}");
-                                local.push(CachedDist::Url(cached_dist));
+                                cached.push(CachedDist::Url(cached_dist));
                                 continue;
                             }
                         }
@@ -409,7 +376,8 @@ impl<'a> Planner<'a> {
         }
 
         Ok(Plan {
-            local,
+            cached,
+            installed,
             remote,
             reinstalls,
             extraneous,
@@ -429,7 +397,11 @@ enum Specifier<'a> {
 pub struct Plan {
     /// The distributions that are not already installed in the current environment, but are
     /// available in the local cache.
-    pub local: Vec<CachedDist>,
+    pub cached: Vec<CachedDist>,
+
+    /// Any distributions that are already installed in the current environment, and can be used
+    /// to satisfy the requirements.
+    pub installed: Vec<InstalledDist>,
 
     /// The distributions that are not already installed in the current environment, and are
     /// not available in the local cache.
@@ -444,37 +416,49 @@ pub struct Plan {
     pub extraneous: Vec<InstalledDist>,
 }
 
-#[derive(Debug, Clone)]
-pub enum Reinstall {
-    /// Don't reinstall any packages; respect the existing installation.
-    None,
+/// Returns true if a requirement is satisfied by an installed distribution.
+///
+/// Returns an error if IO fails during a freshness check for a local path.
+fn installed_satisfies_requirement(
+    distribution: &InstalledDist,
+    requirement: &Requirement,
+) -> Result<bool> {
+    // Filter out already-installed packages.
+    match requirement.version_or_url.as_ref() {
+        // Accept any version of the package.
+        None => return Ok(true),
 
-    /// Reinstall all packages in the plan.
-    All,
+        // If the requirement comes from a registry, check by name.
+        Some(VersionOrUrl::VersionSpecifier(version_specifier)) => {
+            if version_specifier.contains(distribution.version()) {
+                debug!("Requirement already satisfied: {distribution}");
+                return Ok(true);
+            }
+        }
 
-    /// Reinstall only the specified packages.
-    Packages(Vec<PackageName>),
-}
-
-impl Reinstall {
-    /// Determine the reinstall strategy to use.
-    pub fn from_args(reinstall: bool, reinstall_package: Vec<PackageName>) -> Self {
-        if reinstall {
-            Self::All
-        } else if !reinstall_package.is_empty() {
-            Self::Packages(reinstall_package)
-        } else {
-            Self::None
+        // If the requirement comes from a direct URL, check by URL.
+        Some(VersionOrUrl::Url(url)) => {
+            if let InstalledDist::Url(installed) = &distribution {
+                if &installed.url == url.raw() {
+                    // If the requirement came from a local path, check freshness.
+                    if let Ok(archive) = url.to_file_path() {
+                        if ArchiveTimestamp::up_to_date_with(
+                            &archive,
+                            ArchiveTarget::Install(distribution),
+                        )? {
+                            debug!("Requirement already satisfied (and up-to-date): {installed}");
+                            return Ok(true);
+                        }
+                        debug!("Requirement already satisfied (but not up-to-date): {installed}");
+                    } else {
+                        // Otherwise, assume the requirement is up-to-date.
+                        debug!("Requirement already satisfied (assumed up-to-date): {installed}");
+                        return Ok(true);
+                    }
+                }
+            }
         }
     }
 
-    /// Returns `true` if no packages should be reinstalled.
-    pub fn is_none(&self) -> bool {
-        matches!(self, Self::None)
-    }
-
-    /// Returns `true` if all packages should be reinstalled.
-    pub fn is_all(&self) -> bool {
-        matches!(self, Self::All)
-    }
+    Ok(false)
 }

crates/uv-resolver/src/candidate_selector.rs
@@ -4,13 +4,15 @@ use distribution_types::{CompatibleDist, IncompatibleDist, IncompatibleSource};
 use distribution_types::{DistributionMetadata, IncompatibleWheel, Name, PrioritizedDist};
 use pep440_rs::Version;
 use pep508_rs::MarkerEnvironment;
+use tracing::debug;
 use uv_normalize::PackageName;
+use uv_types::InstalledPackagesProvider;
 
 use crate::preferences::Preferences;
 use crate::prerelease_mode::PreReleaseStrategy;
 use crate::resolution_mode::ResolutionStrategy;
 use crate::version_map::{VersionMap, VersionMapDistHandle};
-use crate::{Manifest, Options};
+use crate::{Exclusions, Manifest, Options};
 
 #[derive(Debug, Clone)]
 pub(crate) struct CandidateSelector {
@@ -61,19 +63,59 @@ enum AllowPreRelease {
 
 impl CandidateSelector {
     /// Select a [`Candidate`] from a set of candidate versions and files.
-    pub(crate) fn select<'a>(
+    ///
+    /// Unless present in the provided [`Exclusions`], local distributions from the
+    /// [`InstalledPackagesProvider`] are preferred over remote distributions in
+    /// the [`VersionMap`].
+    pub(crate) fn select<'a, InstalledPackages: InstalledPackagesProvider>(
         &'a self,
         package_name: &'a PackageName,
         range: &'a Range<Version>,
         version_map: &'a VersionMap,
         preferences: &'a Preferences,
+        installed_packages: &'a InstalledPackages,
+        exclusions: &'a Exclusions,
     ) -> Option<Candidate<'a>> {
         // If the package has a preference (e.g., an existing version from an existing lockfile),
         // and the preference satisfies the current range, use that.
         if let Some(version) = preferences.version(package_name) {
             if range.contains(version) {
+                // Check for a locally installed distribution that matches the preferred version
+                if !exclusions.contains(package_name) {
+                    for dist in installed_packages.get_packages(package_name) {
+                        if dist.version() == version {
+                            debug!("Found installed version of {dist} that satisfies preference in {range}");
+
+                            return Some(Candidate {
+                                name: package_name,
+                                version,
+                                dist: CandidateDist::Compatible(CompatibleDist::InstalledDist(
+                                    dist,
+                                )),
+                            });
+                        }
+                    }
+                }
+
+                // Check for a remote distribution that matches the preferred version
                 if let Some(file) = version_map.get(version) {
                     return Some(Candidate::new(package_name, version, file));
+                }
+            }
+        }
+
+        // Check for a locally installed distribution that satisfies the range
+        if !exclusions.contains(package_name) {
+            for dist in installed_packages.get_packages(package_name) {
+                let version = dist.version();
+                if range.contains(version) {
+                    debug!("Found installed version of {dist} that satisfies {range}");
+
+                    return Some(Candidate {
+                        name: package_name,
+                        version,
+                        dist: CandidateDist::Compatible(CompatibleDist::InstalledDist(dist)),
+                    });
                 }
             }
         }
@@ -100,7 +142,7 @@ impl CandidateSelector {
         };
 
         tracing::trace!(
-            "selecting candidate for package {:?} with range {:?} with {} versions",
+            "selecting candidate for package {:?} with range {:?} with {} remote versions",
             package_name,
             range,
             version_map.len()

commit 119d753cfe9e9b4447168fd4b1e5dac325946591

Exclude installed distributions with multiple versions from consideration in the resolver (#2779)
Addresses panic introduced in #2596 and reported in
https://github.com/astral-sh/uv/issues/2763#issuecomment-2030674936

When there are multiple versions of a package available, we remove the
existing packages before installing the resolved version to "fix" the
environment. We must remove all of the package versions and reinstall
because removing _any_ of the package versions could break the others.
Since reinstalls require a pull from the remote, this broke a contract
between the resolver and the planner which must always agree on which
packages should come from the remote. This further demonstrates that we
should be constructing the install plan with more concrete knowledge
from the resolver (i.e. `ResolvedDist` instead of `Requirement`) to
avoid having to manually ensure logic matches.

## Test plan

Fails on `main` with panic succeeds on branch

```
uv venv --seed
source .venv/bin/activate
pip install anyio==3.7.0 --ignore-installed
pip install anyio==4.0.0 --ignore-installed
cargo run -- pip install anyio black -v
```

crates/uv-installer/src/plan.rs
@@ -174,11 +174,15 @@ impl<'a> Planner<'a> {
                     [] => {}
                     [distribution] => {
                         if installed_satisfies_requirement(distribution, requirement)? {
+                            debug!("Requirement already installed: {distribution}");
                             installed.push(distribution.clone());
                             continue;
                         }
                         reinstalls.push(distribution.clone());
                     }
+                    // We reinstall installed distributions with multiple versions because
+                    // we do not want to keep multiple incompatible versions but removing
+                    // one version is likely to break another.
                     _ => reinstalls.extend(installed_dists),
                 }
             }

crates/uv-resolver/src/candidate_selector.rs
@@ -82,18 +82,25 @@ impl CandidateSelector {
             if range.contains(version) {
                 // Check for a locally installed distribution that matches the preferred version
                 if !exclusions.contains(package_name) {
-                    for dist in installed_packages.get_packages(package_name) {
-                        if dist.version() == version {
-                            debug!("Found installed version of {dist} that satisfies preference in {range}");
+                    let installed_dists = installed_packages.get_packages(package_name);
+                    match installed_dists.as_slice() {
+                        [] => {}
+                        [dist] => {
+                            if dist.version() == version {
+                                debug!("Found installed version of {dist} that satisfies preference in {range}");
 
-                            return Some(Candidate {
-                                name: package_name,
-                                version,
-                                dist: CandidateDist::Compatible(CompatibleDist::InstalledDist(
-                                    dist,
-                                )),
-                            });
+                                return Some(Candidate {
+                                    name: package_name,
+                                    version,
+                                    dist: CandidateDist::Compatible(CompatibleDist::InstalledDist(
+                                        dist,
+                                    )),
+                                });
+                            }
                         }
+                        // We do not consider installed distributions with multiple versions because
+                        // during installation these must be reinstalled from the remote
+                        _ => {}
                     }
                 }
 
@@ -106,17 +113,24 @@ impl CandidateSelector {
 
         // Check for a locally installed distribution that satisfies the range
         if !exclusions.contains(package_name) {
-            for dist in installed_packages.get_packages(package_name) {
-                let version = dist.version();
-                if range.contains(version) {
-                    debug!("Found installed version of {dist} that satisfies {range}");
+            let installed_dists = installed_packages.get_packages(package_name);
+            match installed_dists.as_slice() {
+                [] => {}
+                [dist] => {
+                    let version = dist.version();
+                    if range.contains(version) {
+                        debug!("Found installed version of {dist} that satisfies {range}");
 
-                    return Some(Candidate {
-                        name: package_name,
-                        version,
-                        dist: CandidateDist::Compatible(CompatibleDist::InstalledDist(dist)),
-                    });
+                        return Some(Candidate {
+                            name: package_name,
+                            version,
+                            dist: CandidateDist::Compatible(CompatibleDist::InstalledDist(dist)),
+                        });
+                    }
                 }
+                // We do not consider installed distributions with multiple versions because
+                // during installation these must be reinstalled from the remote
+                _ => {}
             }
         }
 

crates/uv/tests/pip_install.rs
@@ -3513,6 +3513,74 @@ fn already_installed_local_version_of_remote_package() {
     );
 }
 
+/// Install a package with multiple installed distributions in a virtual environment.
+#[test]
+#[cfg(unix)]
+fn already_installed_multiple_versions() -> Result<()> {
+    use crate::common::copy_dir_all;
+
+    // Install a version
+    let context1 = TestContext::new("3.12");
+    uv_snapshot!(context1.filters(), context1.install()
+        .arg("anyio==3.7.0"), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Resolved 3 packages in [TIME]
+    Downloaded 3 packages in [TIME]
+    Installed 3 packages in [TIME]
+     + anyio==3.7.0
+     + idna==3.6
+     + sniffio==1.3.1
+    "###
+    );
+
+    // Install another version
+    let context2 = TestContext::new("3.12");
+    uv_snapshot!(context2.filters(), context2.install()
+        .arg("anyio==4.0.0"), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Resolved 3 packages in [TIME]
+    Downloaded 3 packages in [TIME]
+    Installed 3 packages in [TIME]
+     + anyio==4.0.0
+     + idna==3.6
+     + sniffio==1.3.1
+    "###
+    );
+
+    // Copy the second version into the first environment
+    copy_dir_all(
+        context2.site_packages().join("anyio-4.0.0.dist-info"),
+        context1.site_packages().join("anyio-4.0.0.dist-info"),
+    )?;
+
+    // Request the second anyio version again
+    // Should remove both previous versions and reinstall the second one
+    uv_snapshot!(context1.filters(), context1.install().arg("anyio==4.0.0"), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Resolved 3 packages in [TIME]
+    Downloaded 1 package in [TIME]
+    Installed 1 package in [TIME]
+     - anyio==3.7.0
+     - anyio==4.0.0
+     + anyio==4.0.0
+    "###
+    );
+
+    Ok(())
+}
+
 /// Install a package from a remote URL
 #[test]
 #[cfg(feature = "git")]