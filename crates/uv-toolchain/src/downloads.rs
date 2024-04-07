67a62dc0f9fa1b8e640e97762c42f999ee485eb2
Zanie Blue <contact@zanie.dev>
2024-04-07 09:46:34 -0500

    Add utilities to load platform information from the target

--- crates/uv-toolchain/src/downloads.rs
+++ crates/uv-toolchain/src/downloads.rs
@@ -1,3 +1,15 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum DownloadMetadataError {
+    #[error("operating system not supported: {0}")]
+    OsNotSupported(&'static str),
+    #[error("architecture not supported: {0}")]
+    ArchNotSupported(&'static str),
+    #[error("libc type could not be detected")]
+    LibcNotDetected(),
+}
+
 #[derive(Debug, PartialEq)]
 pub struct PythonDownloadMetadata {
     name: ImplementationName,
@@ -12,7 +24,7 @@ pub struct PythonDownloadMetadata {
 }
 
 #[derive(Debug, PartialEq)]
-pub(crate) enum Arch {
+pub enum Arch {
     Arm64,
     I686,
     Ppc64Le,
@@ -22,7 +34,7 @@ pub(crate) enum Arch {
 }
 
 #[derive(Debug, PartialEq)]
-pub(crate) enum Libc {
+pub enum Libc {
     Gnu,
     Musl,
     None,
@@ -34,12 +46,20 @@ pub(crate) enum ImplementationName {
 }
 
 #[derive(Debug, PartialEq)]
-pub(crate) enum Os {
+pub enum Os {
     Darwin,
     Linux,
     Shared,
     Windows,
 }
+
+#[derive(Debug, PartialEq)]
+pub struct Platform {
+    os: Os,
+    arch: Arch,
+    libc: Libc,
+}
+
 include!("python_versions.inc");
 
 impl PythonDownloadMetadata {
@@ -51,5 +71,53 @@ impl PythonDownloadMetadata {
             }
         }
         None
+    }
+}
+
+impl Platform {
+    pub fn new(os: Os, arch: Arch, libc: Libc) -> Self {
+        Self { os, arch, libc }
+    }
+    pub fn from_env() -> Result<Self, DownloadMetadataError> {
+        Ok(Self::new(
+            Os::from_env()?,
+            Arch::from_env()?,
+            Libc::from_env()?,
+        ))
+    }
+}
+
+impl Arch {
+    fn from_env() -> Result<Self, DownloadMetadataError> {
+        match std::env::consts::ARCH {
+            "arm64" => Ok(Arch::Arm64),
+            "i686" => Ok(Arch::I686),
+            "ppc64le" => Ok(Arch::Ppc64Le),
+            "s390x" => Ok(Arch::S390X),
+            "x86_64" => Ok(Arch::X86_64),
+            arch => Err(DownloadMetadataError::ArchNotSupported(arch)),
+        }
+    }
+}
+
+impl Os {
+    fn from_env() -> Result<Self, DownloadMetadataError> {
+        match std::env::consts::OS {
+            "linux" => Ok(Os::Linux),
+            "windows" => Ok(Os::Windows),
+            "macos" => Ok(Os::Darwin),
+            os => Err(DownloadMetadataError::OsNotSupported(os)),
+        }
+    }
+}
+
+impl Libc {
+    fn from_env() -> Result<Self, DownloadMetadataError> {
+        // TODO(zanieb): Perform this lookup
+        match std::env::consts::OS {
+            "linux" | "macos" => Ok(Libc::Gnu),
+            "windows" => Ok(Libc::None),
+            _ => Err(DownloadMetadataError::LibcNotDetected()),
+        }
     }
 }
--- crates/uv-toolchain/src/lib.rs
+++ crates/uv-toolchain/src/lib.rs
@@ -1,3 +1,3 @@
-pub use downloads::PythonDownloadMetadata;
+pub use downloads::{Platform, PythonDownloadMetadata};
 
 mod downloads;
