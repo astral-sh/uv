52577892ebcd4cdd8d7d731a529834b65c5673e8
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-04-07 22:50:24 -0400

    Expand some documentation around identifier traits (#2876)
    ## Summary
    
    I already added more documentation since this issue was created, but
    this doesn't hurt.
    
    Closes https://github.com/astral-sh/uv/issues/496.

--- crates/distribution-types/src/id.rs
+++ crates/distribution-types/src/id.rs
@@ -5,10 +5,12 @@ use url::Url;
 use pep440_rs::Version;
 use uv_normalize::PackageName;
 
-/// A unique identifier for a package (e.g., `black==23.10.0`).
+/// A unique identifier for a package at a specific version (e.g., `black==23.10.0`).
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum PackageId {
+    /// The identifier consists of a package name and version.
     NameVersion(PackageName, Version),
+    /// The identifier consists of a URL.
     Url(String),
 }
 
@@ -33,7 +35,19 @@ impl Display for PackageId {
     }
 }
 
-/// A unique identifier for a distribution (e.g., `black-23.10.0-py3-none-any.whl`).
+/// A unique resource identifier for the distribution, like a SHA-256 hash of the distribution's
+/// contents.
+///
+/// A distribution is a specific archive of a package at a specific version. For a given package
+/// version, there may be multiple distributions, e.g., source distribution, along with
+/// multiple binary distributions (wheels) for different platforms. As a concrete example,
+/// `black-23.10.0-py3-none-any.whl` would represent a (binary) distribution of the `black` package
+/// at version `23.10.0`.
+///
+/// The distribution ID is used to uniquely identify a distribution. Ideally, the distribution
+/// ID should be a hash of the distribution's contents, though in practice, it's only required
+/// that the ID is unique within a single invocation of the resolver (and so, e.g., a hash of
+/// the URL would also be sufficient).
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct DistributionId(String);
 
