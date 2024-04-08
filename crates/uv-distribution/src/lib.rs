31a67f539f860168b133fdba0889a339c1f7bd16
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-04-08 14:15:20 -0400

    Remove unused local wheel types (#2906)
    ## Summary
    
    No behavior changes. Just removing unused code.

--- crates/uv-distribution/src/lib.rs
+++ crates/uv-distribution/src/lib.rs
@@ -1,11 +1,10 @@
 pub use distribution_database::DistributionDatabase;
-pub use download::{BuiltWheel, DiskWheel, LocalWheel};
+pub use download::LocalWheel;
 pub use error::Error;
 pub use git::{is_same_reference, to_precise};
 pub use index::{BuiltWheelIndex, RegistryWheelIndex};
 pub use reporter::Reporter;
 pub use source::SourceDistributionBuilder;
-pub use unzip::Unzip;
 
 mod distribution_database;
 mod download;
@@ -15,4 +14,3 @@ mod index;
 mod locks;
 mod reporter;
 mod source;
-mod unzip;
