3fe593a031ba7830f99ba12ee2d8cc4f5b7b25c3
Zanie Blue <contact@zanie.dev>
2024-04-11 15:21:47 -0500

    Remove manual seeding of cache

--- crates/uv-auth/src/lib.rs
+++ crates/uv-auth/src/lib.rs
@@ -10,7 +10,6 @@ pub use keyring::KeyringProvider;
 pub use middleware::AuthMiddleware;
 use netloc::NetLoc;
 use once_cell::sync::Lazy;
-use url::Url;
 
 // TODO(zanieb): Consider passing a cache explicitly throughout
 
@@ -18,10 +17,3 @@ use url::Url;
 ///
 /// This is used to share credentials across uv clients.
 pub(crate) static CREDENTIALS_CACHE: Lazy<CredentialsCache> = Lazy::new(CredentialsCache::default);
-
-/// Populate the global authentication store with credentials on a URL, if there are any.
-///
-/// Returns `true` if the store was updated.
-pub fn store_credentials_from_url(_url: &Url) -> bool {
-    true
-}
