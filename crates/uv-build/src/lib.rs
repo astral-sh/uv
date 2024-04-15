a67044527d9ce0ec0bf7981037218c976ed99d6b
konstin <konstin@mailbox.org>
2024-04-15 11:00:36 +0200

    Force color for build error messages

--- crates/uv-build/src/lib.rs
+++ crates/uv-build/src/lib.rs
@@ -999,6 +999,7 @@ async fn run_python_script(
         .env("PATH", modified_path)
         // Activate the venv
         .env("VIRTUAL_ENV", venv.root())
+        .env("CLICOLOR_FORCE", "1")
         .output()
         .await
         .map_err(|err| Error::CommandFailed(venv.python_executable().to_path_buf(), err))
