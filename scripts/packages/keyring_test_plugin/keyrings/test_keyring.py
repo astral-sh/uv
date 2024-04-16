f1f7f27e841efacb80c563cc49260e3af5fb3b94
Zanie Blue <contact@zanie.dev>
2024-04-16 12:38:57 -0500

    Add integration test coverage for keyring authentication

---
+++ scripts/packages/keyring_test_plugin/keyrings/test_keyring.py
@@ -0,0 +1,23 @@
+import json
+import os
+import sys
+
+from keyring import backend
+
+
+class KeyringTest(backend.KeyringBackend):
+    priority = 9
+
+    def get_password(self, service, username):
+        print(f"Request for {username}@{service}", file=sys.stderr)
+        credentials = json.loads(os.environ.get("KEYRING_TEST_CREDENTIALS") or {})
+        return credentials.get(service, {}).get(username)
+
+    def set_password(self, service, username, password):
+        raise NotImplementedError()
+
+    def delete_password(self, service, username):
+        raise NotImplementedError()
+
+    def get_credential(self, service, username):
+        raise NotImplementedError()
