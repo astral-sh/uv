325265ec16b10ab4bee08a95d9d86f6a83178417
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-05-13 21:34:14 -0400

    Move `pip` commands into their own module (#3565)
    ## Summary
    
    Matching the structure of the `project` module and API.

---
+++ crates/uv/src/commands/pip/mod.rs
@@ -0,0 +1,8 @@
+pub(crate) mod check;
+pub(crate) mod compile;
+pub(crate) mod freeze;
+pub(crate) mod install;
+pub(crate) mod list;
+pub(crate) mod show;
+pub(crate) mod sync;
+pub(crate) mod uninstall;
