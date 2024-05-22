5cd3af8ab4728922c01734f0d64734ada71aea32
Zanie Blue <contact@zanie.dev>
2024-05-22 17:31:13 -0400

    Use `uv tool run` for `release` script (#3764)
    Dogfooding!

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,28 +2,14 @@
 # Prepare for a release
 #
 # All additional options are passed to `rooster`
-#
-# See `scripts/release/` for supporting files.
 set -eu
 
 script_root="$(realpath "$(dirname "$0")")"
 project_root="$(dirname "$script_root")"
 
-cd "$script_root/release"
-echo "Setting up a temporary environment..."
-uv venv
-
-source ".venv/bin/activate"
-uv pip install -r requirements.txt
-
 echo "Updating metadata with rooster..."
 cd "$project_root"
-rooster release "$@"
+uv tool run --from rooster-blue --isolated -- rooster release "$@"
 
 echo "Updating lockfile..."
 cargo update -p uv
-
-echo "Generating contributors list..."
-echo ""
-echo ""
-rooster contributors --quiet
