7b7da803bbc9bfd8a46d386acf720d2cf69381d8
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-05-31 17:08:00 -0400

    Unify editable and unnamed URL parsing (#3946)
    ## Summary
    
    This will help prevent bugs like #3934 by unifying the implementations
    for editables and non-editable unnamed requirements. Specifically, both
    of these now go through the same parsing paths and use the same struct
    representations (with the exception that the editable flag is flipped in
    the first case):
    
    ```
    -e ./foo/bar
    ./foo/bar
    ```
    
    We also now support PEP 508 in editable URLs. It turns out this is just
    a limitation in pip, so it's correct to support it. For example, this
    now works:
    
    ```
    -e black[d] @ file://${PROJECT_ROOT}/scripts/packages/black_editable
    ```
    
    Closes #3941.
    
    Closes #3942.

--- crates/requirements-txt/src/requirement.rs
+++ crates/requirements-txt/src/requirement.rs
@@ -3,7 +3,29 @@ use std::path::Path;
 use pep508_rs::{
     Pep508Error, Pep508ErrorSource, RequirementOrigin, TracingReporter, UnnamedRequirement,
 };
-use pypi_types::VerbatimParsedUrl;
+use pypi_types::{ParsedPathUrl, ParsedUrl, VerbatimParsedUrl};
+use uv_normalize::PackageName;
+
+#[derive(Debug, thiserror::Error)]
+pub enum EditableError {
+    #[error("Editable `{0}` must refer to a local directory")]
+    MissingVersion(PackageName),
+
+    #[error("Editable `{0}` must refer to a local directory, not a versioned package")]
+    Versioned(PackageName),
+
+    #[error("Editable `{0}` must refer to a local directory, not an HTTPS URL: `{1}`")]
+    Https(PackageName, String),
+
+    #[error("Editable `{0}` must refer to a local directory, not a Git URL: `{1}`")]
+    Git(PackageName, String),
+
+    #[error("Editable must refer to a local directory, not an HTTPS URL: `{0}`")]
+    UnnamedHttps(String),
+
+    #[error("Editable must refer to a local directory, not a Git URL: `{0}`")]
+    UnnamedGit(String),
+}
 
 /// A requirement specifier in a `requirements.txt` file.
 ///
@@ -25,6 +47,69 @@ impl RequirementsTxtRequirement {
         match self {
             Self::Named(requirement) => Self::Named(requirement.with_origin(origin)),
             Self::Unnamed(requirement) => Self::Unnamed(requirement.with_origin(origin)),
+        }
+    }
+
+    /// Convert the [`RequirementsTxtRequirement`] into an editable requirement.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`EditableError`] if the requirement cannot be interpreted as editable.
+    /// Specifically, only local directory URLs are supported.
+    pub fn into_editable(self) -> Result<Self, EditableError> {
+        match self {
+            RequirementsTxtRequirement::Named(requirement) => {
+                let Some(version_or_url) = requirement.version_or_url else {
+                    return Err(EditableError::MissingVersion(requirement.name));
+                };
+
+                let pep508_rs::VersionOrUrl::Url(url) = version_or_url else {
+                    return Err(EditableError::Versioned(requirement.name));
+                };
+
+                let parsed_url = match url.parsed_url {
+                    ParsedUrl::Path(parsed_url) => parsed_url,
+                    ParsedUrl::Archive(_) => {
+                        return Err(EditableError::Https(requirement.name, url.to_string()))
+                    }
+                    ParsedUrl::Git(_) => {
+                        return Err(EditableError::Git(requirement.name, url.to_string()))
+                    }
+                };
+
+                Ok(Self::Named(pep508_rs::Requirement {
+                    version_or_url: Some(pep508_rs::VersionOrUrl::Url(VerbatimParsedUrl {
+                        verbatim: url.verbatim,
+                        parsed_url: ParsedUrl::Path(ParsedPathUrl {
+                            editable: true,
+                            ..parsed_url
+                        }),
+                    })),
+                    ..requirement
+                }))
+            }
+            RequirementsTxtRequirement::Unnamed(requirement) => {
+                let parsed_url = match requirement.url.parsed_url {
+                    ParsedUrl::Path(parsed_url) => parsed_url,
+                    ParsedUrl::Archive(_) => {
+                        return Err(EditableError::UnnamedHttps(requirement.to_string()))
+                    }
+                    ParsedUrl::Git(_) => {
+                        return Err(EditableError::UnnamedGit(requirement.to_string()))
+                    }
+                };
+
+                Ok(Self::Unnamed(UnnamedRequirement {
+                    url: VerbatimParsedUrl {
+                        verbatim: requirement.url.verbatim,
+                        parsed_url: ParsedUrl::Path(ParsedPathUrl {
+                            editable: true,
+                            ..parsed_url
+                        }),
+                    },
+                    ..requirement
+                }))
+            }
         }
     }
 }
