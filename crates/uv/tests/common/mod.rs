eaa3c5714afc1ae84ad8a3ff1cf5ff2d5a5bb7fb
Ibraheem Ahmed <ibraheem@ibraheem.ca>
2024-06-11 07:42:25 -0400

    increase stack size for windows tests

--- crates/uv/tests/common/mod.rs
+++ crates/uv/tests/common/mod.rs
@@ -328,6 +328,13 @@ impl TestContext {
             .env("VIRTUAL_ENV", self.venv.as_os_str())
             .env("UV_NO_WRAP", "1")
             .current_dir(&self.temp_dir);
+
+        if cfg!(all(windows, debug_assertions)) {
+            // TODO(konstin): Reduce stack usage in debug mode enough that the tests pass with the
+            // default windows stack of 1MB
+            command.env("UV_STACK_SIZE", (4 * 1024 * 1024).to_string());
+        }
+
         command
     }
 
@@ -342,6 +349,13 @@ impl TestContext {
             .env("VIRTUAL_ENV", self.venv.as_os_str())
             .env("UV_NO_WRAP", "1")
             .current_dir(&self.temp_dir);
+
+        if cfg!(all(windows, debug_assertions)) {
+            // TODO(konstin): Reduce stack usage in debug mode enough that the tests pass with the
+            // default windows stack of 1MB
+            command.env("UV_STACK_SIZE", (4 * 1024 * 1024).to_string());
+        }
+
         command
     }
 
