5a09c26e7768070bc67720de23e64a0fb9c16883
Nicolas Delaby <ticosax@protonmail.com>
2024-06-12 15:44:08 +0200

    Fix doc for `uv add` cli command s/remove/add/ (#4269)
    ## Summary
    
    Fix the docsting where `remove` was used instead of `add` in the context
    of `uv add` command.
    
    ## Test Plan
    
    ```
    cargo run -- add --help
    ```
    ```
    Add one or more packages to the project requirements
    
    Usage: uv add [OPTIONS] <REQUIREMENTS>...
    
    Arguments:
      <REQUIREMENTS>...
              The packages to add, as PEP 508 requirements (e.g., `flask==2.2.3`)
    
    ```

--- crates/uv/src/cli.rs
+++ crates/uv/src/cli.rs
@@ -1936,7 +1936,7 @@ pub(crate) struct LockArgs {
 #[derive(Args)]
 #[allow(clippy::struct_excessive_bools)]
 pub(crate) struct AddArgs {
-    /// The packages to remove, as PEP 508 requirements (e.g., `flask==2.2.3`).
+    /// The packages to add, as PEP 508 requirements (e.g., `flask==2.2.3`).
     #[arg(required = true)]
     pub(crate) requirements: Vec<String>,
 
