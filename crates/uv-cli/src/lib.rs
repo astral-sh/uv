834065ef608e6d27fdafa2b8b6728a368dc5157b
Zanie Blue <contact@zanie.dev>
2024-06-26 18:04:10 -0500

    Add support for specifying `name@version` in `uv tool run`

--- crates/uv-cli/src/lib.rs
+++ crates/uv-cli/src/lib.rs
@@ -1850,6 +1850,13 @@ pub enum ToolCommand {
 #[allow(clippy::struct_excessive_bools)]
 pub struct ToolRunArgs {
     /// The command to run.
+    ///
+    /// By default, the package to install is assumed to match the command name.
+    ///
+    /// The name of the command can include an exact version in the format `<package>@<version>`.
+    ///
+    /// If more complex version specification is desired or if the command is provided by a different
+    /// package, use `--from`.
     #[command(subcommand)]
     pub command: ExternalCommand,
 
