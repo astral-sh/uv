303dc79a6e883ded5eae346848449ffebc67278d
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-06-27 14:31:21 -0400

    Make --universal and --python-platform mutually exclusive

--- crates/uv-cli/src/lib.rs
+++ crates/uv-cli/src/lib.rs
@@ -575,7 +575,11 @@ pub struct PipCompileArgs {
     /// In universal mode, the current Python version (or user-provided `--python-version`) will be
     /// treated as a lower bound. For example, `--universal --python-version 3.7` would produce a
     /// universal resolution for Python 3.7 and later.
-    #[arg(long, overrides_with("no_universal"))]
+    #[arg(
+        long,
+        overrides_with("no_universal"),
+        conflicts_with("python_platform")
+    )]
     pub universal: bool,
 
     #[arg(long, overrides_with("universal"), hide = true)]
