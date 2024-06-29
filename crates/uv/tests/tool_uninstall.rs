852d9792d743a24c072c082d73a1083c9ee854b2
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-06-29 13:35:57 -0400

    Add more tests

--- crates/uv/tests/tool_uninstall.rs
+++ crates/uv/tests/tool_uninstall.rs
@@ -33,6 +33,7 @@ fn tool_uninstall() {
     Uninstalled: black, blackd
     "###);
 
+    // After uninstalling the tool, it shouldn't be listed.
     uv_snapshot!(context.filters(), context.tool_list()
         .env("UV_TOOL_DIR", tool_dir.as_os_str())
         .env("XDG_BIN_HOME", bin_dir.as_os_str()), @r###"
@@ -43,6 +44,28 @@ fn tool_uninstall() {
     ----- stderr -----
     warning: `uv tool list` is experimental and may change without warning.
     No tools installed
+    "###);
+
+    // After uninstalling the tool, we should be able to reinstall it.
+    uv_snapshot!(context.filters(), context.tool_install()
+        .arg("black==24.2.0")
+        .env("UV_TOOL_DIR", tool_dir.as_os_str())
+        .env("XDG_BIN_HOME", bin_dir.as_os_str()), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    warning: `uv tool install` is experimental and may change without warning.
+    Resolved 6 packages in [TIME]
+    Installed 6 packages in [TIME]
+     + black==24.2.0
+     + click==8.1.7
+     + mypy-extensions==1.0.0
+     + packaging==24.0
+     + pathspec==0.12.1
+     + platformdirs==4.2.0
+    Installed: black, blackd
     "###);
 }
 
