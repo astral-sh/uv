a0406ecfe022eadaf0a70cc477a857503a259005
Caique <caique.porfirio@argyle.com>
2024-07-03 14:06:15 -0300

    tests: add filter for env path cross plataform

--- crates/uv/tests/tool_list.rs
+++ crates/uv/tests/tool_list.rs
@@ -90,7 +90,7 @@ fn tool_list_missing_receipt() {
 
 #[test]
 fn tool_list_bad_environment() -> Result<()> {
-    let context = TestContext::new("3.12");
+    let context = TestContext::new("3.12").with_filtered_exe_suffix();
     let tool_dir = context.temp_dir.child("tools");
     let bin_dir = context.temp_dir.child("bin");
 
@@ -114,21 +114,29 @@ fn tool_list_bad_environment() -> Result<()> {
 
     let venv_path = common::venv_bin_path(tool_dir.path().join("black"));
     // Remove the python interpreter for black
-    fs::remove_dir_all(venv_path)?;
+    fs::remove_dir_all(venv_path.clone())?;
 
-    uv_snapshot!(context.filters(), context.tool_list()
-    .env("UV_TOOL_DIR", tool_dir.as_os_str())
-    .env("XDG_BIN_HOME", bin_dir.as_os_str()), @r###"
+    let mut filters = context.filters().clone();
+    filters.push((r"/black/.*", "/black/[VENV_PATH]`"));
+
+    uv_snapshot!(
+        filters,
+        context
+            .tool_list()
+            .env("UV_TOOL_DIR", tool_dir.as_os_str())
+            .env("XDG_BIN_HOME", bin_dir.as_os_str()),
+        @r###"
     success: true
     exit_code: 0
     ----- stdout -----
     ruff v0.3.4
         ruff
-    
+
     ----- stderr -----
     warning: `uv tool list` is experimental and may change without warning.
-    Python interpreter not found at `[TEMP_DIR]/tools/black/bin/python3`
-    "###);
+    Python interpreter not found at `[TEMP_DIR]/tools/black/[VENV_PATH]`
+    "###
+    );
 
     Ok(())
 }
