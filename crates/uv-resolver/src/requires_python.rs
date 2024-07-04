6daafdd708b31d523e3026ce9371b40a7419cb7a
konstin <konstin@mailbox.org>
2024-07-04 11:52:13 +0200

    Document the `RequiresPython` fields

--- crates/uv-resolver/src/requires_python.rs
+++ crates/uv-resolver/src/requires_python.rs
@@ -25,7 +25,14 @@ pub enum RequiresPythonError {
 /// See: <https://packaging.python.org/en/latest/guides/dropping-older-python-versions/>
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct RequiresPython {
+    /// The supported Python versions as provides by the user, usually through the `requires-python`
+    /// field in `pyproject.toml`.
+    ///
+    /// For a workspace, it's the union of all `requires-python` fields in the workspace. If no
+    /// bound was provided by the user, it's greater equal the current Python version.
     specifiers: VersionSpecifiers,
+    /// The lower bound from the `specifiers` field, i.e. greater or greater equal the lowest
+    /// version allowed by `specifiers`.
     bound: RequiresPythonBound,
 }
 
