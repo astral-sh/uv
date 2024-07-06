1895339cd299e51d94b92052a9db00f57a615543
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-07-06 14:29:15 -0500

    Clippy

--- crates/uv/tests/common/mod.rs
+++ crates/uv/tests/common/mod.rs
@@ -108,8 +108,10 @@ impl TestContext {
                 format!("{verb} [N] packages"),
             ));
         }
-        self.filters
-            .push((format!("Removed \\d+ files?"), format!("Removed [N] files")));
+        self.filters.push((
+            "Removed \\d+ files?".to_string(),
+            "Removed [N] files".to_string(),
+        ));
         self
     }
 
