astral-sh/uv

commit 89b3324ae146ec3a87bb2f4cbc7799ba1af30cd6

Move `Requires-Python` incompatibilities out of version map (#4705)
## Summary

This is required to solve https://github.com/astral-sh/uv/issues/4669,
because the `Requires-Python` version can now vary across a resolution.
For example, within certain forks, we might have a more narrow range,
which would allow us to use distributions that would not be allowed for
the global resolution.

This should be fine because `requires-python` is part of the package
metadata, so it should be consistent between files within a package
version. As such, there shouldn't be any risk that we incorrectly
prioritize distributions by omitting this information.

(To be more specific, the risk is something like: we prioritize some
wheel over a source distribution within a package-version, so we don't
track the source distribution at all. Then, later, when we choose a
candidate, we see that the wheel doesn't meet the `Requires-Python`
requirement, even though the source distribution _would've_ met it. If
files within a distribution could have varied support, this would be a
real risk.)

crates/uv-resolver/src/resolver/mod.rs
@@ -23,9 +23,9 @@ use tokio_stream::wrappers::ReceiverStream;
 use tracing::{debug, enabled, instrument, trace, warn, Level};
 
 use distribution_types::{
-    BuiltDist, Dist, DistributionMetadata, IncompatibleDist, IncompatibleSource, IncompatibleWheel,
-    InstalledDist, PythonRequirementKind, RemoteSource, ResolvedDist, ResolvedDistRef, SourceDist,
-    VersionOrUrlRef,
+    BuiltDist, CompatibleDist, Dist, DistributionMetadata, IncompatibleDist, IncompatibleSource,
+    IncompatibleWheel, InstalledDist, PythonRequirementKind, RemoteSource, ResolvedDist,
+    ResolvedDistRef, SourceDist, VersionOrUrlRef,
 };
 pub(crate) use locals::Locals;
 use pep440_rs::{Version, MIN_VERSION};
@@ -155,7 +155,6 @@ impl<'a, Context: BuildContext, InstalledPackages: InstalledPackagesProvider>
             database,
             flat_index,
             tags,
-            python_requirement.clone(),
             AllowedYanks::from_manifest(&manifest, markers, options.dependency_mode),
             hasher,
             options.exclude_newer,
@@ -921,6 +920,77 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                 )));
             }
         };
+
+        let incompatibility = match dist {
+            CompatibleDist::InstalledDist(_) => None,
+            CompatibleDist::SourceDist { sdist, .. }
+            | CompatibleDist::IncompatibleWheel { sdist, .. } => {
+                // Source distributions must meet both the _target_ Python version and the
+                // _installed_ Python version (to build successfully).
+                sdist
+                    .file
+                    .requires_python
+                    .as_ref()
+                    .and_then(|requires_python| {
+                        if let Some(target) = self.python_requirement.target() {
+                            if !target.is_compatible_with(requires_python) {
+                                return Some(IncompatibleDist::Source(
+                                    IncompatibleSource::RequiresPython(
+                                        requires_python.clone(),
+                                        PythonRequirementKind::Target,
+                                    ),
+                                ));
+                            }
+                        }
+                        if !requires_python.contains(self.python_requirement.installed()) {
+                            return Some(IncompatibleDist::Source(
+                                IncompatibleSource::RequiresPython(
+                                    requires_python.clone(),
+                                    PythonRequirementKind::Installed,
+                                ),
+                            ));
+                        }
+                        None
+                    })
+            }
+            CompatibleDist::CompatibleWheel { wheel, .. } => {
+                // Wheels must meet the _target_ Python version.
+                wheel
+                    .file
+                    .requires_python
+                    .as_ref()
+                    .and_then(|requires_python| {
+                        if let Some(target) = self.python_requirement.target() {
+                            if !target.is_compatible_with(requires_python) {
+                                return Some(IncompatibleDist::Wheel(
+                                    IncompatibleWheel::RequiresPython(
+                                        requires_python.clone(),
+                                        PythonRequirementKind::Target,
+                                    ),
+                                ));
+                            }
+                        } else {
+                            if !requires_python.contains(self.python_requirement.installed()) {
+                                return Some(IncompatibleDist::Wheel(
+                                    IncompatibleWheel::RequiresPython(
+                                        requires_python.clone(),
+                                        PythonRequirementKind::Installed,
+                                    ),
+                                ));
+                            }
+                        }
+                        None
+                    })
+            }
+        };
+
+        // The version is incompatible due to its Python requirement.
+        if let Some(incompatibility) = incompatibility {
+            return Ok(Some(ResolverVersion::Unavailable(
+                candidate.version().clone(),
+                UnavailableVersion::IncompatibleDist(incompatibility),
+            )));
+        }
 
         let filename = match dist.for_installation() {
             ResolvedDistRef::InstallableRegistrySourceDist { sdist, .. } => sdist

crates/uv-resolver/src/resolver/provider.rs
@@ -8,7 +8,6 @@ use uv_normalize::PackageName;
 use uv_types::{BuildContext, HashStrategy};
 
 use crate::flat_index::FlatIndex;
-use crate::python_requirement::PythonRequirement;
 use crate::version_map::VersionMap;
 use crate::yanks::AllowedYanks;
 use crate::ExcludeNewer;
@@ -77,7 +76,6 @@ pub struct DefaultResolverProvider<'a, Context: BuildContext> {
     /// These are the entries from `--find-links` that act as overrides for index responses.
     flat_index: FlatIndex,
     tags: Option<Tags>,
-    python_requirement: PythonRequirement,
     allowed_yanks: AllowedYanks,
     hasher: HashStrategy,
     exclude_newer: Option<ExcludeNewer>,
@@ -90,7 +88,6 @@ impl<'a, Context: BuildContext> DefaultResolverProvider<'a, Context> {
         fetcher: DistributionDatabase<'a, Context>,
         flat_index: &'a FlatIndex,
         tags: Option<&'a Tags>,
-        python_requirement: PythonRequirement,
         allowed_yanks: AllowedYanks,
         hasher: &'a HashStrategy,
         exclude_newer: Option<ExcludeNewer>,
@@ -100,7 +97,6 @@ impl<'a, Context: BuildContext> DefaultResolverProvider<'a, Context> {
             fetcher,
             flat_index: flat_index.clone(),
             tags: tags.cloned(),
-            python_requirement,
             allowed_yanks,
             hasher: hasher.clone(),
             exclude_newer,
@@ -131,7 +127,6 @@ impl<'a, Context: BuildContext> ResolverProvider for DefaultResolverProvider<'a,
                             package_name,
                             &index,
                             self.tags.as_ref(),
-                            &self.python_requirement,
                             &self.allowed_yanks,
                             &self.hasher,
                             self.exclude_newer.as_ref(),

crates/uv-resolver/src/version_map.rs
@@ -8,10 +8,9 @@ use tracing::instrument;
 use distribution_filename::{DistFilename, WheelFilename};
 use distribution_types::{
     HashComparison, IncompatibleSource, IncompatibleWheel, IndexUrl, PrioritizedDist,
-    PythonRequirementKind, RegistryBuiltWheel, RegistrySourceDist, SourceDistCompatibility,
-    WheelCompatibility,
+    RegistryBuiltWheel, RegistrySourceDist, SourceDistCompatibility, WheelCompatibility,
 };
-use pep440_rs::{Version, VersionSpecifiers};
+use pep440_rs::Version;
 use platform_tags::{TagCompatibility, Tags};
 use pypi_types::{HashDigest, Yanked};
 use uv_client::{OwnedArchive, SimpleMetadata, VersionFiles};
@@ -21,7 +20,7 @@ use uv_types::HashStrategy;
 use uv_warnings::warn_user_once;
 
 use crate::flat_index::FlatDistributions;
-use crate::{python_requirement::PythonRequirement, yanks::AllowedYanks, ExcludeNewer};
+use crate::{yanks::AllowedYanks, ExcludeNewer};
 
 /// A map from versions to distributions.
 #[derive(Debug)]
@@ -45,7 +44,6 @@ impl VersionMap {
         package_name: &PackageName,
         index: &IndexUrl,
         tags: Option<&Tags>,
-        python_requirement: &PythonRequirement,
         allowed_yanks: &AllowedYanks,
         hasher: &HashStrategy,
         exclude_newer: Option<&ExcludeNewer>,
@@ -107,7 +105,6 @@ impl VersionMap {
                 no_build: build_options.no_build_package(package_name),
                 index: index.clone(),
                 tags: tags.cloned(),
-                python_requirement: python_requirement.clone(),
                 exclude_newer: exclude_newer.copied(),
                 allowed_yanks,
                 required_hashes,
@@ -285,10 +282,6 @@ struct VersionMapLazy {
     /// The set of compatibility tags that determines whether a wheel is usable
     /// in the current environment.
     tags: Option<Tags>,
-    /// The version of Python active in the current environment. This is used
-    /// to determine whether a package's Python version constraint (if one
-    /// exists) is satisfied or not.
-    python_requirement: PythonRequirement,
     /// Whether files newer than this timestamp should be excluded or not.
     exclude_newer: Option<ExcludeNewer>,
     /// Which yanked versions are allowed
@@ -369,7 +362,6 @@ impl VersionMapLazy {
 
                 // Prioritize amongst all available files.
                 let version = filename.version().clone();
-                let requires_python = file.requires_python.clone();
                 let yanked = file.yanked.clone();
                 let hashes = file.hashes.clone();
                 match filename {
@@ -377,7 +369,6 @@ impl VersionMapLazy {
                         let compatibility = self.wheel_compatibility(
                             &filename,
                             &version,
-                            requires_python,
                             &hashes,
                             yanked,
                             excluded,
@@ -393,7 +384,6 @@ impl VersionMapLazy {
                     DistFilename::SourceDistFilename(filename) => {
                         let compatibility = self.source_dist_compatibility(
                             &version,
-                            requires_python,
                             &hashes,
                             yanked,
                             excluded,
@@ -422,7 +412,6 @@ impl VersionMapLazy {
     fn source_dist_compatibility(
         &self,
         version: &Version,
-        requires_python: Option<VersionSpecifiers>,
         hashes: &[HashDigest],
         yanked: Option<Yanked>,
         excluded: bool,
@@ -444,28 +433,6 @@ impl VersionMapLazy {
         if let Some(yanked) = yanked {
             if yanked.is_yanked() && !self.allowed_yanks.contains(version) {
                 return SourceDistCompatibility::Incompatible(IncompatibleSource::Yanked(yanked));
-            }
-        }
-
-        // Check if Python version is supported
-        // Source distributions must meet both the _target_ Python version and the
-        // _installed_ Python version (to build successfully)
-        if let Some(requires_python) = requires_python {
-            if let Some(target) = self.python_requirement.target() {
-                if !target.is_compatible_with(&requires_python) {
-                    return SourceDistCompatibility::Incompatible(
-                        IncompatibleSource::RequiresPython(
-                            requires_python,
-                            PythonRequirementKind::Target,
-                        ),
-                    );
-                }
-            }
-            if !requires_python.contains(self.python_requirement.installed()) {
-                return SourceDistCompatibility::Incompatible(IncompatibleSource::RequiresPython(
-                    requires_python,
-                    PythonRequirementKind::Installed,
-                ));
             }
         }
 
@@ -492,7 +459,6 @@ impl VersionMapLazy {
         &self,
         filename: &WheelFilename,
         version: &Version,
-        requires_python: Option<VersionSpecifiers>,
         hashes: &[HashDigest],
         yanked: Option<Yanked>,
         excluded: bool,
@@ -512,25 +478,6 @@ impl VersionMapLazy {
         if let Some(yanked) = yanked {
             if yanked.is_yanked() && !self.allowed_yanks.contains(version) {
                 return WheelCompatibility::Incompatible(IncompatibleWheel::Yanked(yanked));
-            }
-        }
-
-        // Check for a Python version incompatibility
-        if let Some(requires_python) = requires_python {
-            if let Some(target) = self.python_requirement.target() {
-                if !target.is_compatible_with(&requires_python) {
-                    return WheelCompatibility::Incompatible(IncompatibleWheel::RequiresPython(
-                        requires_python,
-                        PythonRequirementKind::Target,
-                    ));
-                }
-            } else {
-                if !requires_python.contains(self.python_requirement.installed()) {
-                    return WheelCompatibility::Incompatible(IncompatibleWheel::RequiresPython(
-                        requires_python,
-                        PythonRequirementKind::Installed,
-                    ));
-                }
             }
         }
 

commit ac3a08508461f3dabe1d762fc91978058c8230ff

Respect `requires-python` when prefetching (#4900)
## Summary

This is fallout from https://github.com/astral-sh/uv/pull/4705. We need
to respect `requires-python` in the prefetch code to avoid building
unsupported distributions.

Closes https://github.com/astral-sh/uv/issues/4898.

crates/uv-resolver/src/resolver/batch_prefetch.rs
@@ -6,13 +6,13 @@ use rustc_hash::FxHashMap;
 use tokio::sync::mpsc::Sender;
 use tracing::{debug, trace};
 
-use distribution_types::DistributionMetadata;
+use distribution_types::{CompatibleDist, DistributionMetadata};
 use pep440_rs::Version;
 
-use crate::candidate_selector::{CandidateDist, CandidateSelector};
+use crate::candidate_selector::CandidateSelector;
 use crate::pubgrub::{PubGrubPackage, PubGrubPackageInner};
 use crate::resolver::Request;
-use crate::{InMemoryIndex, ResolveError, VersionsResponse};
+use crate::{InMemoryIndex, PythonRequirement, ResolveError, VersionsResponse};
 
 enum BatchPrefetchStrategy {
     /// Go through the next versions assuming the existing selection and its constraints
@@ -49,6 +49,7 @@ impl BatchPrefetcher {
         next: &PubGrubPackage,
         version: &Version,
         current_range: &Range<Version>,
+        python_requirement: &PythonRequirement,
         request_sink: &Sender<Request>,
         index: &InMemoryIndex,
         selector: &CandidateSelector,
@@ -128,7 +129,7 @@ impl BatchPrefetcher {
                 }
             };
 
-            let CandidateDist::Compatible(dist) = candidate.dist() else {
+            let Some(dist) = candidate.compatible() else {
                 continue;
             };
 
@@ -136,6 +137,41 @@ impl BatchPrefetcher {
             if !dist.prefetchable() {
                 continue;
             }
+
+            // Avoid prefetching for distributions that don't satisfy the Python requirement.
+            match dist {
+                CompatibleDist::InstalledDist(_) => {}
+                CompatibleDist::SourceDist { sdist, .. }
+                | CompatibleDist::IncompatibleWheel { sdist, .. } => {
+                    // Source distributions must meet both the _target_ Python version and the
+                    // _installed_ Python version (to build successfully).
+                    if let Some(requires_python) = sdist.file.requires_python.as_ref() {
+                        if let Some(target) = python_requirement.target() {
+                            if !target.is_compatible_with(requires_python) {
+                                continue;
+                            }
+                        }
+                        if !requires_python.contains(python_requirement.installed()) {
+                            continue;
+                        }
+                    }
+                }
+                CompatibleDist::CompatibleWheel { wheel, .. } => {
+                    // Wheels must meet the _target_ Python version.
+                    if let Some(requires_python) = wheel.file.requires_python.as_ref() {
+                        if let Some(target) = python_requirement.target() {
+                            if !target.is_compatible_with(requires_python) {
+                                continue;
+                            }
+                        } else {
+                            if !requires_python.contains(python_requirement.installed()) {
+                                continue;
+                            }
+                        }
+                    }
+                }
+            };
+
             let dist = dist.for_resolution();
 
             // Emit a request to fetch the metadata for this version.

crates/uv-resolver/src/resolver/mod.rs
@@ -364,6 +364,7 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                     Self::pre_visit(
                         state.pubgrub.partial_solution.prioritized_packages(),
                         &self.urls,
+                        &state.python_requirement,
                         &request_sink,
                     )?;
                 }
@@ -487,6 +488,7 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                         &state.next,
                         &version,
                         term_intersection.unwrap_positive(),
+                        &state.python_requirement,
                         &request_sink,
                         &self.index,
                         &self.selector,
@@ -721,6 +723,7 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
     fn pre_visit<'data>(
         packages: impl Iterator<Item = (&'data PubGrubPackage, &'data Range<Version>)>,
         urls: &Urls,
+        python_requirement: &PythonRequirement,
         request_sink: &Sender<Request>,
     ) -> Result<(), ResolveError> {
         // Iterate over the potential packages, and fetch file metadata for any of them. These
@@ -740,7 +743,11 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
             if urls.any_url(name) {
                 continue;
             }
-            request_sink.blocking_send(Request::Prefetch(name.clone(), range.clone()))?;
+            request_sink.blocking_send(Request::Prefetch(
+                name.clone(),
+                range.clone(),
+                python_requirement.clone(),
+            ))?;
         }
         Ok(())
     }
@@ -1656,7 +1663,7 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
             }
 
             // Pre-fetch the package and distribution metadata.
-            Request::Prefetch(package_name, range) => {
+            Request::Prefetch(package_name, range, python_requirement) => {
                 // Wait for the package metadata to become available.
                 let versions_response = self
                     .index
@@ -1719,6 +1726,39 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                         }
                     }
                 }
+
+                match dist {
+                    CompatibleDist::InstalledDist(_) => {}
+                    CompatibleDist::SourceDist { sdist, .. }
+                    | CompatibleDist::IncompatibleWheel { sdist, .. } => {
+                        // Source distributions must meet both the _target_ Python version and the
+                        // _installed_ Python version (to build successfully).
+                        if let Some(requires_python) = sdist.file.requires_python.as_ref() {
+                            if let Some(target) = python_requirement.target() {
+                                if !target.is_compatible_with(requires_python) {
+                                    return Ok(None);
+                                }
+                            }
+                            if !requires_python.contains(python_requirement.installed()) {
+                                return Ok(None);
+                            }
+                        }
+                    }
+                    CompatibleDist::CompatibleWheel { wheel, .. } => {
+                        // Wheels must meet the _target_ Python version.
+                        if let Some(requires_python) = wheel.file.requires_python.as_ref() {
+                            if let Some(target) = python_requirement.target() {
+                                if !target.is_compatible_with(requires_python) {
+                                    return Ok(None);
+                                }
+                            } else {
+                                if !requires_python.contains(python_requirement.installed()) {
+                                    return Ok(None);
+                                }
+                            }
+                        }
+                    }
+                };
 
                 // Emit a request to fetch the metadata for this version.
                 if self.index.distributions().register(candidate.version_id()) {
@@ -2213,7 +2253,7 @@ pub(crate) enum Request {
     /// A request to fetch the metadata from an already-installed distribution.
     Installed(InstalledDist),
     /// A request to pre-fetch the metadata for a package and the best-guess distribution.
-    Prefetch(PackageName, Range<Version>),
+    Prefetch(PackageName, Range<Version>, PythonRequirement),
 }
 
 impl<'a> From<ResolvedDistRef<'a>> for Request {
@@ -2265,7 +2305,7 @@ impl Display for Request {
             Self::Installed(dist) => {
                 write!(f, "Installed metadata {dist}")
             }
-            Self::Prefetch(package_name, range) => {
+            Self::Prefetch(package_name, range, _) => {
                 write!(f, "Prefetch {package_name} {range}")
             }
         }

crates/uv/tests/pip_compile.rs
@@ -4544,6 +4544,35 @@ coverage = ["example[test]", "extras>=0.0.1,<=0.0.2"]
     Ok(())
 }
 
+/// Respect `requires-python` when prefetching.
+///
+/// `voluptuous==0.15.1` requires Python 3.9 or later, so we should resolve to an earlier version
+/// and avoiding building 0.15.1 at all.
+#[test]
+fn requires_python_prefetch() -> Result<()> {
+    let context = TestContext::new("3.8");
+    let requirements_in = context.temp_dir.child("requirements.in");
+    requirements_in.write_str("voluptuous<=0.15.1")?;
+
+    uv_snapshot!(context
+        .pip_compile()
+        .env_remove("UV_EXCLUDE_NEWER")
+        .arg("requirements.in"), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    # This file was autogenerated by uv via the following command:
+    #    uv pip compile --cache-dir [CACHE_DIR] requirements.in
+    voluptuous==0.14.2
+        # via -r requirements.in
+
+    ----- stderr -----
+    Resolved 1 package in [TIME]
+    "###);
+
+    Ok(())
+}
+
 /// Use an existing resolution for `black==23.10.1`, with stale versions of `click` and `pathspec`.
 /// Nothing should change.
 #[test]