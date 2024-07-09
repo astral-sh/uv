55e1a7e01186f5c918325bd1b3c47f476d49c825
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-07-09 12:26:17 -0700

    Enable `--all` to uninstall all managed tools (#4937)
    ## Summary
    
    Like #4932 but for tools.

--- crates/uv/src/commands/tool/uninstall.rs
+++ crates/uv/src/commands/tool/uninstall.rs
@@ -8,7 +8,7 @@ use tracing::debug;
 use uv_configuration::PreviewMode;
 use uv_fs::Simplified;
 use uv_normalize::PackageName;
-use uv_tool::InstalledTools;
+use uv_tool::{InstalledTools, Tool, ToolEntrypoint};
 use uv_warnings::warn_user_once;
 
 use crate::commands::ExitStatus;
@@ -16,7 +16,7 @@ use crate::printer::Printer;
 
 /// Uninstall a tool.
 pub(crate) async fn uninstall(
-    name: PackageName,
+    name: Option<PackageName>,
     preview: PreviewMode,
     printer: Printer,
 ) -> Result<ExitStatus> {
@@ -25,27 +25,64 @@ pub(crate) async fn uninstall(
     }
 
     let installed_tools = InstalledTools::from_settings()?;
-    let Some(receipt) = installed_tools.get_tool_receipt(&name)? else {
-        // If the tool is not installed, attempt to remove the environment anyway.
-        match installed_tools.remove_environment(&name) {
-            Ok(()) => {
-                writeln!(
-                    printer.stderr(),
-                    "Removed dangling environment for `{name}`"
-                )?;
-                return Ok(ExitStatus::Success);
-            }
-            Err(uv_tool::Error::IO(err)) if err.kind() == std::io::ErrorKind::NotFound => {
-                bail!("`{name}` is not installed");
-            }
-            Err(err) => {
-                return Err(err.into());
-            }
-        }
-    };
 
+    let mut entrypoints = if let Some(name) = name {
+        let Some(receipt) = installed_tools.get_tool_receipt(&name)? else {
+            // If the tool is not installed, attempt to remove the environment anyway.
+            match installed_tools.remove_environment(&name) {
+                Ok(()) => {
+                    writeln!(
+                        printer.stderr(),
+                        "Removed dangling environment for `{name}`"
+                    )?;
+                    return Ok(ExitStatus::Success);
+                }
+                Err(uv_tool::Error::IO(err)) if err.kind() == std::io::ErrorKind::NotFound => {
+                    bail!("`{name}` is not installed");
+                }
+                Err(err) => {
+                    return Err(err.into());
+                }
+            }
+        };
+
+        uninstall_tool(&name, &receipt, &installed_tools).await?
+    } else {
+        let mut entrypoints = vec![];
+        for (name, receipt) in installed_tools.tools()? {
+            entrypoints.extend(uninstall_tool(&name, &receipt, &installed_tools).await?);
+        }
+        entrypoints
+    };
+    entrypoints.sort_unstable_by(|a, b| a.name.cmp(&b.name));
+
+    if entrypoints.is_empty() {
+        writeln!(printer.stderr(), "Nothing to uninstall")?;
+        return Ok(ExitStatus::Success);
+    }
+
+    let s = if entrypoints.len() == 1 { "" } else { "s" };
+    writeln!(
+        printer.stderr(),
+        "Uninstalled {} executable{s}: {}",
+        entrypoints.len(),
+        entrypoints
+            .iter()
+            .map(|entrypoint| entrypoint.name.bold())
+            .join(", ")
+    )?;
+
+    Ok(ExitStatus::Success)
+}
+
+/// Uninstall a tool.
+async fn uninstall_tool(
+    name: &PackageName,
+    receipt: &Tool,
+    tools: &InstalledTools,
+) -> Result<Vec<ToolEntrypoint>> {
     // Remove the tool itself.
-    installed_tools.remove_environment(&name)?;
+    tools.remove_environment(name)?;
 
     // Remove the tool's entrypoints.
     let entrypoints = receipt.entrypoints();
@@ -68,16 +105,5 @@ pub(crate) async fn uninstall(
         }
     }
 
-    let s = if entrypoints.len() == 1 { "" } else { "s" };
-    writeln!(
-        printer.stderr(),
-        "Uninstalled {} executable{s}: {}",
-        entrypoints.len(),
-        entrypoints
-            .iter()
-            .map(|entrypoint| entrypoint.name.bold())
-            .join(", ")
-    )?;
-
-    Ok(ExitStatus::Success)
+    Ok(entrypoints.to_vec())
 }
