137a179c6acc99f5214fceb61640c85584a0e7f7
Zanie Blue <contact@zanie.dev>
2024-07-09 18:24:52 -0500

    Add `uv python pin`

--- crates/uv/src/commands/mod.rs
+++ crates/uv/src/commands/mod.rs
@@ -28,6 +28,7 @@ pub(crate) use python::dir::dir as python_dir;
 pub(crate) use python::find::find as python_find;
 pub(crate) use python::install::install as python_install;
 pub(crate) use python::list::list as python_list;
+pub(crate) use python::pin::pin as python_pin;
 pub(crate) use python::uninstall::uninstall as python_uninstall;
 #[cfg(feature = "self-update")]
 pub(crate) use self_update::self_update;
--- crates/uv/src/commands/python/mod.rs
+++ crates/uv/src/commands/python/mod.rs
@@ -2,4 +2,5 @@ pub(crate) mod dir;
 pub(crate) mod find;
 pub(crate) mod install;
 pub(crate) mod list;
+pub(crate) mod pin;
 pub(crate) mod uninstall;
