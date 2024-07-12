9fc4448009d482cde796152dc2f26c3883844269
Silvano Cerza <silvanocerza@gmail.com>
2024-07-12 17:14:09 +0200

    Comment pager test to explain it can't actually test properly

--- crates/uv/tests/help.rs
+++ crates/uv/tests/help.rs
@@ -668,6 +668,8 @@ fn help_with_version() {
 fn test_with_no_pager() {
     let context = TestContext::new_with_versions(&[]);
 
+    // We can't really test wether the --no-pager option works with a snapshot test.
+    // It's still nice to have a test for the option to confirm the option exists.
     uv_snapshot!(context.filters(), context.help().arg("--no-pager"), @r###"
     success: true
     exit_code: 0
