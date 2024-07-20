73ae7a26704c467d5d06197c84237c3ccf6a7f52
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-07-19 21:38:26 -0400

    Avoid URL parsing when deserializing wheels

--- crates/pep508-rs/src/lib.rs
+++ crates/pep508-rs/src/lib.rs
@@ -33,8 +33,6 @@ use pyo3::{
     create_exception, exceptions::PyNotImplementedError, pyclass, pyclass::CompareOp, pymethods,
     pymodule, types::PyModule, IntoPy, PyObject, PyResult, Python,
 };
-use schemars::gen::SchemaGenerator;
-use schemars::schema::Schema;
 use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 use thiserror::Error;
 use url::Url;
@@ -497,7 +495,7 @@ impl<T: Pep508Url> schemars::JsonSchema for Requirement<T> {
         "Requirement".to_string()
     }
 
-    fn json_schema(_gen: &mut SchemaGenerator) -> Schema {
+    fn json_schema(_gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
         schemars::schema::SchemaObject {
             instance_type: Some(schemars::schema::InstanceType::String.into()),
             metadata: Some(Box::new(schemars::schema::Metadata {
