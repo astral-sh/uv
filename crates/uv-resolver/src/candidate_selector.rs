829c2cb420bbcdfb2a580dae274ef8669e76006a
Yorick van Pelt <yorick@yorickvanpelt.nl>
2024-07-22 16:02:28 +0200

    If multiple indices contain the same version, use the first index
    This fixes resolving packages that publish an invalid stub to pypi,
    such as tensorrt-llm.

--- crates/uv-resolver/src/candidate_selector.rs
+++ crates/uv-resolver/src/candidate_selector.rs
@@ -232,8 +232,19 @@ impl CandidateSelector {
                 Self::select_candidate(
                     version_maps
                         .iter()
-                        .map(|version_map| version_map.iter().rev())
-                        .kmerge_by(|(version1, _), (version2, _)| version1 > version2),
+                        .enumerate()
+                        .map(|(map_index, version_map)| {
+                            version_map.iter().rev().map(move |item| (map_index, item))
+                        })
+                        .kmerge_by(
+                            |(index1, (version1, _)), (index2, (version2, _))| match version2
+                                .cmp(version1)
+                            {
+                                std::cmp::Ordering::Equal => index1 < index2,
+                                _ => version1 > version2,
+                            },
+                        )
+                        .map(|(_, item)| item),
                     package_name,
                     range,
                     allow_prerelease,
@@ -242,8 +253,19 @@ impl CandidateSelector {
                 Self::select_candidate(
                     version_maps
                         .iter()
-                        .map(VersionMap::iter)
-                        .kmerge_by(|(version1, _), (version2, _)| version1 < version2),
+                        .enumerate()
+                        .map(|(map_index, version_map)| {
+                            version_map.iter().map(move |item| (map_index, item))
+                        })
+                        .kmerge_by(
+                            |(index1, (version1, _)), (index2, (version2, _))| match version2
+                                .cmp(version1)
+                            {
+                                std::cmp::Ordering::Equal => index1 < index2,
+                                _ => version1 < version2,
+                            },
+                        )
+                        .map(|(_, item)| item),
                     package_name,
                     range,
                     allow_prerelease,
