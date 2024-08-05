92a2996e8e76b8e2fd0d9daf66690ef7f8099d60
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-08-05 15:15:02 -0400

    Show extras and dev dependencies in `uv tree` (#5768)
    ## Summary
    
    Closes https://github.com/astral-sh/uv/issues/5759.

--- crates/uv/tests/tree.rs
+++ crates/uv/tests/tree.rs
@@ -406,17 +406,175 @@ fn dev_dependencies() -> Result<()> {
     "#,
     )?;
 
-    // Dev dependencies should be omitted.
     uv_snapshot!(context.filters(), context.tree().arg("--universal"), @r###"
     success: true
     exit_code: 0
     ----- stdout -----
     project v0.1.0
-    └── iniconfig v2.0.0
+    ├── iniconfig v2.0.0
+    └── anyio v4.3.0 (group: dev)
+        ├── idna v3.6
+        └── sniffio v1.3.1
 
     ----- stderr -----
     warning: `uv tree` is experimental and may change without warning
     Resolved 5 packages in [TIME]
+    "###
+    );
+
+    // `uv tree` should update the lockfile
+    let lock = fs_err::read_to_string(context.temp_dir.join("uv.lock"))?;
+    assert!(!lock.is_empty());
+
+    Ok(())
+}
+
+#[test]
+fn dev_dependencies_inverted() -> Result<()> {
+    let context = TestContext::new("3.12");
+
+    let pyproject_toml = context.temp_dir.child("pyproject.toml");
+    pyproject_toml.write_str(
+        r#"
+        [project]
+        name = "project"
+        version = "0.1.0"
+        # ...
+        requires-python = ">=3.12"
+        dependencies = ["iniconfig"]
+        [tool.uv]
+        dev-dependencies = ["anyio"]
+    "#,
+    )?;
+
+    uv_snapshot!(context.filters(), context.tree().arg("--universal").arg("--invert"), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    idna v3.6
+    └── anyio v4.3.0
+        └── project v0.1.0 (group: dev)
+    iniconfig v2.0.0
+    └── project v0.1.0
+    sniffio v1.3.1
+    └── anyio v4.3.0 (*)
+    (*) Package tree already displayed
+
+    ----- stderr -----
+    warning: `uv tree` is experimental and may change without warning
+    Resolved 5 packages in [TIME]
+    "###
+    );
+
+    // `uv tree` should update the lockfile
+    let lock = fs_err::read_to_string(context.temp_dir.join("uv.lock"))?;
+    assert!(!lock.is_empty());
+
+    Ok(())
+}
+
+#[test]
+fn optional_dependencies() -> Result<()> {
+    let context = TestContext::new("3.12");
+
+    let pyproject_toml = context.temp_dir.child("pyproject.toml");
+    pyproject_toml.write_str(
+        r#"
+        [project]
+        name = "project"
+        version = "0.1.0"
+        # ...
+        requires-python = ">=3.12"
+        dependencies = ["iniconfig", "flask[dotenv]"]
+
+        [project.optional-dependencies]
+        async = ["anyio"]
+    "#,
+    )?;
+
+    uv_snapshot!(context.filters(), context.tree().arg("--universal"), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    project v0.1.0
+    ├── flask[dotenv] v3.0.2
+    │   ├── blinker v1.7.0
+    │   ├── click v8.1.7
+    │   │   └── colorama v0.4.6
+    │   ├── itsdangerous v2.1.2
+    │   ├── jinja2 v3.1.3
+    │   │   └── markupsafe v2.1.5
+    │   ├── werkzeug v3.0.1
+    │   │   └── markupsafe v2.1.5
+    │   └── python-dotenv v1.0.1 (extra: dotenv)
+    ├── iniconfig v2.0.0
+    └── anyio v4.3.0 (extra: async)
+        ├── idna v3.6
+        └── sniffio v1.3.1
+
+    ----- stderr -----
+    warning: `uv tree` is experimental and may change without warning
+    Resolved 14 packages in [TIME]
+    "###
+    );
+
+    // `uv tree` should update the lockfile
+    let lock = fs_err::read_to_string(context.temp_dir.join("uv.lock"))?;
+    assert!(!lock.is_empty());
+
+    Ok(())
+}
+
+#[test]
+fn optional_dependencies_inverted() -> Result<()> {
+    let context = TestContext::new("3.12");
+
+    let pyproject_toml = context.temp_dir.child("pyproject.toml");
+    pyproject_toml.write_str(
+        r#"
+        [project]
+        name = "project"
+        version = "0.1.0"
+        # ...
+        requires-python = ">=3.12"
+        dependencies = ["iniconfig", "flask[dotenv]"]
+
+        [project.optional-dependencies]
+        async = ["anyio"]
+    "#,
+    )?;
+
+    uv_snapshot!(context.filters(), context.tree().arg("--universal").arg("--invert"), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    blinker v1.7.0
+    └── flask v3.0.2
+        └── project[dotenv] v0.1.0
+    colorama v0.4.6
+    └── click v8.1.7
+        └── flask v3.0.2 (*)
+    idna v3.6
+    └── anyio v4.3.0
+        └── project v0.1.0 (extra: async)
+    iniconfig v2.0.0
+    └── project v0.1.0
+    itsdangerous v2.1.2
+    └── flask v3.0.2 (*)
+    markupsafe v2.1.5
+    ├── jinja2 v3.1.3
+    │   └── flask v3.0.2 (*)
+    └── werkzeug v3.0.1
+        └── flask v3.0.2 (*)
+    python-dotenv v1.0.1
+    └── flask v3.0.2 (extra: dotenv) (*)
+    sniffio v1.3.1
+    └── anyio v4.3.0 (*)
+    (*) Package tree already displayed
+
+    ----- stderr -----
+    warning: `uv tree` is experimental and may change without warning
+    Resolved 14 packages in [TIME]
     "###
     );
 
