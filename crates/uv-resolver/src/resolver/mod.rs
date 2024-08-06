astral-sh/uv

commit bb73edb03bfc90a32440bcf8c8ffc9ac125cb37a

Respect local versions for all user requirements (#5232)
## Summary

This fixes a few bugs introduced by
https://github.com/astral-sh/uv/pull/5104. I previously thought we could
track conflicting locals the same way we track conflicting URLs in
forks, but it turns out that ends up being very tricky. URL forks work
because we prioritize directly URL requirements. We can't prioritize
locals in the same way without conflicting with the URL prioritization
(this may be possible but it's not trivial), so we run into issues where
a correct resolution depends on the order in which dependencies are
traversed.

Instead, we track local versions across all forks in `Locals`. When
applying a local version, we apply all locals with markers that
intersect with the current fork. This way we end up applying some local
versions without creating a fork. For example, given:
```
// pyproject.toml
dependencies = [
    "torch==2.0.0+cu118 ; platform_machine == 'x86_64'",
]

// requirements.in
torch==2.0.0
.
```

We choose `2.0.0+cu118` in all cases. However, if a disjoint fork is
created based on local versions, the resolver will choose the most
compatible local when it narrows to a specific fork. Thus we correctly
respect local versions when forking:
```
// pyproject.toml
dependencies = [
    "torch==2.0.0+cu118 ; platform_machine == 'x86_64'",
    "torch==2.0.0+cpu ; platform_machine != 'x86_64'"
]

// requirements.in
torch==2.0.0
.
``` 

We should also be able to use a similar strategy for
https://github.com/astral-sh/uv/pull/5150.

## Test Plan

This fixes https://github.com/astral-sh/uv/issues/5220 locally for me,
as well as a few other bugs that were not reported yet.

crates/uv-resolver/src/resolver/mod.rs
@@ -26,7 +26,7 @@ use distribution_types::{
     IncompatibleWheel, IndexLocations, InstalledDist, PythonRequirementKind, RemoteSource,
     ResolvedDist, ResolvedDistRef, SourceDist, VersionOrUrlRef,
 };
-pub(crate) use locals::ForkLocals;
+pub(crate) use locals::Locals;
 use pep440_rs::{Version, MIN_VERSION};
 use pep508_rs::MarkerTree;
 use platform_tags::Tags;
@@ -96,6 +96,7 @@ struct ResolverState<InstalledPackages: InstalledPackagesProvider> {
     git: GitResolver,
     exclusions: Exclusions,
     urls: Urls,
+    locals: Locals,
     dependency_mode: DependencyMode,
     hasher: HashStrategy,
     markers: ResolverMarkers,
@@ -215,6 +216,11 @@ impl<Provider: ResolverProvider, InstalledPackages: InstalledPackagesProvider>
                 git,
                 options.dependency_mode,
             )?,
+            locals: Locals::from_manifest(
+                &manifest,
+                markers.marker_environment(),
+                options.dependency_mode,
+            ),
             project: manifest.project,
             requirements: manifest.requirements,
             constraints: manifest.constraints,
@@ -309,7 +315,6 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
             next: root,
             pins: FilePins::default(),
             fork_urls: ForkUrls::default(),
-            fork_locals: ForkLocals::default(),
             priorities: PubGrubPriorities::default(),
             added_dependencies: FxHashMap::default(),
             markers: self.markers.clone(),
@@ -492,7 +497,6 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                         &state.next,
                         &version,
                         &state.fork_urls,
-                        &state.fork_locals,
                         &state.markers,
                         state.requires_python.as_ref(),
                     )?;
@@ -512,17 +516,19 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                                 for_package.as_deref(),
                                 &version,
                                 &self.urls,
+                                &self.locals,
                                 dependencies.clone(),
                                 &self.git,
                                 self.selector.resolution_strategy(),
                             )?;
+
                             // Emit a request to fetch the metadata for each registry package.
                             for dependency in &dependencies {
                                 let PubGrubDependency {
                                     package,
                                     version: _,
+                                    specifier: _,
                                     url: _,
-                                    local: _,
                                 } = dependency;
                                 let url = package.name().and_then(|name| state.fork_urls.get(name));
                                 self.visit_package(package, url, &request_sink)?;
@@ -581,11 +587,11 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                                 }
 
                                 forked_state.markers = ResolverMarkers::Fork(combined_markers);
-
                                 forked_state.add_package_version_dependencies(
                                     for_package.as_deref(),
                                     &version,
                                     &self.urls,
+                                    &self.locals,
                                     fork.dependencies.clone(),
                                     &self.git,
                                     self.selector.resolution_strategy(),
@@ -595,8 +601,8 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                                     let PubGrubDependency {
                                         package,
                                         version: _,
+                                        specifier: _,
                                         url: _,
-                                        local: _,
                                     } = dependency;
                                     let url = package
                                         .name()
@@ -1100,18 +1106,10 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
         package: &PubGrubPackage,
         version: &Version,
         fork_urls: &ForkUrls,
-        fork_locals: &ForkLocals,
         markers: &ResolverMarkers,
         requires_python: Option<&MarkerTree>,
     ) -> Result<ForkedDependencies, ResolveError> {
-        let result = self.get_dependencies(
-            package,
-            version,
-            fork_urls,
-            fork_locals,
-            markers,
-            requires_python,
-        );
+        let result = self.get_dependencies(package, version, fork_urls, markers, requires_python);
         match markers {
             ResolverMarkers::SpecificEnvironment(_) => result.map(|deps| match deps {
                 Dependencies::Available(deps) => ForkedDependencies::Unforked(deps),
@@ -1128,7 +1126,6 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
         package: &PubGrubPackage,
         version: &Version,
         fork_urls: &ForkUrls,
-        fork_locals: &ForkLocals,
         markers: &ResolverMarkers,
         requires_python: Option<&MarkerTree>,
     ) -> Result<Dependencies, ResolveError> {
@@ -1148,14 +1145,7 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
 
                 requirements
                     .iter()
-                    .flat_map(|requirement| {
-                        PubGrubDependency::from_requirement(requirement, None, fork_locals)
-                            // Keep track of local versions to propagate to transitive dependencies.
-                            .map_ok(|dependency| PubGrubDependency {
-                                local: locals::from_source(&requirement.source),
-                                ..dependency
-                            })
-                    })
+                    .flat_map(|requirement| PubGrubDependency::from_requirement(requirement, None))
                     .collect::<Result<Vec<_>, _>>()?
             }
             PubGrubPackageInner::Package {
@@ -1283,7 +1273,7 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                 let mut dependencies = requirements
                     .iter()
                     .flat_map(|requirement| {
-                        PubGrubDependency::from_requirement(requirement, Some(name), fork_locals)
+                        PubGrubDependency::from_requirement(requirement, Some(name))
                     })
                     .collect::<Result<Vec<_>, _>>()?;
 
@@ -1302,8 +1292,8 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                                 marker: marker.clone(),
                             }),
                             version: Range::singleton(version.clone()),
+                            specifier: None,
                             url: None,
-                            local: None,
                         });
                     }
                 }
@@ -1325,8 +1315,8 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                                 marker: marker.cloned(),
                             }),
                             version: Range::singleton(version.clone()),
+                            specifier: None,
                             url: None,
-                            local: None,
                         })
                         .collect(),
                 ))
@@ -1353,8 +1343,8 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                                         marker: marker.cloned(),
                                     }),
                                     version: Range::singleton(version.clone()),
+                                    specifier: None,
                                     url: None,
-                                    local: None,
                                 })
                         })
                         .collect(),
@@ -1379,8 +1369,8 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
                                         marker: marker.cloned(),
                                     }),
                                     version: Range::singleton(version.clone()),
+                                    specifier: None,
                                     url: None,
-                                    local: None,
                                 })
                         })
                         .collect(),
@@ -1983,8 +1973,6 @@ struct ForkState {
     /// one URL per package. By prioritizing direct URL dependencies over registry dependencies,
     /// this map is populated for all direct URL packages before we look at any registry packages.
     fork_urls: ForkUrls,
-    /// The local versions required by packages in this fork.
-    fork_locals: ForkLocals,
     /// When dependencies for a package are retrieved, this map of priorities
     /// is updated based on how each dependency was specified. Certain types
     /// of dependencies have more "priority" than others (like direct URL
@@ -2034,16 +2022,17 @@ impl ForkState {
         for_package: Option<&str>,
         version: &Version,
         urls: &Urls,
-        dependencies: Vec<PubGrubDependency>,
+        locals: &Locals,
+        mut dependencies: Vec<PubGrubDependency>,
         git: &GitResolver,
         resolution_strategy: &ResolutionStrategy,
     ) -> Result<(), ResolveError> {
-        for dependency in &dependencies {
+        for dependency in &mut dependencies {
             let PubGrubDependency {
                 package,
                 version,
+                specifier,
                 url,
-                local,
             } = dependency;
 
             let mut has_url = false;
@@ -2057,11 +2046,36 @@ impl ForkState {
                     has_url = true;
                 };
 
-                // `PubGrubDependency` also gives us a local version if specified by the user.
-                // Keep track of which local version we will be using in this fork for transitive
-                // dependencies.
-                if let Some(local) = local {
-                    self.fork_locals.insert(name.clone(), local.clone());
+                // If the specifier is an exact version and the user requested a local version for this
+                // fork that's more precise than the specifier, use the local version instead.
+                if let Some(specifier) = specifier {
+                    let locals = locals.get(name, &self.markers);
+
+                    // Prioritize local versions over the original version range.
+                    if !locals.is_empty() {
+                        *version = Range::empty();
+                    }
+
+                    // It's possible that there are multiple matching local versions requested with
+                    // different marker expressions. All of these are potentially compatible until we
+                    // narrow to a specific fork.
+                    for local in locals {
+                        let local = specifier
+                            .iter()
+                            .map(|specifier| {
+                                Locals::map(local, specifier)
+                                    .map_err(ResolveError::InvalidVersion)
+                                    .and_then(|specifier| {
+                                        Ok(PubGrubSpecifier::from_pep440_specifier(&specifier)?)
+                                    })
+                            })
+                            .fold_ok(Range::full(), |range, specifier| {
+                                range.intersection(&specifier.into())
+                            })?;
+
+                        // Add the local version.
+                        *version = version.union(&local);
+                    }
                 }
             }
 
@@ -2096,8 +2110,8 @@ impl ForkState {
                 let PubGrubDependency {
                     package,
                     version,
+                    specifier: _,
                     url: _,
-                    local: _,
                 } = dependency;
                 (package, version)
             }),

crates/uv-resolver/src/pubgrub/dependencies.rs
@@ -12,37 +12,37 @@ use pypi_types::{
 use uv_normalize::{ExtraName, PackageName};
 
 use crate::pubgrub::{PubGrubPackage, PubGrubPackageInner};
-use crate::resolver::ForkLocals;
 use crate::{PubGrubSpecifier, ResolveError};
 
 #[derive(Clone, Debug)]
 pub(crate) struct PubGrubDependency {
     pub(crate) package: PubGrubPackage,
     pub(crate) version: Range<Version>,
+
+    /// The original version specifiers from the requirement.
+    pub(crate) specifier: Option<VersionSpecifiers>,
+
     /// This field is set if the [`Requirement`] had a URL. We still use a URL from [`Urls`]
     /// even if this field is None where there is an override with a URL or there is a different
     /// requirement or constraint for the same package that has a URL.
     pub(crate) url: Option<VerbatimParsedUrl>,
-    /// The local version for this requirement, if specified.
-    pub(crate) local: Option<Version>,
 }
 
 impl PubGrubDependency {
     pub(crate) fn from_requirement<'a>(
         requirement: &'a Requirement,
         source_name: Option<&'a PackageName>,
-        fork_locals: &'a ForkLocals,
     ) -> impl Iterator<Item = Result<Self, ResolveError>> + 'a {
         // Add the package, plus any extra variants.
         iter::once(None)
             .chain(requirement.extras.clone().into_iter().map(Some))
-            .map(|extra| PubGrubRequirement::from_requirement(requirement, extra, fork_locals))
+            .map(|extra| PubGrubRequirement::from_requirement(requirement, extra))
             .filter_map_ok(move |requirement| {
                 let PubGrubRequirement {
                     package,
                     version,
+                    specifier,
                     url,
-                    local,
                 } = requirement;
                 match &*package {
                     PubGrubPackageInner::Package { name, .. } => {
@@ -55,15 +55,15 @@ impl PubGrubDependency {
                         Some(PubGrubDependency {
                             package: package.clone(),
                             version: version.clone(),
+                            specifier,
                             url,
-                            local,
                         })
                     }
                     PubGrubPackageInner::Marker { .. } => Some(PubGrubDependency {
                         package: package.clone(),
                         version: version.clone(),
+                        specifier,
                         url,
-                        local,
                     }),
                     PubGrubPackageInner::Extra { name, .. } => {
                         debug_assert!(
@@ -73,8 +73,8 @@ impl PubGrubDependency {
                         Some(PubGrubDependency {
                             package: package.clone(),
                             version: version.clone(),
+                            specifier,
                             url: None,
-                            local: None,
                         })
                     }
                     _ => None,
@@ -88,8 +88,8 @@ impl PubGrubDependency {
 pub(crate) struct PubGrubRequirement {
     pub(crate) package: PubGrubPackage,
     pub(crate) version: Range<Version>,
+    pub(crate) specifier: Option<VersionSpecifiers>,
     pub(crate) url: Option<VerbatimParsedUrl>,
-    pub(crate) local: Option<Version>,
 }
 
 impl PubGrubRequirement {
@@ -98,11 +98,10 @@ impl PubGrubRequirement {
     pub(crate) fn from_requirement(
         requirement: &Requirement,
         extra: Option<ExtraName>,
-        fork_locals: &ForkLocals,
     ) -> Result<Self, ResolveError> {
         let (verbatim_url, parsed_url) = match &requirement.source {
             RequirementSource::Registry { specifier, .. } => {
-                return Self::from_registry_requirement(specifier, extra, requirement, fork_locals);
+                return Self::from_registry_requirement(specifier, extra, requirement);
             }
             RequirementSource::Url {
                 subdirectory,
@@ -165,11 +164,11 @@ impl PubGrubRequirement {
                 requirement.marker.clone(),
             ),
             version: Range::full(),
+            specifier: None,
             url: Some(VerbatimParsedUrl {
                 parsed_url,
                 verbatim: verbatim_url.clone(),
             }),
-            local: None,
         })
     }
 
@@ -177,26 +176,8 @@ impl PubGrubRequirement {
         specifier: &VersionSpecifiers,
         extra: Option<ExtraName>,
         requirement: &Requirement,
-        fork_locals: &ForkLocals,
     ) -> Result<PubGrubRequirement, ResolveError> {
-        // If the specifier is an exact version and the user requested a local version for this
-        // fork that's more precise than the specifier, use the local version instead.
-        let version = if let Some(local) = fork_locals.get(&requirement.name) {
-            specifier
-                .iter()
-                .map(|specifier| {
-                    ForkLocals::map(local, specifier)
-                        .map_err(ResolveError::InvalidVersion)
-                        .and_then(|specifier| {
-                            Ok(PubGrubSpecifier::from_pep440_specifier(&specifier)?)
-                        })
-                })
-                .fold_ok(Range::full(), |range, specifier| {
-                    range.intersection(&specifier.into())
-                })?
-        } else {
-            PubGrubSpecifier::from_pep440_specifiers(specifier)?.into()
-        };
+        let version = PubGrubSpecifier::from_pep440_specifiers(specifier)?.into();
 
         let requirement = Self {
             package: PubGrubPackage::from_package(
@@ -204,9 +185,9 @@ impl PubGrubRequirement {
                 extra,
                 requirement.marker.clone(),
             ),
-            version,
+            specifier: Some(specifier.clone()),
             url: None,
-            local: None,
+            version,
         };
 
         Ok(requirement)

commit e651e67f295a4503a0162f1a24b56fe827a2e79d

Support overlapping local and non-local requirements in forks (#5812)
## Summary

This fixes a bug introduced by
https://github.com/astral-sh/uv/pull/5232. It turns out that the
`universal_disjoint_base_or_local_requirement` test does not actually do
what it was meant to because of the incorrect python requirement. With a
valid python requirement, it fails on `main`. The problem is that we try
to exclude the original base version from the range of allowed versions
to try and prefer local versions. However, in the test, there is a
branch that depends on the non-local version, with no applicable local
in its fork. We should remove this exclusion as prioritization is
handled by the candidate resolver.

crates/uv-resolver/src/resolver/mod.rs
@@ -2117,11 +2117,6 @@ impl ForkState {
                 if let Some(specifier) = specifier {
                     let locals = locals.get(name, &self.markers);
 
-                    // Prioritize local versions over the original version range.
-                    if !locals.is_empty() {
-                        *version = Range::empty();
-                    }
-
                     // It's possible that there are multiple matching local versions requested with
                     // different marker expressions. All of these are potentially compatible until we
                     // narrow to a specific fork.

crates/uv/tests/pip_compile.rs
@@ -7170,7 +7170,7 @@ fn universal_disjoint_local_requirement() -> Result<()> {
 /// expressions, we should fork the root requirement.
 #[test]
 fn universal_disjoint_base_or_local_requirement() -> Result<()> {
-    let context = TestContext::new("3.12");
+    let context = TestContext::new("3.10");
 
     let pyproject_toml = context.temp_dir.child("pyproject.toml");
     pyproject_toml.write_str(indoc! {r#"
@@ -7178,11 +7178,11 @@ fn universal_disjoint_base_or_local_requirement() -> Result<()> {
         name = "example"
         version = "0.0.0"
         dependencies = [
-            "torch==2.0.0; python_version < '3.10'",
-            "torch==2.0.0+cu118 ; python_version >= '3.10' and python_version <= '3.12'",
+            "torch==2.0.0; python_version < '3.11'",
+            "torch==2.0.0+cu118 ; python_version >= '3.11' and python_version <= '3.12'",
             "torch==2.0.0+cpu ; python_version > '3.12'"
         ]
-        requires-python = ">=3.11"
+        requires-python = ">=3.10"
     "#})?;
 
     let requirements_in = context.temp_dir.child("requirements.in");
@@ -7203,42 +7203,46 @@ fn universal_disjoint_base_or_local_requirement() -> Result<()> {
     ----- stdout -----
     # This file was autogenerated by uv via the following command:
     #    uv pip compile --cache-dir [CACHE_DIR] requirements.in --universal
-    cmake==3.28.4 ; python_version <= '3.12' and platform_machine == 'x86_64' and platform_system == 'Linux'
+    cmake==3.28.4 ; python_version <= '3.12' and python_version >= '3.11' and platform_machine == 'x86_64' and platform_system == 'Linux'
         # via triton
-    .
+    . ; python_version < '3.11' or python_version > '3.12' or (python_version <= '3.12' and python_version >= '3.11')
         # via -r requirements.in
-    filelock==3.13.1
+    filelock==3.13.1 ; python_version < '3.11' or python_version > '3.12' or (python_version <= '3.12' and python_version >= '3.11') or (python_version <= '3.12' and python_version >= '3.11' and platform_machine == 'x86_64' and platform_system == 'Linux')
         # via
         #   torch
         #   triton
-    jinja2==3.1.3
+    jinja2==3.1.3 ; python_version < '3.11' or python_version > '3.12' or (python_version <= '3.12' and python_version >= '3.11')
         # via torch
-    lit==18.1.2 ; python_version <= '3.12' and platform_machine == 'x86_64' and platform_system == 'Linux'
+    lit==18.1.2 ; python_version <= '3.12' and python_version >= '3.11' and platform_machine == 'x86_64' and platform_system == 'Linux'
         # via triton
-    markupsafe==2.1.5
+    markupsafe==2.1.5 ; (python_version < '3.11' or python_version > '3.12' or (python_version <= '3.12' and python_version >= '3.11')) and (python_version < '3.11' or python_version > '3.12' or (python_version <= '3.12' and python_version >= '3.11'))
         # via jinja2
-    mpmath==1.3.0
+    mpmath==1.3.0 ; (python_version < '3.11' or python_version > '3.12' or (python_version <= '3.12' and python_version >= '3.11')) and (python_version < '3.11' or python_version > '3.12' or (python_version <= '3.12' and python_version >= '3.11'))
         # via sympy
-    networkx==3.2.1
+    networkx==3.2.1 ; python_version < '3.11' or python_version > '3.12' or (python_version <= '3.12' and python_version >= '3.11')
         # via torch
-    sympy==1.12
+    sympy==1.12 ; python_version < '3.11' or python_version > '3.12' or (python_version <= '3.12' and python_version >= '3.11')
         # via torch
-    torch==2.0.0+cpu
+    torch==2.0.0 ; python_version < '3.11' or (python_version <= '3.12' and python_version > '3.12' and python_version >= '3.11')
         # via
         #   -r requirements.in
         #   example
-    torch==2.0.0+cu118 ; python_version <= '3.12'
+    torch==2.0.0+cpu ; python_version > '3.12'
+        # via
+        #   -r requirements.in
+        #   example
+    torch==2.0.0+cu118 ; python_version <= '3.12' and python_version >= '3.11'
         # via
         #   -r requirements.in
         #   example
         #   triton
-    triton==2.0.0 ; python_version <= '3.12' and platform_machine == 'x86_64' and platform_system == 'Linux'
+    triton==2.0.0 ; python_version <= '3.12' and python_version >= '3.11' and platform_machine == 'x86_64' and platform_system == 'Linux'
         # via torch
-    typing-extensions==4.10.0
+    typing-extensions==4.10.0 ; python_version < '3.11' or python_version > '3.12' or (python_version <= '3.12' and python_version >= '3.11')
         # via torch
 
     ----- stderr -----
-    Resolved 13 packages in [TIME]
+    Resolved 14 packages in [TIME]
     "###
     );
 