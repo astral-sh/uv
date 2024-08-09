62b3d37bb84b1bc28b8454fedf1187d7b1d4a60d
konstin <konstin@mailbox.org>
2024-08-09 10:53:35 +0200

    Warn when there are missing bounds on transitive deps in lowest
    Warn when there are missing bounds on transitive dependencies with `--resolution lowest`.
    
    Implemented as a lazy resolution graph check. Dev deps are odd because they are missing the edge from the root that extras have, but this is more complex because we can put dev dep information in a `Requirement` so i special cased them here.
    
    Closes #2797
    Should help with #1718

--- crates/uv-resolver/src/resolver/mod.rs
+++ crates/uv-resolver/src/resolver/mod.rs
@@ -627,6 +627,7 @@ impl<InstalledPackages: InstalledPackagesProvider> ResolverState<InstalledPackag
             &self.index,
             &self.git,
             &self.python_requirement,
+            self.selector.resolution_strategy(),
             self.options,
         )
     }
@@ -2156,7 +2157,11 @@ impl ForkState {
                     ResolutionStrategy::Lowest | ResolutionStrategy::LowestDirect(..)
                 );
                 if !has_url && missing_lower_bound && strategy_lowest {
-                    warn_user_once!("The direct dependency `{package}` is unpinned. Consider setting a lower bound when using `--resolution-strategy lowest` to avoid using outdated versions.");
+                    warn_user_once!(
+                        "The direct dependency `{package}` is unpinned. \
+                        Consider setting a lower bound when using `--resolution-strategy lowest` \
+                        to avoid using outdated versions."
+                    );
                 }
             }
 
