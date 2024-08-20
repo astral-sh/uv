35a0896d586aef217b0b2ca4f216216206ece99b
Zanie Blue <contact@zanie.dev>
2024-08-20 11:47:46 -0500

    Remove the preview changelog (#6259)
    I'll link to the old file in Git from the release changelog for 0.3.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,16 +10,9 @@ project_root="$(dirname "$script_root")"
 echo "Updating metadata with rooster..."
 cd "$project_root"
 
-# Update the preview changelog
+# Update the changelog
 uv tool run --from 'rooster-blue>=0.0.7' --python 3.12 -- \
-    rooster release "$@" \
-    --only-sections preview \
-    --changelog-file PREVIEW-CHANGELOG.md \
-    --no-update-pyproject --no-update-version-files
-
-# Update the real changelog
-uv tool run --from 'rooster-blue>=0.0.7' --python 3.12 -- \
-    rooster release "$@" --without-sections preview
+    rooster release "$@"
 
 echo "Updating lockfile..."
 cargo update -p uv
