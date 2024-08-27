68acb140df8316c74a1256d8c846e53a06b9b62d
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-08-27 13:18:42 -0400

    Warn on unclosed script tags

--- crates/uv/tests/run.rs
+++ crates/uv/tests/run.rs
@@ -347,6 +347,30 @@ fn run_pep723_script() -> Result<()> {
       ╰─▶ Because there are no versions of add and you require add, we can conclude that your requirements are unsatisfiable.
     "###);
 
+    // If the script contains an unclosed PEP 723 tag, we should error.
+    let test_script = context.temp_dir.child("main.py");
+    test_script.write_str(indoc! { r#"
+        # /// script
+        # requires-python = ">=3.11"
+        # dependencies = [
+        #   "iniconfig",
+        # ]
+
+        # ///
+
+        import iniconfig
+       "#
+    })?;
+
+    uv_snapshot!(context.filters(), context.run().arg("--no-project").arg("main.py"), @r###"
+    success: false
+    exit_code: 2
+    ----- stdout -----
+
+    ----- stderr -----
+    error: An opening tag (`# /// script`) was found without a closing tag (`# ///`). Ensure that every line between the opening and closing tags (including empty lines) starts with a leading `#`.
+    "###);
+
     Ok(())
 }
 
