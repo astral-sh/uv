d94b44a53c019a18d8099c77978490fd15313afd
Zanie Blue <contact@zanie.dev>
2024-09-04 08:02:46 -0500

    Update snapshot

--- crates/uv/tests/pip_install.rs
+++ crates/uv/tests/pip_install.rs
@@ -212,7 +212,7 @@ dependencies = ["flask==1.0.x"]
                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       File "[CACHE_DIR]/builds-v0/[TMP]/core.py", line 159, in setup
         dist.parse_config_files()
-      File "[CACHE_DIR]/builds-v0/[TMP]/_virtualenv.py", line 22, in parse_config_files
+      File "[CACHE_DIR]/builds-v0/[TMP]/_virtualenv.py", line 20, in parse_config_files
         result = old_parse_config_files(self, *args, **kwargs)
                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       File "[CACHE_DIR]/builds-v0/[TMP]/dist.py", line 631, in parse_config_files
