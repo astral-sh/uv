6b76e42f750244ee7d5809bdd8f147a6311a7762
j178 <10510431+j178@users.noreply.github.com>
2024-09-12 14:17:40 +0800

    A failing test

--- crates/uv/tests/run.rs
+++ crates/uv/tests/run.rs
@@ -1708,6 +1708,28 @@ fn run_module() {
 
     ----- stderr -----
     "#);
+
+    uv_snapshot!(context.filters(), context.run().arg("-m").arg("http.server").arg("-h"), @r#"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    usage: server.py [-h] [--cgi] [-b ADDRESS] [-d DIRECTORY] [-p VERSION] [port]
+
+    positional arguments:
+      port                  bind to this port (default: 8000)
+
+    options:
+      -h, --help            show this help message and exit
+      --cgi                 run as CGI server
+      -b ADDRESS, --bind ADDRESS
+                            bind to this address (default: all interfaces)
+      -d DIRECTORY, --directory DIRECTORY
+                            serve this directory (default: current directory)
+      -p VERSION, --protocol VERSION
+                            conform to this HTTP version (default: HTTP/1.0)
+
+    ----- stderr -----
+    "#);
 }
 
 /// When the `pyproject.toml` file is invalid.
