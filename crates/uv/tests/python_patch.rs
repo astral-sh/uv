66fe10793a962b7c7a8e8e697d3f25e0aea98fa4
Tej Singh <tejfsingh@gmail.com>
2024-09-25 16:45:47 -0700

    Remove test file

--- crates/uv/tests/python_patch.rs
+++
@@ -1,8 +0,0 @@
-use assert_fs::fixture::{FileWriteStr, PathChild};
-
-use anyhow::Result;
-use common::{uv_snapshot, TestContext};
-use insta::assert_snapshot;
-use uv_python::{PYTHON_VERSIONS_FILENAME, PYTHON_VERSION_FILENAME};
-
-mod common;
