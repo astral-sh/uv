7ad6ca10456fed724bcc52279f2115a916c491ea
Kemal Akkoyun <kakkoyun@gmail.com>
2024-09-26 21:47:55 +0200

    Address review comments
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- crates/uv-python/src/cpuinfo.rs
+++ crates/uv-python/src/cpuinfo.rs
@@ -1,8 +1,10 @@
 //! Fetches CPU information.
 
 use anyhow::Error;
+#[cfg(target_os = "linux")]
 use procfs::{CpuInfo, Current};
 
+#[cfg(target_os = "linux")]
 pub(crate) fn detect_hardware_floating_point_support() -> Result<bool, Error> {
     let cpu_info = CpuInfo::current()?;
     for num in 0..cpu_info.num_cores() {
@@ -12,5 +14,11 @@ pub(crate) fn detect_hardware_floating_point_support() -> Result<bool, Error> {
             }
         }
     }
+    Ok(false)
+}
+
+#[cfg(not(target_os = "linux"))]
+#[allow(clippy::unnecessary_wraps)]
+pub(crate) fn detect_hardware_floating_point_support() -> Result<bool, Error> {
     Ok(false)
 }
