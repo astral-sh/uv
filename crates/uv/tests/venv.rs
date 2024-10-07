a4f64d2be6fc9a691ccacad66a78299a37c7dbbf
Zanie Blue <contact@zanie.dev>
2024-10-07 12:38:12 -0500

    Bump patch Python versions for project (#7972)

--- crates/uv/tests/venv.rs
+++ crates/uv/tests/venv.rs
@@ -666,18 +666,18 @@ fn create_venv_unknown_python_patch() {
 #[cfg(feature = "python-patch")]
 #[test]
 fn create_venv_python_patch() {
-    let context = TestContext::new_with_versions(&["3.12.1"]);
+    let context = TestContext::new_with_versions(&["3.12.6"]);
 
     uv_snapshot!(context.filters(), context.venv()
         .arg(context.venv.as_os_str())
         .arg("--python")
-        .arg("3.12.1"), @r###"
+        .arg("3.12.6"), @r###"
     success: true
     exit_code: 0
     ----- stdout -----
 
     ----- stderr -----
-    Using CPython 3.12.1 interpreter at: [PYTHON-3.12.1]
+    Using CPython 3.12.6 interpreter at: [PYTHON-3.12.6]
     Creating virtual environment at: .venv
     Activate with: source .venv/[BIN]/activate
     "###
