882b4d395d1d905fa19c5851f7b46260605217d8
Jacob Coffee <jacob@z7x.org>
2024-10-08 16:05:57 -0500

    chore: address review

--- crates/uv-cli/src/lib.rs
+++ crates/uv-cli/src/lib.rs
@@ -3263,9 +3263,9 @@ pub enum ToolCommand {
     /// Packages are installed into an ephemeral virtual environment in the uv
     /// cache directory.
     #[command(
-        about = "Also available via the alias `uvx`.",
         after_help = "You can also use `uvx` as an alias for `uv tool run`. \n\
-        Use `uv help tool run` for more details."
+        Use `uv help tool run` for more details.",
+        after_long_help = ""
     )]
     Run(ToolRunArgs),
     /// Hidden alias for `uv tool run` for the `uvx` command
