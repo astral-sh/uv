dd9fd71d89a74bf524785ac51786ee94323d3d47
Eduardo González Vaquero <47718648+edugzlez@users.noreply.github.com>
2024-10-22 16:18:04 +0200

    Add support for `.env` and custom env files in `uv run` (#8263)
    I have been reading discussion #1384 about .env and how to include it in
    the `uv run` command.
    
    I have always wanted to include this possibility in `uv`, so I was
    interested in the latest changes. Following @charliermarsh's [advice
    ](https://github.com/astral-sh/uv/issues/1384#issuecomment-2381434225) I
    have tried to respect the philosophy that `uv run` uses the default
    `.env` and this can be discarded or changed via terminal or environment
    variables.
    
    The behaviour is as follows:
    - `uv run file.py` executes file.py using the `.env` (if it exists).
    - `uv run --env-file .env.development file.py` uses the
    `.env.development` file to load the variables and then runs file.py. In
    this case the program fails if the file does not exist.
    - `uv run --no-env-file file.py` skips reading the `.env` file.
    
    Equivalently, I have included the `UV_ENV_FILE` and `UV_NO_ENV_FILE`
    environment variables.
    
    I haven't got into including automated tests, I would need help with
    this.
    
    I have tried the above commands, with a python script that prints
    environment variables.
    
    ---------
    
    Co-authored-by: Charlie Marsh <charlie.r.marsh@gmail.com>

--- crates/uv/src/lib.rs
+++ crates/uv/src/lib.rs
@@ -1,4 +1,5 @@
 use std::borrow::Cow;
+use std::env;
 use std::ffi::OsString;
 use std::fmt::Write;
 use std::io::stdout;
@@ -1295,6 +1296,8 @@ async fn run_project(
                 globals.native_tls,
                 &cache,
                 printer,
+                args.env_file,
+                args.no_env_file,
             ))
             .await
         }
