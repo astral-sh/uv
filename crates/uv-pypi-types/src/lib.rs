202ef0a3f76c280bf8a03669a7b2f7d353e1f949
Andrew Gallant <andrew@astral.sh>
2024-11-13 09:56:32 -0500

    uv-pypi-types: rename conflicting_groups module to conflicts
    We'll do the rest of the renaming in the next commit.

--- crates/uv-pypi-types/src/lib.rs
+++ crates/uv-pypi-types/src/lib.rs
@@ -1,5 +1,5 @@
 pub use base_url::*;
-pub use conflicting_groups::*;
+pub use conflicts::*;
 pub use direct_url::*;
 pub use lenient_requirement::*;
 pub use marker_environment::*;
@@ -11,7 +11,7 @@ pub use simple_json::*;
 pub use supported_environments::*;
 
 mod base_url;
-mod conflicting_groups;
+mod conflicts;
 mod direct_url;
 mod lenient_requirement;
 mod marker_environment;
