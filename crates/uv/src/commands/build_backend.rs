92e4b6009e51650a0988e91e9d88dfc6d46327a2
konstin <konstin@mailbox.org>
2024-11-19 22:35:57 +0100

    Review

--- crates/uv/src/commands/build_backend.rs
+++ crates/uv/src/commands/build_backend.rs
@@ -1,11 +1,13 @@
 #![allow(clippy::print_stdout)]
 
 use crate::commands::ExitStatus;
-use anyhow::Result;
+use anyhow::{Context, Result};
 use std::env;
+use std::io::Write;
 use std::path::Path;
 use uv_build_backend::SourceDistSettings;
 
+/// PEP 517 hook to build a source distribution.
 pub(crate) fn build_sdist(sdist_directory: &Path) -> Result<ExitStatus> {
     let filename = uv_build_backend::build_source_dist(
         &env::current_dir()?,
@@ -13,9 +15,12 @@ pub(crate) fn build_sdist(sdist_directory: &Path) -> Result<ExitStatus> {
         SourceDistSettings::default(),
         uv_version::version(),
     )?;
-    println!("{filename}");
+    // Tell the build frontend about the name of the artifact we built
+    writeln!(&mut std::io::stdout(), "{filename}").context("stdout is closed")?;
     Ok(ExitStatus::Success)
 }
+
+/// PEP 517 hook to build a wheel.
 pub(crate) fn build_wheel(
     wheel_directory: &Path,
     metadata_directory: Option<&Path>,
@@ -26,10 +31,12 @@ pub(crate) fn build_wheel(
         metadata_directory,
         uv_version::version(),
     )?;
-    println!("{filename}");
+    // Tell the build frontend about the name of the artifact we built
+    writeln!(&mut std::io::stdout(), "{filename}").context("stdout is closed")?;
     Ok(ExitStatus::Success)
 }
 
+/// PEP 660 hook to build a wheel.
 pub(crate) fn build_editable(
     wheel_directory: &Path,
     metadata_directory: Option<&Path>,
@@ -40,7 +47,8 @@ pub(crate) fn build_editable(
         metadata_directory,
         uv_version::version(),
     )?;
-    println!("{filename}");
+    // Tell the build frontend about the name of the artifact we built
+    writeln!(&mut std::io::stdout(), "{filename}").context("stdout is closed")?;
     Ok(ExitStatus::Success)
 }
 
@@ -54,13 +62,15 @@ pub(crate) fn get_requires_for_build_wheel() -> Result<ExitStatus> {
     unimplemented!("uv does not support extra requires")
 }
 
+/// PEP 517 hook to just emit metadata through `.dist-info`.
 pub(crate) fn prepare_metadata_for_build_wheel(metadata_directory: &Path) -> Result<ExitStatus> {
     let filename = uv_build_backend::metadata(
         &env::current_dir()?,
         metadata_directory,
         uv_version::version(),
     )?;
-    println!("{filename}");
+    // Tell the build frontend about the name of the artifact we built
+    writeln!(&mut std::io::stdout(), "{filename}").context("stdout is closed")?;
     Ok(ExitStatus::Success)
 }
 
@@ -69,12 +79,14 @@ pub(crate) fn get_requires_for_build_editable() -> Result<ExitStatus> {
     unimplemented!("uv does not support extra requires")
 }
 
+/// PEP 660 hook to just emit metadata through `.dist-info`.
 pub(crate) fn prepare_metadata_for_build_editable(metadata_directory: &Path) -> Result<ExitStatus> {
     let filename = uv_build_backend::metadata(
         &env::current_dir()?,
         metadata_directory,
         uv_version::version(),
     )?;
-    println!("{filename}");
+    // Tell the build frontend about the name of the artifact we built
+    writeln!(&mut std::io::stdout(), "{filename}").context("stdout is closed")?;
     Ok(ExitStatus::Success)
 }
