d3c2854ffa7de058d69bd03c689171a25cacfc08
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-11-23 21:12:44 -0500

    Support --extra --no-extra

--- crates/uv-configuration/src/extras.rs
+++ crates/uv-configuration/src/extras.rs
@@ -1,5 +1,4 @@
-use std::collections::HashSet;
-
+use rustc_hash::FxHashSet;
 use uv_normalize::ExtraName;
 
 #[derive(Debug, Default, Clone)]
@@ -8,19 +7,25 @@ pub enum ExtrasSpecification {
     None,
     All,
     Some(Vec<ExtraName>),
-    Exclude(HashSet<ExtraName>),
+    Exclude(FxHashSet<ExtraName>),
 }
 
 impl ExtrasSpecification {
     /// Determine the extras specification to use based on the command-line arguments.
-    pub fn from_args(all_extras: bool, no_extra: Vec<ExtraName>, extra: Vec<ExtraName>) -> Self {
+    pub fn from_args(
+        all_extras: bool,
+        no_extra: Vec<ExtraName>,
+        mut extra: Vec<ExtraName>,
+    ) -> Self {
         if all_extras && !no_extra.is_empty() {
-            ExtrasSpecification::Exclude(HashSet::from_iter(no_extra))
+            ExtrasSpecification::Exclude(FxHashSet::from_iter(no_extra))
         } else if all_extras {
             ExtrasSpecification::All
         } else if extra.is_empty() {
             ExtrasSpecification::None
         } else {
+            // If a package is included in both `no_extra` and `extra`, it should be excluded.
+            extra.retain(|name| !no_extra.contains(name));
             ExtrasSpecification::Some(extra)
         }
     }
@@ -52,11 +57,13 @@ impl ExtrasSpecification {
     }
 }
 
+/// An iterator over the extra names to include.
+#[derive(Debug)]
 pub enum ExtrasIter<'a, Names: Iterator<Item = &'a ExtraName>> {
     None,
     All(Names),
     Some(std::slice::Iter<'a, ExtraName>),
-    Exclude(Names, &'a HashSet<ExtraName>),
+    Exclude(Names, &'a FxHashSet<ExtraName>),
 }
 
 impl<'a, Names: Iterator<Item = &'a ExtraName>> Iterator for ExtrasIter<'a, Names> {
@@ -153,5 +160,15 @@ mod tests {
         let spec = ExtrasSpecification::from_args(true, no_extra, vec![]);
         let result: Vec<_> = spec.extra_names(pkg_extras.iter()).cloned().collect();
         assert_eq!(result, extras!["dev", "docs"]);
+    }
+
+    #[test]
+    fn test_no_extra_extra() {
+        let pkg_extras = extras!["dev", "docs", "extra-1", "extra-2"];
+        let no_extra = extras!["extra-1", "extra-2"];
+        let extra = extras!["extra-1", "extra-2", "docs"];
+        let spec = ExtrasSpecification::from_args(false, no_extra, extra);
+        let result: Vec<_> = spec.extra_names(pkg_extras.iter()).cloned().collect();
+        assert_eq!(result, extras!["docs"]);
     }
 }
