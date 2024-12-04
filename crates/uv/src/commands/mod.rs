7939d3fb5bcb79c16b85327820993efdfc513bd0
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-12-04 22:20:30 +0100

    Create missing dir for `uv export`/ `uv pip compile` (#9648)
    ## Summary
    
    Closes #9643.
    
    I modified the `commit` fn so this applies to `uv compile --output-file`
    too. But I can move it to the export module if we want to restrict this
    to `uv export` only.
    
    ## Test Plan
    
    `cargo test`

--- crates/uv/src/commands/mod.rs
+++ crates/uv/src/commands/mod.rs
@@ -231,6 +231,10 @@ impl<'a> OutputWriter<'a> {
     /// Commit the buffer to the output file.
     async fn commit(self) -> std::io::Result<()> {
         if let Some(output_file) = self.output_file {
+            if let Some(parent_dir) = output_file.parent() {
+                fs_err::create_dir_all(parent_dir)?;
+            }
+
             // If the output file is an existing symlink, write to the destination instead.
             let output_file = fs_err::read_link(output_file)
                 .map(Cow::Owned)
