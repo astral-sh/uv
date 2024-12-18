dd760ee507121536d98f639f1f5f097f0f93fc71
Charlie Marsh <charlie.r.marsh@gmail.com>
2024-12-18 10:29:34 -0500

    Normalize `platform_system` to `sys_platform` (#9949)
    ## Summary
    
    A revival of an old idea (#9344) that I have slightly more confidence in
    now. I abandoned this idea because (1) it couldn't capture that, e.g.,
    `platform_system == 'Windows' and sys_platform == 'foo'` (or some other
    unknown value) are disjoint, and (2) I thought that Android returned
    `"android"` for one of `sys_platform` or `platform_system`, which
    would've made this logic incorrect.
    
    However, it looks like Android... doesn't do that? And the values here
    are almost always in a small, known set. So in the end, the tradeoffs
    here actually seem pretty good.
    
    Vis-a-vis our current solution, this can (e.g.) _simplify out_
    expressions like `sys_platform == 'win32' or platform_system ==
    'Windows'`.

--- crates/uv/tests/it/lock.rs
+++ crates/uv/tests/it/lock.rs
@@ -1014,7 +1014,7 @@ fn lock_wheel_url() -> Result<()> {
             { name = "trio", marker = "extra == 'trio'", specifier = ">=0.23" },
             { name = "trustme", marker = "extra == 'test'" },
             { name = "typing-extensions", marker = "python_full_version < '3.11'", specifier = ">=4.1" },
-            { name = "uvloop", marker = "platform_python_implementation == 'CPython' and platform_system != 'Windows' and extra == 'test'", specifier = ">=0.17" },
+            { name = "uvloop", marker = "platform_python_implementation == 'CPython' and sys_platform != 'win32' and extra == 'test'", specifier = ">=0.17" },
         ]
 
         [[package]]
@@ -1159,7 +1159,7 @@ fn lock_sdist_url() -> Result<()> {
             { name = "trio", marker = "extra == 'trio'", specifier = ">=0.23" },
             { name = "trustme", marker = "extra == 'test'" },
             { name = "typing-extensions", marker = "python_full_version < '3.11'", specifier = ">=4.1" },
-            { name = "uvloop", marker = "platform_python_implementation == 'CPython' and platform_system != 'Windows' and extra == 'test'", specifier = ">=0.17" },
+            { name = "uvloop", marker = "platform_python_implementation == 'CPython' and sys_platform != 'win32' and extra == 'test'", specifier = ">=0.17" },
         ]
 
         [[package]]
@@ -1675,7 +1675,7 @@ fn lock_dependency_extra() -> Result<()> {
         version = "8.1.7"
         source = { registry = "https://pypi.org/simple" }
         dependencies = [
-            { name = "colorama", marker = "platform_system == 'Windows'" },
+            { name = "colorama", marker = "sys_platform == 'win32'" },
         ]
         sdist = { url = "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz", hash = "sha256:ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de", size = 336121 }
         wheels = [
@@ -2180,7 +2180,7 @@ fn lock_dependency_non_existent_extra() -> Result<()> {
         version = "8.1.7"
         source = { registry = "https://pypi.org/simple" }
         dependencies = [
-            { name = "colorama", marker = "platform_system == 'Windows'" },
+            { name = "colorama", marker = "sys_platform == 'win32'" },
         ]
         sdist = { url = "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz", hash = "sha256:ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de", size = 336121 }
         wheels = [
@@ -12694,10 +12694,10 @@ fn lock_constrained_environment() -> Result<()> {
         version = 1
         requires-python = ">=3.12"
         resolution-markers = [
-            "platform_system != 'Windows'",
+            "sys_platform != 'win32'",
         ]
         supported-markers = [
-            "platform_system != 'Windows'",
+            "sys_platform != 'win32'",
         ]
 
         [options]
@@ -12708,11 +12708,11 @@ fn lock_constrained_environment() -> Result<()> {
         version = "24.3.0"
         source = { registry = "https://pypi.org/simple" }
         dependencies = [
-            { name = "click", marker = "platform_system != 'Windows'" },
-            { name = "mypy-extensions", marker = "platform_system != 'Windows'" },
-            { name = "packaging", marker = "platform_system != 'Windows'" },
-            { name = "pathspec", marker = "platform_system != 'Windows'" },
-            { name = "platformdirs", marker = "platform_system != 'Windows'" },
+            { name = "click", marker = "sys_platform != 'win32'" },
+            { name = "mypy-extensions", marker = "sys_platform != 'win32'" },
+            { name = "packaging", marker = "sys_platform != 'win32'" },
+            { name = "pathspec", marker = "sys_platform != 'win32'" },
+            { name = "platformdirs", marker = "sys_platform != 'win32'" },
         ]
         sdist = { url = "https://files.pythonhosted.org/packages/8f/5f/bac24a952668c7482cfdb4ebf91ba57a796c9da8829363a772040c1a3312/black-24.3.0.tar.gz", hash = "sha256:a0c9c4a0771afc6919578cec71ce82a3e31e054904e7197deacbc9382671c41f", size = 634292 }
         wheels = [
@@ -12772,7 +12772,7 @@ fn lock_constrained_environment() -> Result<()> {
         version = "0.1.0"
         source = { editable = "." }
         dependencies = [
-            { name = "black", marker = "platform_system != 'Windows'" },
+            { name = "black", marker = "sys_platform != 'win32'" },
         ]
 
         [package.metadata]
@@ -12907,7 +12907,7 @@ fn lock_constrained_environment() -> Result<()> {
         version = "8.1.7"
         source = { registry = "https://pypi.org/simple" }
         dependencies = [
-            { name = "colorama", marker = "platform_system == 'Windows'" },
+            { name = "colorama", marker = "sys_platform == 'win32'" },
         ]
         sdist = { url = "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz", hash = "sha256:ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de", size = 336121 }
         wheels = [
@@ -13024,10 +13024,10 @@ fn lock_constrained_environment_legacy() -> Result<()> {
         version = 1
         requires-python = ">=3.12"
         resolution-markers = [
-            "platform_system != 'Windows'",
+            "sys_platform != 'win32'",
         ]
         supported-markers = [
-            "platform_system != 'Windows'",
+            "sys_platform != 'win32'",
         ]
 
         [options]
@@ -13043,11 +13043,11 @@ fn lock_constrained_environment_legacy() -> Result<()> {
         version = "24.3.0"
         source = { registry = "https://pypi.org/simple" }
         dependencies = [
-            { name = "click", marker = "platform_system != 'Windows'" },
-            { name = "mypy-extensions", marker = "platform_system != 'Windows'" },
-            { name = "packaging", marker = "platform_system != 'Windows'" },
-            { name = "pathspec", marker = "platform_system != 'Windows'" },
-            { name = "platformdirs", marker = "platform_system != 'Windows'" },
+            { name = "click", marker = "sys_platform != 'win32'" },
+            { name = "mypy-extensions", marker = "sys_platform != 'win32'" },
+            { name = "packaging", marker = "sys_platform != 'win32'" },
+            { name = "pathspec", marker = "sys_platform != 'win32'" },
+            { name = "platformdirs", marker = "sys_platform != 'win32'" },
         ]
         sdist = { url = "https://files.pythonhosted.org/packages/8f/5f/bac24a952668c7482cfdb4ebf91ba57a796c9da8829363a772040c1a3312/black-24.3.0.tar.gz", hash = "sha256:a0c9c4a0771afc6919578cec71ce82a3e31e054904e7197deacbc9382671c41f", size = 634292 }
         wheels = [
@@ -13062,7 +13062,7 @@ fn lock_constrained_environment_legacy() -> Result<()> {
         version = "0.1.0"
         source = { virtual = "child" }
         dependencies = [
-            { name = "black", marker = "platform_system != 'Windows'" },
+            { name = "black", marker = "sys_platform != 'win32'" },
         ]
 
         [package.metadata]
@@ -13169,9 +13169,9 @@ fn lock_overlapping_environment() -> Result<()> {
     ----- stdout -----
 
     ----- stderr -----
-    error: Supported environments must be disjoint, but the following markers overlap: `platform_system != 'Windows'` and `python_full_version >= '3.11'`.
+    error: Supported environments must be disjoint, but the following markers overlap: `sys_platform != 'win32'` and `python_full_version >= '3.11'`.
 
-    hint: replace `python_full_version >= '3.11'` with `python_full_version >= '3.11' and platform_system == 'Windows'`.
+    hint: replace `python_full_version >= '3.11'` with `python_full_version >= '3.11' and sys_platform == 'win32'`.
     "###);
 
     Ok(())
@@ -15049,7 +15049,7 @@ fn lock_explicit_virtual_project() -> Result<()> {
         version = "8.1.7"
         source = { registry = "https://pypi.org/simple" }
         dependencies = [
-            { name = "colorama", marker = "platform_system == 'Windows'" },
+            { name = "colorama", marker = "sys_platform == 'win32'" },
         ]
         sdist = { url = "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz", hash = "sha256:ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de", size = 336121 }
         wheels = [
@@ -15266,7 +15266,7 @@ fn lock_implicit_virtual_project() -> Result<()> {
         version = "8.1.7"
         source = { registry = "https://pypi.org/simple" }
         dependencies = [
-            { name = "colorama", marker = "platform_system == 'Windows'" },
+            { name = "colorama", marker = "sys_platform == 'win32'" },
         ]
         sdist = { url = "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz", hash = "sha256:ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de", size = 336121 }
         wheels = [
@@ -15765,7 +15765,7 @@ fn lock_python_upper_bound() -> Result<()> {
         version = "8.1.7"
         source = { registry = "https://pypi.org/simple" }
         dependencies = [
-            { name = "colorama", marker = "platform_system == 'Windows'" },
+            { name = "colorama", marker = "sys_platform == 'win32'" },
         ]
         sdist = { url = "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz", hash = "sha256:ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de", size = 336121 }
         wheels = [
@@ -16048,7 +16048,7 @@ fn lock_python_upper_bound() -> Result<()> {
         version = "4.66.2"
         source = { registry = "https://pypi.org/simple" }
         dependencies = [
-            { name = "colorama", marker = "platform_system == 'Windows'" },
+            { name = "colorama", marker = "sys_platform == 'win32'" },
         ]
         sdist = { url = "https://files.pythonhosted.org/packages/ea/85/3ce0f9f7d3f596e7ea57f4e5ce8c18cb44e4a9daa58ddb46ee0d13d6bff8/tqdm-4.66.2.tar.gz", hash = "sha256:6cd52cdf0fef0e0f543299cfc96fec90d7b8a7e88745f411ec33eb44d5ed3531", size = 169462 }
         wheels = [
@@ -17985,7 +17985,7 @@ fn lock_multiple_sources_respect_marker() -> Result<()> {
         name = "project"
         version = "0.1.0"
         requires-python = ">=3.12"
-        dependencies = ["iniconfig ; platform_system == 'Windows'"]
+        dependencies = ["iniconfig ; sys_platform == 'win32'"]
 
         [tool.uv.sources]
         iniconfig = [
@@ -18030,11 +18030,11 @@ fn lock_multiple_sources_respect_marker() -> Result<()> {
         version = "0.1.0"
         source = { virtual = "." }
         dependencies = [
-            { name = "iniconfig", marker = "platform_system == 'Windows' and sys_platform != 'darwin'" },
+            { name = "iniconfig", marker = "sys_platform == 'win32'" },
         ]
 
         [package.metadata]
-        requires-dist = [{ name = "iniconfig", marker = "platform_system == 'Windows' and sys_platform != 'darwin'" }]
+        requires-dist = [{ name = "iniconfig", marker = "sys_platform == 'win32'" }]
         "###
         );
     });
