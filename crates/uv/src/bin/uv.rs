8feeedd4d09328978c1aa6116450befa345a9682
Charlie Marsh <charlie.r.marsh@gmail.com>
2025-01-02 11:53:26 -0500

    Gate performance feature on cfg

--- crates/uv/src/bin/uv.rs
+++ crates/uv/src/bin/uv.rs
@@ -1,5 +1,6 @@
 // Don't optimize the alloc crate away due to it being otherwise unused.
 // https://github.com/rust-lang/rust/issues/64402
+// #[cfg(feature = "performance-memory-allocator")]
 extern crate uv_performance_memory_allocator;
 
 use std::process::ExitCode;
