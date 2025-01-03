9f9b90129b4f8495e155818bb78a2f961f8c0185
konstin <konstin@mailbox.org>
2025-01-03 13:41:04 +0100

    Optimize `requirements_for_extra`

--- crates/uv-configuration/src/overrides.rs
+++ crates/uv-configuration/src/overrides.rs
@@ -42,7 +42,16 @@ impl Overrides {
         &'a self,
         requirements: impl IntoIterator<Item = &'a Requirement>,
     ) -> impl Iterator<Item = Cow<'a, Requirement>> {
-        requirements.into_iter().flat_map(|requirement| {
+        if self.0.is_empty() {
+            // Fast path: There are no overrides.
+            return Either::Left(
+                requirements
+                    .into_iter()
+                    .map(|requirement| Cow::Borrowed(requirement)),
+            );
+        }
+
+        Either::Right(requirements.into_iter().flat_map(|requirement| {
             let Some(overrides) = self.get(&requirement.name) else {
                 // Case 1: No override(s).
                 return Either::Left(std::iter::once(Cow::Borrowed(requirement)));
@@ -70,6 +79,6 @@ impl Overrides {
                     })
                 },
             )))
-        })
+        }))
     }
 }
