e0e8ba582a77ac473e05320eec63995d403eea40
Charlie Marsh <charlie.r.marsh@gmail.com>
2025-01-13 19:49:43 -0500

    Use structured types for parsing and formatting language and ABI tags (#10525)
    ## Summary
    
    I need to be able to do non-lexicographic comparisons between tags
    (e.g., so I can sort `cp313` as greater than `cp39`). It ended up being
    easiest to just create structured types for all the tags we support,
    with `FromStr` and `Display` implementations.
    
    We don't currently store these in `Tags` or in `WheelFilename`. We may
    want to, since they're really small (and `Copy`), but I need to
    benchmark to determine whether parsing these in `WheelFilename` is
    prohibitively slow.

--- crates/uv-platform-tags/src/lib.rs
+++ crates/uv-platform-tags/src/lib.rs
@@ -1,5 +1,9 @@
+pub use abi_tag::AbiTag;
+pub use language_tag::LanguageTag;
 pub use platform::{Arch, Os, Platform, PlatformError};
 pub use tags::{IncompatibleTag, TagCompatibility, TagPriority, Tags, TagsError};
 
+mod abi_tag;
+mod language_tag;
 mod platform;
 mod tags;
