254ac6387ac98e589a335e2def33a73c5068f36f
Zanie Blue <contact@zanie.dev>
2025-01-15 12:02:49 -0600

    Toggle based on the presence of a D: drive

--- .github/workflows/setup-dev-drive.ps1
+++ .github/workflows/setup-dev-drive.ps1
@@ -1,7 +1,26 @@
-# This uses `D:` as the workspace instead of `C:`, as the performance is much
-# better. Previously, we created a ReFS Dev Drive, but this is actually faster.
+# Configures a drive for testing in CI.
 
-$Drive = "D:"
+# When not using a GitHub Actions "larger runner", the `D:` drive is present and
+# has similar or better performance characteristics than a ReFS dev drive.
+# Sometimes using a larger runner is still more performant (e.g., when running
+# the test suite) and we need to create a dev drive. This script automatically
+# configures the appropriate drive.
+
+# Note we use `Get-PSDrive` is not sufficient because the drive letter is assigned.
+if (Test-Path "D:\") {
+    Write-Output "Using `D:` drive"
+    $Drive = "D:"
+} else {
+    $Volume = New-VHD -Path C:/uv_dev_drive.vhdx -SizeBytes 20GB |
+        Mount-VHD -Passthru |
+        Initialize-Disk -Passthru |
+        New-Partition -AssignDriveLetter -UseMaximumSize |
+        Format-Volume -FileSystem ReFS -Confirm:$false -Force
+
+    Write-Output "Using ReFS drive at $Volume"
+    $Drive = "$($Volume.DriveLetter):"
+}
+
 $Tmp = "$($Drive)\uv-tmp"
 
 # Create the directory ahead of time in an attempt to avoid race-conditions
