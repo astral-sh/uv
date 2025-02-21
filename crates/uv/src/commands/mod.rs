4a882468ea7eeb0fbe9a12bf41478b37b6688021
Loïc LESCOAT <loic@example.com>
2025-02-20 22:28:18 -0800

    can self uninstall iff can self update

--- crates/uv/src/commands/mod.rs
+++ crates/uv/src/commands/mod.rs
@@ -36,6 +36,7 @@ pub(crate) use python::install::install as python_install;
 pub(crate) use python::list::list as python_list;
 pub(crate) use python::pin::pin as python_pin;
 pub(crate) use python::uninstall::uninstall as python_uninstall;
+#[cfg(feature = "self-update")]
 pub(crate) use self_uninstall::self_uninstall;
 #[cfg(feature = "self-update")]
 pub(crate) use self_update::self_update;
@@ -73,6 +74,7 @@ mod publish;
 mod python;
 pub(crate) mod reporters;
 mod run;
+#[cfg(feature = "self-update")]
 mod self_uninstall;
 #[cfg(feature = "self-update")]
 mod self_update;
--- crates/uv/src/lib.rs
+++ crates/uv/src/lib.rs
@@ -18,16 +18,13 @@ use tokio::task::spawn_blocking;
 use tracing::{debug, instrument};
 use uv_cache::{Cache, Refresh};
 use uv_cache_info::Timestamp;
-#[cfg(feature = "self-update")]
-use uv_cli::SelfUpdateArgs;
 use uv_cli::{
     compat::CompatArgs, BuildBackendCommand, CacheCommand, CacheNamespace, Cli, Commands,
     PipCommand, PipNamespace, ProjectCommand,
 };
-use uv_cli::{
-    PythonCommand, PythonNamespace, SelfCommand, SelfNamespace, SelfUninstallArgs, ToolCommand,
-    ToolNamespace, TopLevelArgs,
-};
+use uv_cli::{PythonCommand, PythonNamespace, ToolCommand, ToolNamespace, TopLevelArgs};
+#[cfg(feature = "self-update")]
+use uv_cli::{SelfCommand, SelfNamespace, SelfUninstallArgs, SelfUpdateArgs};
 use uv_fs::{Simplified, CWD};
 use uv_requirements::RequirementsSource;
 use uv_scripts::{Pep723Error, Pep723Item, Pep723Metadata, Pep723Script};
@@ -911,13 +908,14 @@ async fn run(mut cli: Cli) -> Result<ExitStatus> {
                     token,
                 }),
         }) => commands::self_update(target_version, token, printer).await,
+        #[cfg(feature = "self-update")]
         Commands::Self_(SelfNamespace {
             command: SelfCommand::Uninstall(SelfUninstallArgs { remove_data }),
         }) => commands::self_uninstall(&cache, printer, remove_data),
         #[cfg(not(feature = "self-update"))]
         Commands::Self_(_) => {
             anyhow::bail!(
-                "uv was installed through an external package manager, and self-update \
+                "uv was installed through an external package manager, and self {update,uninstall} \
                 is not available. Please use your package manager to update uv."
             );
         }
