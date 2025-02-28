6f90ded42f48eeacfeb3c480cbed1e8efb003910
Meitar Reihan <meitarr013@gmail.com>
2025-02-28 23:38:33 +0200

    skip debug versions in the minified json

--- crates/uv-python/minify-download-metadata.py
+++ crates/uv-python/minify-download-metadata.py
@@ -19,8 +19,22 @@ VERSION_METADATA = CRATE_ROOT / "download-metadata.json"
 TARGET = CRATE_ROOT / "src" / "download-metadata-minified.json"
 
 
+def process_json(data: dict) -> dict:
+    out_data = {}
+
+    for key, value in data.items():
+        # Exclude debug variants for now, we don't support them in the Rust side
+        if value["variant"] == "debug":
+            continue
+
+        out_data[key] = value
+
+    return out_data
+
+
 def main() -> None:
     json_data = json.loads(Path(VERSION_METADATA).read_text())
+    json_data = process_json(json_data)
     json_string = json.dumps(json_data, separators=(",", ":"))
     TARGET.write_text(json_string)
 
