cfaaf584a911b1b7f89ef148ae6245af5e835129
Charlie Marsh <charlie.r.marsh@gmail.com>
2025-02-27 21:06:18 -0500

    Fix reference to macOS cache path

--- crates/uv-settings/src/settings.rs
+++ crates/uv-settings/src/settings.rs
@@ -218,8 +218,8 @@ pub struct GlobalOptions {
     pub no_cache: Option<bool>,
     /// Path to the cache directory.
     ///
-    /// Defaults to `$HOME/Library/Caches/uv` on macOS, `$XDG_CACHE_HOME/uv` or `$HOME/.cache/uv` on
-    /// Linux, and `%LOCALAPPDATA%\uv\cache` on Windows.
+    /// Defaults to `$XDG_CACHE_HOME/uv` or `$HOME/.cache/uv` on Linux and macOS, and
+    /// `%LOCALAPPDATA%\uv\cache` on Windows.
     #[option(
         default = "None",
         value_type = "str",
