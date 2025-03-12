b73bf606e88a6955e7dec72c5342267d5e0c8eaf
Zanie Blue <contact@zanie.dev>
2025-03-12 12:23:05 -0500

    Add comments

--- scripts/smoke-test/commands.sh
+++ scripts/smoke-test/commands.sh
@@ -1,7 +1,10 @@
-# Note this is not a real shell-script, it's parsed by `smoke-test/__main__.py` and executed
-# serially via Python.
+# NOTE this is not a real shell-script, it's parsed by `smoke-test/__main__.py` and executed
+# serially via Python for cross-platform support.
 
+# Create a virtual environment and install a package with `uv pip`
 uv venv -v
 uv pip install ruff -v
+
+# Run a package via `uvx`
 uvx -v ruff --version
 
