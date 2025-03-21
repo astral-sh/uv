97115b92b7afc12d760e4849cb44963e53443326
Zanie Blue <contact@zanie.dev>
2025-03-21 09:08:30 -0500

    Revert "Logs!"
    This reverts commit e9a6c890f6e019c4712b3cf28511c68d05bb1d1b.

--- crates/uv/tests/it/sync.rs
+++ crates/uv/tests/it/sync.rs
@@ -7681,7 +7681,7 @@ fn sync_dry_run() -> Result<()> {
      + iniconfig==2.0.0
     "###);
 
-    uv_snapshot!(context.filters(), context.sync().arg("--dry-run").arg("-vv"), @r###"
+    uv_snapshot!(context.filters(), context.sync().arg("--dry-run"), @r###"
     success: true
     exit_code: 0
     ----- stdout -----
