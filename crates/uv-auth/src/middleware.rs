866ed93e8f90833b2da98f5e318a8baffe257762
John Mumm <jtfmumm@gmail.com>
2025-04-03 15:55:43 +0200

    Fetch keyring credentials from index URL in auth middleware
    Some registries (like Azure Artifact) can require you to authenticate
    separately for every package URL if you do not authenticate for the
    /simple endpoint. These changes make the auth middleware aware of index
    URL endpoints and attempts to fetch keyring credentials for such an
    index URL when making a request to any URL it's a prefix of.
    
    Currently, if we find a prefix index URL we attempt to fetch keyring
    credentials for it. Otherwise we attempt to fetch for the full URL.
    
    Addresses part of #4056
    Closes #4583

--- crates/uv-auth/src/middleware.rs
+++ crates/uv-auth/src/middleware.rs
@@ -520,7 +520,7 @@ impl AuthMiddleware {
                         debug!("Checking keyring for credentials for index URL {}@{}", username, index_url);
                         keyring.fetch(index_url, Some(username)).await
                     } else {
-                        debug!("Checking keyring for credentials for full URL {}@{}", username, *url);
+                        debug!("Checking keyring for credentials for full URL {}@{}", username, url);
                         keyring.fetch(url, Some(username)).await
                     }
                 } else if matches!(auth_policy, AuthPolicy::Always) {
@@ -530,10 +530,7 @@ impl AuthMiddleware {
                         );
                         keyring.fetch(index_url, None).await
                     } else {
-                        debug!(
-                            "Checking keyring for credentials for full URL {url} without username due to `authenticate = always`"
-                        );
-                        keyring.fetch(url, None).await
+                        None
                     }
                 } else {
                     debug!("Skipping keyring fetch for {url} without username; use `authenticate = always` to force");
