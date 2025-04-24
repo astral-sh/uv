161769faccd93ca4d76f526efd76347eda5ce9f5
Aria Desires <aria.desires@gmail.com>
2025-04-24 13:38:30 -0400

    change `uv version` to be an interface for project version reads and edits (#12349)
    This is a reimplementation of #7248 with a new CLI interface.
    
    The old `uv version` is now `uv self version` (also it has gained a
    `--short` flag for parity).
    The new `uv version` is now an interface for getting/setting the project
    version.
    
    To give a modicum of support for migration, if `uv version` is run and
    we fail to find/read a `pyproject.toml` we will fallback to `uv self
    version`. `uv version --project .` prevents this fallback from being
    allowed.
    
    The new API of `uv version` is as follows:
    
    * pass nothing to read the project version
    * pass a version to set the project version
    * `--bump major|minor|patch` to semver-bump the project version
    * `--dry-run` to show the result but not apply it
    * `--short` to have the final printout contain only the final version
    * `--output-format json` to get the final printout as json
    
    ```
    $ uv version
    myfast 0.1.0
    
    $ uv version --bump major --dry-run
    myfast 0.1.0 => 1.0.0
    
    $ uv version 1.2.3 --dry-run
    myfast 0.1.0 => 1.2.3
    
    $ uv version 1.2.3
    myfast 0.1.0 => 1.2.3
    
    $ uv version  --short
    1.2.3
    
    $ uv version  --output-format json
    {
      "package_name": "myfast",
      "version": "1.2.3",
      "commit_info": null
    }
    ```
    
    Fixes #6298

--- crates/uv/src/lib.rs
+++ crates/uv/src/lib.rs
@@ -19,13 +19,13 @@ use tokio::task::spawn_blocking;
 use tracing::{debug, instrument};
 use uv_cache::{Cache, Refresh};
 use uv_cache_info::Timestamp;
+#[cfg(feature = "self-update")]
+use uv_cli::SelfUpdateArgs;
 use uv_cli::{
     compat::CompatArgs, BuildBackendCommand, CacheCommand, CacheNamespace, Cli, Commands,
-    PipCommand, PipNamespace, ProjectCommand,
+    PipCommand, PipNamespace, ProjectCommand, PythonCommand, PythonNamespace, SelfCommand,
+    SelfNamespace, ToolCommand, ToolNamespace, TopLevelArgs, VersionArgs,
 };
-use uv_cli::{PythonCommand, PythonNamespace, ToolCommand, ToolNamespace, TopLevelArgs};
-#[cfg(feature = "self-update")]
-use uv_cli::{SelfCommand, SelfNamespace, SelfUpdateArgs};
 use uv_configuration::min_stack_size;
 use uv_fs::{Simplified, CWD};
 use uv_pep508::VersionOrUrl;
@@ -359,7 +359,7 @@ async fn run(mut cli: Cli) -> Result<ExitStatus> {
     // Don't initialize the rayon threadpool yet, this is too costly when we're doing a noop sync.
     uv_configuration::RAYON_PARALLELISM.store(globals.concurrency.installs, Ordering::SeqCst);
 
-    debug!("uv {}", uv_cli::version::version());
+    debug!("uv {}", uv_cli::version::uv_self_version());
 
     // Write out any resolved settings.
     macro_rules! show_settings {
@@ -1018,6 +1018,16 @@ async fn run(mut cli: Cli) -> Result<ExitStatus> {
                     token,
                 }),
         }) => commands::self_update(target_version, token, printer).await,
+        Commands::Self_(SelfNamespace {
+            command:
+                SelfCommand::Version {
+                    short,
+                    output_format,
+                },
+        }) => {
+            commands::self_version(short, output_format, printer)?;
+            Ok(ExitStatus::Success)
+        }
         #[cfg(not(feature = "self-update"))]
         Commands::Self_(_) => {
             anyhow::bail!(
@@ -1025,10 +1035,30 @@ async fn run(mut cli: Cli) -> Result<ExitStatus> {
                 is not available. Please use your package manager to update uv."
             );
         }
-        Commands::Version { output_format } => {
-            commands::version(output_format, &mut stdout())?;
-            Ok(ExitStatus::Success)
+        Commands::Version(VersionArgs {
+            value,
+            bump,
+            dry_run,
+            short,
+            output_format,
+        }) => {
+            // If they specified a project, they probably don't want `uv --version` semantics
+            let strict =
+                cli.top_level.global_args.project.is_some() || globals.preview.is_enabled();
+            commands::project_version(
+                &project_dir,
+                value,
+                bump,
+                dry_run,
+                short,
+                output_format,
+                strict,
+                &workspace_cache,
+                printer,
+            )
+            .await
         }
+
         Commands::GenerateShellCompletion(args) => {
             args.shell.generate(&mut Cli::command(), &mut stdout());
             Ok(ExitStatus::Success)
