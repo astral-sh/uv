73eb2dfb1f09ce40086aa4b296f6a87d27670a56
konsti <konstin@mailbox.org>
2025-06-02 17:53:03 +0200

    Remove winsafe (#13779)
    We've been using a number of different winapi crates. This PR removes
    winsafe in favor of the official windows-* crates, so all of uv's own
    winapi calls go through the official windows-* crates.
    
    ---------
    
    Co-authored-by: Aria Desires <aria.desires@gmail.com>

--- crates/uv-fs/src/which.rs
+++ crates/uv-fs/src/which.rs
@@ -1,5 +1,26 @@
 use std::path::Path;
 
+#[cfg(windows)]
+#[allow(unsafe_code)] // We need to do an FFI call through the windows-* crates.
+fn get_binary_type(path: &Path) -> windows::core::Result<u32> {
+    use std::os::windows::ffi::OsStrExt;
+    use windows::Win32::Storage::FileSystem::GetBinaryTypeW;
+    use windows_core::PCWSTR;
+
+    // References:
+    // https://github.com/denoland/deno/blob/01a6379505712be34ebf2cdc874fa7f54a6e9408/runtime/permissions/which.rs#L131-L154
+    // https://github.com/conradkleinespel/rooster/blob/afa78dc9918535752c4af59d2f812197ad754e5a/src/quale.rs#L51-L77
+    let mut binary_type = 0u32;
+    let name = path
+        .as_os_str()
+        .encode_wide()
+        .chain(Some(0))
+        .collect::<Vec<u16>>();
+    // SAFETY: winapi call
+    unsafe { GetBinaryTypeW(PCWSTR(name.as_ptr()), &mut binary_type)? };
+    Ok(binary_type)
+}
+
 /// Check whether a path in PATH is a valid executable.
 ///
 /// Derived from `which`'s `Checker`.
@@ -20,9 +41,7 @@ pub fn is_executable(path: &Path) -> bool {
         if !file_type.is_file() && !file_type.is_symlink() {
             return false;
         }
-        if path.extension().is_none()
-            && winsafe::GetBinaryType(&path.display().to_string()).is_err()
-        {
+        if path.extension().is_none() && get_binary_type(path).is_err() {
             return false;
         }
     }
