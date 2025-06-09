dc455bfc26a1487a02815a645810e07c86522891
Jack O'Connor <oconnor663@gmail.com>
2025-06-09 14:44:43 -0700

    add UV_NO_GITHUB_FAST_PATH

--- crates/uv-git/src/resolver.rs
+++ crates/uv-git/src/resolver.rs
@@ -12,6 +12,7 @@ use tracing::debug;
 use uv_cache_key::{RepositoryUrl, cache_digest};
 use uv_fs::LockedFile;
 use uv_git_types::{GitHubRepository, GitOid, GitReference, GitUrl};
+use uv_static::EnvVars;
 use uv_version::version;
 
 use crate::{Fetch, GitSource, Reporter};
@@ -54,6 +55,10 @@ impl GitResolver {
         url: &GitUrl,
         client: ClientWithMiddleware,
     ) -> Result<Option<GitOid>, GitResolverError> {
+        if std::env::var_os(EnvVars::UV_NO_GITHUB_FAST_PATH).is_some() {
+            return Ok(None);
+        }
+
         let reference = RepositoryReference::from(url);
 
         // If the URL is already precise, return it.
