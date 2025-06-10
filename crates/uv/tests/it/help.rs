aec1f76344bc389a11980f01d14d83152dbaea08
Rene Leonhardt <65483435+reneleonhardt@users.noreply.github.com>
2025-06-10 06:52:36 +0200

    fix tests

--- crates/uv/tests/it/help.rs
+++ crates/uv/tests/it/help.rs
@@ -22,6 +22,7 @@ fn help() {
       add                        Add dependencies to the project
       remove                     Remove dependencies from the project
       version                    Read or update the project's version
+      upgrade                    Upgrade the project's dependency constraints
       sync                       Update the project's environment
       lock                       Update the project's lockfile
       export                     Export the project's lockfile to an alternate format
@@ -104,6 +105,7 @@ fn help_flag() {
       add      Add dependencies to the project
       remove   Remove dependencies from the project
       version  Read or update the project's version
+      upgrade  Upgrade the project's dependency constraints
       sync     Update the project's environment
       lock     Update the project's lockfile
       export   Export the project's lockfile to an alternate format
@@ -184,6 +186,7 @@ fn help_short_flag() {
       add      Add dependencies to the project
       remove   Remove dependencies from the project
       version  Read or update the project's version
+      upgrade  Upgrade the project's dependency constraints
       sync     Update the project's environment
       lock     Update the project's lockfile
       export   Export the project's lockfile to an alternate format
@@ -881,6 +884,7 @@ fn help_unknown_subcommand() {
         add
         remove
         version
+        upgrade
         sync
         lock
         export
@@ -910,6 +914,7 @@ fn help_unknown_subcommand() {
         add
         remove
         version
+        upgrade
         sync
         lock
         export
@@ -968,6 +973,7 @@ fn help_with_global_option() {
       add                        Add dependencies to the project
       remove                     Remove dependencies from the project
       version                    Read or update the project's version
+      upgrade                    Upgrade the project's dependency constraints
       sync                       Update the project's environment
       lock                       Update the project's lockfile
       export                     Export the project's lockfile to an alternate format
@@ -1091,6 +1097,7 @@ fn help_with_no_pager() {
       add                        Add dependencies to the project
       remove                     Remove dependencies from the project
       version                    Read or update the project's version
+      upgrade                    Upgrade the project's dependency constraints
       sync                       Update the project's environment
       lock                       Update the project's lockfile
       export                     Export the project's lockfile to an alternate format
