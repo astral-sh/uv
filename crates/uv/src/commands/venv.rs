af55c954f293a2002852cbbc05f41f703aa4fc4a
Aria Desires <aria.desires@gmail.com>
2025-06-17 15:09:45 -0400

    allow venv to handle full virtual pyprojects

--- crates/uv/src/commands/venv.rs
+++ crates/uv/src/commands/venv.rs
@@ -157,8 +157,12 @@ async fn venv_impl(
     let project = if no_project {
         None
     } else {
-        match VirtualProject::discover(project_dir, &DiscoveryOptions::default(), &workspace_cache)
-            .await
+        match VirtualProject::discover_defaulted(
+            project_dir,
+            &DiscoveryOptions::default(),
+            &workspace_cache,
+        )
+        .await
         {
             Ok(project) => Some(project),
             Err(WorkspaceError::MissingProject(_)) => None,
