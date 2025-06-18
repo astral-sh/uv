8a054f83d7058247aaa9624b93ff6967c54119c4
John Mumm <jtfmumm@gmail.com>
2025-06-18 04:49:17 -0400

    Properly handle authentication when encountering redirects (#13595)
    This is mostly restoring #13215. It also includes a one-line fix for
    #13208 (which resulted from that PR). In particular, Azure was returning
    303s which were not being correctly handled.
    
    I have also opened another PR (#13754) that refactors and improves the
    redirect handling here. It also supersedes the fix here. There are some
    tests failing here but they all pass there.
    
    This PR depends on #13615, which adds a script for testing against
    registries. The test fails for Azure when running against the restored
    #13215 alone and passes with the fix. It also passes for AWS
    CodeArtifact, GCP Artifact Registry, JFrog Artifactory, GitLab, and
    Gemfury in both cases. I also plan to test against Cloudsmith and Nexus.

--- crates/uv-git/src/resolver.rs
+++ crates/uv-git/src/resolver.rs
@@ -53,7 +53,7 @@ impl GitResolver {
     pub async fn github_fast_path(
         &self,
         url: &GitUrl,
-        client: ClientWithMiddleware,
+        client: &ClientWithMiddleware,
     ) -> Result<Option<GitOid>, GitResolverError> {
         if std::env::var_os(EnvVars::UV_NO_GITHUB_FAST_PATH).is_some() {
             return Ok(None);
@@ -117,7 +117,7 @@ impl GitResolver {
     pub async fn fetch(
         &self,
         url: &GitUrl,
-        client: ClientWithMiddleware,
+        client: impl Into<ClientWithMiddleware>,
         disable_ssl: bool,
         offline: bool,
         cache: PathBuf,
