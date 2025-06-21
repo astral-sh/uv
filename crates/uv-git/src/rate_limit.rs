67f9a65de590d07606985211ad01b18243410dd6
Christopher Tee <christopherfytee@gmail.com>
2025-06-21 09:22:06 -0400

    Remove use of LazyLock

--- crates/uv-git/src/rate_limit.rs
+++ crates/uv-git/src/rate_limit.rs
@@ -1,10 +1,8 @@
-use std::sync::LazyLock;
 use std::sync::atomic::{AtomicBool, Ordering};
 
 /// A global state on whether we are being rate-limited by GitHub's REST API.
 /// If we are, avoid "fast-path" attempts.
-pub(crate) static GITHUB_RATE_LIMIT_STATUS: LazyLock<GitHubRateLimitStatus> =
-    LazyLock::new(GitHubRateLimitStatus::default);
+pub(crate) static GITHUB_RATE_LIMIT_STATUS: GitHubRateLimitStatus = GitHubRateLimitStatus::new();
 
 /// GitHub REST API rate limit status tracker.
 ///
@@ -12,10 +10,14 @@ pub(crate) static GITHUB_RATE_LIMIT_STATUS: LazyLock<GitHubRateLimitStatus> =
 ///
 /// The rate limit timeout duration is much longer than the runtime of a `uv` command.
 /// And so we do not need to invalidate this state based on `x-ratelimit-reset`.
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub(crate) struct GitHubRateLimitStatus(AtomicBool);
 
 impl GitHubRateLimitStatus {
+    pub(crate) const fn new() -> Self {
+        Self(AtomicBool::new(false))
+    }
+
     pub(crate) fn activate(&self) {
         self.0.store(true, Ordering::SeqCst);
     }
