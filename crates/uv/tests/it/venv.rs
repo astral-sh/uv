astral-sh/uv

commit e9d5780369da14ecf9f834efc8a17b0118823cfb

Support transparent Python patch version upgrades (#13954)
> NOTE: The PRs that were merged into this feature branch have all been
independently reviewed. But it's also useful to see all of the changes
in their final form. I've added comments to significant changes
throughout the PR to aid discussion.

This PR introduces transparent Python version upgrades to uv, allowing
for a smoother experience when upgrading to new patch versions.
Previously, upgrading Python patch versions required manual updates to
each virtual environment. Now, virtual environments can transparently
upgrade to newer patch versions.

Due to significant changes in how uv installs and executes managed
Python executables, this functionality is initially available behind a
`--preview` flag. Once an installation has been made upgradeable through
`--preview`, subsequent operations (like `uv venv -p 3.10` or patch
upgrades) will work without requiring the flag again. This is
accomplished by checking for the existence of a minor version symlink
directory (or junction on Windows).

### Features

* New `uv python upgrade` command to upgrade installed Python versions
to the latest available patch release:
``` 
# Upgrade specific minor version 
uv python upgrade 3.12 --preview
# Upgrade all installed minor versions
uv python upgrade --preview
```
* Transparent upgrades also occur when installing newer patch versions: 
```
uv python install 3.10.8 --preview
# Automatically upgrades existing 3.10 environments
uv python install 3.10.18
```
* Support for transparently upgradeable Python `bin` installations via
`--preview` flag
```
uv python install 3.13 --preview
# Automatically upgrades the `bin` installation if there is a newer patch version available
uv python upgrade 3.13 --preview
```
* Virtual environments can still be tied to a patch version if desired
(ignoring patch upgrades):
```
uv venv -p 3.10.8
```

### Implementation

Transparent upgrades are implemented using:

crates/uv-virtualenv/src/virtualenv.rs
@@ -10,8 +10,10 @@ use fs_err::File;
 use itertools::Itertools;
 use tracing::debug;
 
+use uv_configuration::PreviewMode;
 use uv_fs::{CWD, Simplified, cachedir};
 use uv_pypi_types::Scheme;
+use uv_python::managed::{PythonMinorVersionLink, create_link_to_executable};
 use uv_python::{Interpreter, VirtualEnvironment};
 use uv_shell::escape_posix_for_single_quotes;
 use uv_version::version;
@@ -53,6 +55,8 @@ pub(crate) fn create(
     allow_existing: bool,
     relocatable: bool,
     seed: bool,
+    upgradeable: bool,
+    preview: PreviewMode,
 ) -> Result<VirtualEnvironment, Error> {
     // Determine the base Python executable; that is, the Python executable that should be
     // considered the "base" for the virtual environment.
@@ -143,13 +147,49 @@ pub(crate) fn create(
     // Create a `.gitignore` file to ignore all files in the venv.
     fs::write(location.join(".gitignore"), "*")?;
 
+    let executable_target = if upgradeable && interpreter.is_standalone() {
+        if let Some(minor_version_link) = PythonMinorVersionLink::from_executable(
+            base_python.as_path(),
+            &interpreter.key(),
+            preview,
+        ) {
+            if !minor_version_link.exists() {
+                base_python.clone()
+            } else {
+                let debug_symlink_term = if cfg!(windows) {
+                    "junction"
+                } else {
+                    "symlink directory"
+                };
+                debug!(
+                    "Using {} {} instead of base Python path: {}",
+                    debug_symlink_term,
+                    &minor_version_link.symlink_directory.display(),
+                    &base_python.display()
+                );
+                minor_version_link.symlink_executable.clone()
+            }
+        } else {
+            base_python.clone()
+        }
+    } else {
+        base_python.clone()
+    };
+
     // Per PEP 405, the Python `home` is the parent directory of the interpreter.
-    let python_home = base_python.parent().ok_or_else(|| {
-        io::Error::new(
-            io::ErrorKind::NotFound,
-            "The Python interpreter needs to have a parent directory",
-        )
-    })?;
+    // In preview mode, for standalone interpreters, this `home` value will include a
+    // symlink directory on Unix or junction on Windows to enable transparent Python patch
+    // upgrades.
+    let python_home = executable_target
+        .parent()
+        .ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::NotFound,
+                "The Python interpreter needs to have a parent directory",
+            )
+        })?
+        .to_path_buf();
+    let python_home = python_home.as_path();
 
     // Different names for the python interpreter
     fs::create_dir_all(&scripts)?;
@@ -157,7 +197,7 @@ pub(crate) fn create(
 
     #[cfg(unix)]
     {
-        uv_fs::replace_symlink(&base_python, &executable)?;
+        uv_fs::replace_symlink(&executable_target, &executable)?;
         uv_fs::replace_symlink(
             "python",
             scripts.join(format!("python{}", interpreter.python_major())),
@@ -184,91 +224,102 @@ pub(crate) fn create(
         }
     }
 
-    // No symlinking on Windows, at least not on a regular non-dev non-admin Windows install.
+    // On Windows, we use trampolines that point to an executable target. For standalone
+    // interpreters, this target path includes a minor version junction to enable
+    // transparent upgrades.
     if cfg!(windows) {
-        copy_launcher_windows(
-            WindowsExecutable::Python,
-            interpreter,
-            &base_python,
-            &scripts,
-            python_home,
-        )?;
-
-        if interpreter.markers().implementation_name() == "graalpy" {
-            copy_launcher_windows(
-                WindowsExecutable::GraalPy,
-                interpreter,
-                &base_python,
-                &scripts,
-                python_home,
-            )?;
-            copy_launcher_windows(
-                WindowsExecutable::PythonMajor,
-                interpreter,
-                &base_python,
-                &scripts,
-                python_home,
-            )?;
+        if interpreter.is_standalone() {
+            let target = scripts.join(WindowsExecutable::Python.exe(interpreter));
+            create_link_to_executable(target.as_path(), executable_target.clone())
+                .map_err(Error::Python)?;
+            let targetw = scripts.join(WindowsExecutable::Pythonw.exe(interpreter));
+            create_link_to_executable(targetw.as_path(), executable_target)
+                .map_err(Error::Python)?;
         } else {
             copy_launcher_windows(
-                WindowsExecutable::Pythonw,
+                WindowsExecutable::Python,
                 interpreter,
                 &base_python,
                 &scripts,
                 python_home,
             )?;
-        }
 
-        if interpreter.markers().implementation_name() == "pypy" {
-            copy_launcher_windows(
-                WindowsExecutable::PythonMajor,
-                interpreter,
-                &base_python,
-                &scripts,
-                python_home,
-            )?;
-            copy_launcher_windows(
-                WindowsExecutable::PythonMajorMinor,
-                interpreter,
-                &base_python,
-                &scripts,
-                python_home,
-            )?;
-            copy_launcher_windows(
-                WindowsExecutable::PyPy,
-                interpreter,
-                &base_python,
-                &scripts,
-                python_home,
-            )?;
-            copy_launcher_windows(
-                WindowsExecutable::PyPyMajor,
-                interpreter,
-                &base_python,
-                &scripts,
-                python_home,
-            )?;
-            copy_launcher_windows(
-                WindowsExecutable::PyPyMajorMinor,
-                interpreter,
-                &base_python,
-                &scripts,
-                python_home,
-            )?;
-            copy_launcher_windows(
-                WindowsExecutable::PyPyw,
-                interpreter,
-                &base_python,
-                &scripts,
-                python_home,
-            )?;
-            copy_launcher_windows(
-                WindowsExecutable::PyPyMajorMinorw,
-                interpreter,
-                &base_python,
-                &scripts,
-                python_home,
-            )?;
+            if interpreter.markers().implementation_name() == "graalpy" {
+                copy_launcher_windows(
+                    WindowsExecutable::GraalPy,
+                    interpreter,
+                    &base_python,
+                    &scripts,
+                    python_home,
+                )?;
+                copy_launcher_windows(
+                    WindowsExecutable::PythonMajor,
+                    interpreter,
+                    &base_python,
+                    &scripts,
+                    python_home,
+                )?;
+            } else {
+                copy_launcher_windows(
+                    WindowsExecutable::Pythonw,
+                    interpreter,
+                    &base_python,
+                    &scripts,
+                    python_home,
+                )?;
+            }
+
+            if interpreter.markers().implementation_name() == "pypy" {
+                copy_launcher_windows(
+                    WindowsExecutable::PythonMajor,
+                    interpreter,
+                    &base_python,
+                    &scripts,
+                    python_home,
+                )?;
+                copy_launcher_windows(
+                    WindowsExecutable::PythonMajorMinor,
+                    interpreter,
+                    &base_python,
+                    &scripts,
+                    python_home,
+                )?;
+                copy_launcher_windows(
+                    WindowsExecutable::PyPy,
+                    interpreter,
+                    &base_python,
+                    &scripts,
+                    python_home,
+                )?;
+                copy_launcher_windows(
+                    WindowsExecutable::PyPyMajor,
+                    interpreter,
+                    &base_python,
+                    &scripts,
+                    python_home,
+                )?;
+                copy_launcher_windows(
+                    WindowsExecutable::PyPyMajorMinor,
+                    interpreter,
+                    &base_python,
+                    &scripts,
+                    python_home,
+                )?;
+                copy_launcher_windows(
+                    WindowsExecutable::PyPyw,
+                    interpreter,
+                    &base_python,
+                    &scripts,
+                    python_home,
+                )?;
+                copy_launcher_windows(
+                    WindowsExecutable::PyPyMajorMinorw,
+                    interpreter,
+                    &base_python,
+                    &scripts,
+                    python_home,
+                )?;
+            }
         }
     }
 

crates/uv/tests/it/venv.rs
@@ -868,14 +868,14 @@ fn create_venv_unknown_python_patch() {
         "###
         );
     } else {
-        uv_snapshot!(&mut command, @r###"
+        uv_snapshot!(&mut command, @r"
         success: false
         exit_code: 1
         ----- stdout -----
 
         ----- stderr -----
           × No interpreter found for Python 3.12.100 in managed installations or search path
-        "###
+        "
         );
     }
 

crates/uv/src/commands/venv.rs
@@ -47,7 +47,7 @@ use super::project::default_dependency_groups;
 pub(crate) async fn venv(
     project_dir: &Path,
     path: Option<PathBuf>,
-    python_request: Option<&str>,
+    python_request: Option<PythonRequest>,
     install_mirrors: PythonInstallMirrors,
     python_preference: PythonPreference,
     python_downloads: PythonDownloads,
@@ -130,7 +130,7 @@ enum VenvError {
 async fn venv_impl(
     project_dir: &Path,
     path: Option<PathBuf>,
-    python_request: Option<&str>,
+    python_request: Option<PythonRequest>,
     install_mirrors: PythonInstallMirrors,
     link_mode: LinkMode,
     index_locations: &IndexLocations,
@@ -212,7 +212,7 @@ async fn venv_impl(
         python_request,
         requires_python,
     } = WorkspacePython::from_request(
-        python_request.map(PythonRequest::parse),
+        python_request,
         project.as_ref().map(VirtualProject::workspace),
         &groups,
         project_dir,
@@ -234,6 +234,7 @@ async fn venv_impl(
             install_mirrors.python_install_mirror.as_deref(),
             install_mirrors.pypy_install_mirror.as_deref(),
             install_mirrors.python_downloads_json_url.as_deref(),
+            preview,
         )
         .await
         .into_diagnostic()?;
@@ -276,6 +277,11 @@ async fn venv_impl(
     )
     .into_diagnostic()?;
 
+    let upgradeable = preview.is_enabled()
+        && python_request
+            .as_ref()
+            .is_none_or(|request| !request.includes_patch());
+
     // Create the virtual environment.
     let venv = uv_virtualenv::create_venv(
         &path,
@@ -285,6 +291,8 @@ async fn venv_impl(
         allow_existing,
         relocatable,
         seed,
+        upgradeable,
+        preview,
     )
     .map_err(VenvError::Creation)?;
 

commit 177df19f303b6021754cd8629d7369c0bca9d76c

Add check for using minor version link when creating a venv on Windows (#14252)
There was a regression introduced in #13954 on Windows where creating a
venv behaved as if there was a minor version link even if none existed.
This PR adds a check to fix this.

Closes #14249.

crates/uv-virtualenv/src/virtualenv.rs
@@ -147,6 +147,7 @@ pub(crate) fn create(
     // Create a `.gitignore` file to ignore all files in the venv.
     fs::write(location.join(".gitignore"), "*")?;
 
+    let mut using_minor_version_link = false;
     let executable_target = if upgradeable && interpreter.is_standalone() {
         if let Some(minor_version_link) = PythonMinorVersionLink::from_executable(
             base_python.as_path(),
@@ -167,6 +168,7 @@ pub(crate) fn create(
                     &minor_version_link.symlink_directory.display(),
                     &base_python.display()
                 );
+                using_minor_version_link = true;
                 minor_version_link.symlink_executable.clone()
             }
         } else {
@@ -228,7 +230,7 @@ pub(crate) fn create(
     // interpreters, this target path includes a minor version junction to enable
     // transparent upgrades.
     if cfg!(windows) {
-        if interpreter.is_standalone() {
+        if using_minor_version_link {
             let target = scripts.join(WindowsExecutable::Python.exe(interpreter));
             create_link_to_executable(target.as_path(), executable_target.clone())
                 .map_err(Error::Python)?;

crates/uv/tests/it/venv.rs
@@ -516,7 +516,7 @@ fn create_venv_respects_group_requires_python() -> Result<()> {
         name = "foo"
         version = "1.0.0"
         dependencies = []
-        
+
         [dependency-groups]
         dev = ["sortedcontainers"]
         other = ["sniffio"]
@@ -549,7 +549,7 @@ fn create_venv_respects_group_requires_python() -> Result<()> {
         version = "1.0.0"
         requires-python = ">=3.11"
         dependencies = []
-        
+
         [dependency-groups]
         dev = ["sortedcontainers"]
         other = ["sniffio"]
@@ -582,7 +582,7 @@ fn create_venv_respects_group_requires_python() -> Result<()> {
         version = "1.0.0"
         requires-python = ">=3.10"
         dependencies = []
-        
+
         [dependency-groups]
         dev = ["sortedcontainers"]
         other = ["sniffio"]
@@ -612,7 +612,7 @@ fn create_venv_respects_group_requires_python() -> Result<()> {
         name = "foo"
         version = "1.0.0"
         dependencies = []
-        
+
         [dependency-groups]
         dev = ["sortedcontainers"]
 
@@ -643,7 +643,7 @@ fn create_venv_respects_group_requires_python() -> Result<()> {
         version = "1.0.0"
         requires-python = "<3.12"
         dependencies = []
-        
+
         [dependency-groups]
         dev = ["sortedcontainers"]
         other = ["sniffio"]
@@ -1050,6 +1050,39 @@ fn non_empty_dir_exists_allow_existing() -> Result<()> {
     );
 
     Ok(())
+}
+
+/// Run `uv venv` followed by `uv venv --allow-existing`.
+#[test]
+fn create_venv_then_allow_existing() {
+    let context = TestContext::new_with_versions(&["3.12"]);
+
+    // Create a venv
+    uv_snapshot!(context.filters(), context.venv(), @r"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Using CPython 3.12.[X] interpreter at: [PYTHON-3.12]
+    Creating virtual environment at: .venv
+    Activate with: source .venv/[BIN]/activate
+    "
+    );
+
+    // Create a venv again with `--allow-existing`
+    uv_snapshot!(context.filters(), context.venv()
+        .arg("--allow-existing"), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Using CPython 3.12.[X] interpreter at: [PYTHON-3.12]
+    Creating virtual environment at: .venv
+    Activate with: source .venv/[BIN]/activate
+    "###
+    );
 }
 
 #[test]