bddaba1659e71ae8aa5861c07801d8c663cb0167
Charlie Marsh <charlie.r.marsh@gmail.com>
2025-06-29 09:26:49 -0400

    Move validation

--- crates/uv-distribution-types/src/index_url.rs
+++ crates/uv-distribution-types/src/index_url.rs
@@ -44,16 +44,9 @@ impl IndexUrl {
         let url = match split_scheme(path) {
             Some((scheme, ..)) => {
                 match Scheme::parse(scheme) {
-                    Some(scheme) => {
-                        if scheme.is_file() {
-                            // Ex) `file:///path/to/something/`
-                            VerbatimUrl::parse_url(path)?
-                        } else {
-                            // Ex) `https://pypi.org/simple/`
-                            // Remove a trailing slash if it exists.
-                            let normalized_path = path.strip_suffix('/').unwrap_or(path);
-                            VerbatimUrl::parse_url(normalized_path)?
-                        }
+                    Some(_) => {
+                        // Ex) `https://pypi.org/simple`
+                        VerbatimUrl::parse_url(path)?
                     }
                     None => {
                         // Ex) `C:\Users\user\index`
@@ -269,7 +262,8 @@ impl From<VerbatimUrl> for IndexUrl {
         if url.scheme() == "file" {
             Self::Path(Arc::new(url))
         } else {
-            // Remove trailing slashes for consistency.
+            // Remove trailing slashes for consistency. They'll be re-added if necessary when
+            // querying the Simple API.
             if let Ok(mut path_segments) = url.raw_mut().path_segments_mut() {
                 path_segments.pop_if_empty();
             }
