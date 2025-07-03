0fd8580e20cf9d6e25fe9cdd79eb3fc6a26e576b
Charlie Marsh <charlie.r.marsh@gmail.com>
2025-07-03 16:22:43 -0400

    Revert "Includes `sys.prefix` in cached environment keys to avoid `--with` collisions across projects (#14403)"
    This reverts commit ec54dce9191c8cde770572afde78ab4ce4001f6a.

--- crates/uv/src/commands/project/environment.rs
+++ crates/uv/src/commands/project/environment.rs
@@ -107,15 +107,13 @@ impl CachedEnvironment {
         printer: Printer,
         preview: PreviewMode,
     ) -> Result<Self, ProjectError> {
-        // Resolve the "base" interpreter, which resolves to an underlying parent interpreter if the
-        // given interpreter is a virtual environment.
-        let base_interpreter = Self::base_interpreter(interpreter, cache)?;
+        let interpreter = Self::base_interpreter(interpreter, cache)?;
 
         // Resolve the requirements with the interpreter.
         let resolution = Resolution::from(
             resolve_environment(
                 spec,
-                &base_interpreter,
+                &interpreter,
                 build_constraints.clone(),
                 &settings.resolver,
                 network_settings,
@@ -143,29 +141,20 @@ impl CachedEnvironment {
         // Use the canonicalized base interpreter path since that's the interpreter we performed the
         // resolution with and the interpreter the environment will be created with.
         //
-        // We also include the canonicalized `sys.prefix` of the non-base interpreter, that is, the
-        // virtual environment's path. Originally, we shared cached environments independent of the
-        // environment they'd be layered on top of. However, this causes collisions as the overlay
-        // `.pth` file can be overridden by another instance of uv. Including this element in the key
-        // avoids this problem at the cost of creating separate cached environments for identical
-        // `--with` invocations across projects. We use `sys.prefix` rather than `sys.executable` so
-        // we can canonicalize it without invalidating the purpose of the element — it'd probably be
-        // safe to just use the absolute `sys.executable` as well.
-        //
-        // TODO(zanieb): Since we're not sharing these environmments across projects, we should move
-        // [`CachedEvnvironment::set_overlay`] etc. here since the values there should be constant
-        // now.
+        // We cache environments independent of the environment they'd be layered on top of. The
+        // assumption is such that the environment will _not_ be modified by the user or uv;
+        // otherwise, we risk cache poisoning. For example, if we were to write a `.pth` file to
+        // the cached environment, it would be shared across all projects that use the same
+        // interpreter and the same cached dependencies.
         //
         // TODO(zanieb): We should include the version of the base interpreter in the hash, so if
         // the interpreter at the canonicalized path changes versions we construct a new
         // environment.
-        let environment_hash = cache_digest(&(
-            &canonicalize_executable(base_interpreter.sys_executable())?,
-            &interpreter.sys_prefix().canonicalize()?,
-        ));
+        let interpreter_hash =
+            cache_digest(&canonicalize_executable(interpreter.sys_executable())?);
 
         // Search in the content-addressed cache.
-        let cache_entry = cache.entry(CacheBucket::Environments, environment_hash, resolution_hash);
+        let cache_entry = cache.entry(CacheBucket::Environments, interpreter_hash, resolution_hash);
 
         if cache.refresh().is_none() {
             if let Ok(root) = cache.resolve_link(cache_entry.path()) {
@@ -179,7 +168,7 @@ impl CachedEnvironment {
         let temp_dir = cache.venv_dir()?;
         let venv = uv_virtualenv::create_venv(
             temp_dir.path(),
-            base_interpreter,
+            interpreter,
             uv_virtualenv::Prompt::None,
             false,
             false,
