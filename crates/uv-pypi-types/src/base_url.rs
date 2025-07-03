6e4e5aaf8d38d986960ab68af7d60d4d2740056e
konstin <konstin@mailbox.org>
2025-07-03 23:20:48 +0200

    Simplify relative URL handling
    I was trying to figure out what the correct relative-and-absolute URL handling function was and realized there are two and they are redundant.

--- crates/uv-pypi-types/src/base_url.rs
+++ crates/uv-pypi-types/src/base_url.rs
@@ -1,37 +1,6 @@
 use serde::{Deserialize, Serialize};
 use uv_redacted::DisplaySafeUrl;
 
-/// Join a relative URL to a base URL.
-pub fn base_url_join_relative(
-    base: &str,
-    relative: &str,
-) -> Result<DisplaySafeUrl, JoinRelativeError> {
-    let base_url = DisplaySafeUrl::parse(base).map_err(|err| JoinRelativeError::ParseError {
-        original: base.to_string(),
-        source: err,
-    })?;
-
-    base_url
-        .join(relative)
-        .map_err(|err| JoinRelativeError::ParseError {
-            original: format!("{base}/{relative}"),
-            source: err,
-        })
-}
-
-/// An error that occurs when `base_url_join_relative` fails.
-///
-/// The error message includes the URL (`base` or `maybe_relative`) passed to
-/// `base_url_join_relative` that provoked the error.
-#[derive(Clone, Debug, thiserror::Error)]
-pub enum JoinRelativeError {
-    #[error("Failed to parse URL: `{original}`")]
-    ParseError {
-        original: String,
-        source: url::ParseError,
-    },
-}
-
 #[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize)]
 pub struct BaseUrl(
     #[serde(
