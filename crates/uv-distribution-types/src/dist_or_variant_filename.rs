493305d31a36b082c8a0c6950d421b63fbe0dbd0
konstin <konstin@mailbox.org>
2025-07-03 15:46:32 +0200

    A variants.json support to flat indexes

--- crates/uv-distribution-types/src/dist_or_variant_filename.rs
+++ crates/uv-distribution-types/src/dist_or_variant_filename.rs
@@ -1,9 +1,32 @@
-use crate::VariantJson;
 use uv_distribution_filename::DistFilename;
+use uv_normalize::PackageName;
 
-/// On an index page, there can be wheels, source distributions and `variant.json` files.
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum DistOrVariantFilename {
+use crate::VariantsJson;
+
+/// On an index page, there can be wheels, source distributions and `variants.json` files.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum IndexEntryFilename {
     DistFilename(DistFilename),
-    VariantJson(VariantJson),
+    VariantJson(VariantsJson),
+}
+
+impl IndexEntryFilename {
+    pub fn name(&self) -> &PackageName {
+        match self {
+            Self::DistFilename(filename) => filename.name(),
+            Self::VariantJson(variant_json) => &variant_json.name,
+        }
+    }
+
+    /// Parse a filename as either a distribution filename or a `variants.json` filename.
+    #[allow(clippy::manual_map)]
+    pub fn try_from_normalized_filename(filename: &str) -> Option<Self> {
+        if let Some(dist_filename) = DistFilename::try_from_normalized_filename(filename) {
+            Some(Self::DistFilename(dist_filename))
+        } else if let Some(variant_json) = VariantsJson::try_from_normalized_filename(filename) {
+            Some(Self::VariantJson(variant_json))
+        } else {
+            None
+        }
+    }
 }
