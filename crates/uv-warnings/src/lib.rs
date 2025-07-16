262ac1e19626b3b62b6b8dd86434660a4c0008cb
Zanie Blue <contact@zanie.dev>
2025-07-16 13:06:34 -0500

    Add a GitHub Actions warning to the `uv venv` behavior change

--- crates/uv-warnings/src/lib.rs
+++ crates/uv-warnings/src/lib.rs
@@ -7,6 +7,8 @@ pub use anstream;
 #[doc(hidden)]
 pub use owo_colors;
 use rustc_hash::FxHashSet;
+#[doc(hidden)]
+pub use uv_static::EnvVars;
 
 /// Whether user-facing warnings are enabled.
 pub static ENABLED: AtomicBool = AtomicBool::new(false);
@@ -55,4 +57,47 @@ macro_rules! warn_user_once {
             }
         }
     }};
+}
+
+/// Create a warning for a visibility in a CI provider, if warnings are enabled. Only shows the
+/// warning once in a given uv invocation, with uniqueness determined by the content of the message.
+#[macro_export]
+macro_rules! warn_ci_once {
+    ($($arg:tt)*) => {{
+        use $crate::anstream::eprintln;
+        use $crate::owo_colors::OwoColorize;
+
+        if $crate::ENABLED.load(std::sync::atomic::Ordering::Relaxed) {
+            if let Ok(mut states) = $crate::WARNINGS.lock() {
+                if let Some(provider) = $crate::CIProvider::from_env() {
+                    let message = provider.format(&format!("{}", format_args!($($arg)*)));
+                    if states.insert(message.clone()) {
+                        eprintln!("{message}");
+                    }
+                }
+            }
+        }
+    }};
+}
+
+pub enum CIProvider {
+    GitHubActions,
+}
+
+impl CIProvider {
+    pub fn from_env() -> Option<Self> {
+        if std::env::var_os(EnvVars::GITHUB_ACTIONS).is_some() {
+            Some(Self::GitHubActions)
+        } else {
+            None
+        }
+    }
+
+    pub fn format(&self, message: &str) -> String {
+        match self {
+            Self::GitHubActions => {
+                format!("::warning ::{message}")
+            }
+        }
+    }
 }
