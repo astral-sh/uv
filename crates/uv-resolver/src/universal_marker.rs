6ca33c711520996323bd01d768e1a384c9e5e331
Andrew Gallant <andrew@astral.sh>
2025-07-23 13:56:43 -0400

    Fully support project level conflicts
    There was a fair bit of support for project level conflicts missing from
    `UniversalMarker`. I think that's because my initial PR pre-dated the
    beefing up of `UniversalMarker`.

--- crates/uv-resolver/src/universal_marker.rs
+++ crates/uv-resolver/src/universal_marker.rs
@@ -151,7 +151,7 @@ impl UniversalMarker {
         match *item.kind() {
             ConflictKind::Extra(ref extra) => self.assume_extra(item.package(), extra),
             ConflictKind::Group(ref group) => self.assume_group(item.package(), group),
-            ConflictKind::Project => {}
+            ConflictKind::Project => self.assume_project(item.package()),
         }
         self.pep508 = self.marker.without_extras();
     }
@@ -165,8 +165,34 @@ impl UniversalMarker {
         match *item.kind() {
             ConflictKind::Extra(ref extra) => self.assume_not_extra(item.package(), extra),
             ConflictKind::Group(ref group) => self.assume_not_group(item.package(), group),
-            ConflictKind::Project => {}
+            ConflictKind::Project => self.assume_not_project(item.package()),
         }
+        self.pep508 = self.marker.without_extras();
+    }
+
+    /// Assumes that the "production" dependencies for the given project are
+    /// activated.
+    ///
+    /// This may simplify the conflicting marker component of this universal
+    /// marker.
+    fn assume_project(&mut self, package: &PackageName) {
+        let extra = encode_project(package);
+        self.marker = self
+            .marker
+            .simplify_extras_with(|candidate| *candidate == extra);
+        self.pep508 = self.marker.without_extras();
+    }
+
+    /// Assumes that the "production" dependencies for the given project are
+    /// not activated.
+    ///
+    /// This may simplify the conflicting marker component of this universal
+    /// marker.
+    fn assume_not_project(&mut self, package: &PackageName) {
+        let extra = encode_project(package);
+        self.marker = self
+            .marker
+            .simplify_not_extras_with(|candidate| *candidate == extra);
         self.pep508 = self.marker.without_extras();
     }
 
@@ -174,7 +200,7 @@ impl UniversalMarker {
     ///
     /// This may simplify the conflicting marker component of this universal
     /// marker.
-    pub(crate) fn assume_extra(&mut self, package: &PackageName, extra: &ExtraName) {
+    fn assume_extra(&mut self, package: &PackageName, extra: &ExtraName) {
         let extra = encode_package_extra(package, extra);
         self.marker = self
             .marker
@@ -186,7 +212,7 @@ impl UniversalMarker {
     ///
     /// This may simplify the conflicting marker component of this universal
     /// marker.
-    pub(crate) fn assume_not_extra(&mut self, package: &PackageName, extra: &ExtraName) {
+    fn assume_not_extra(&mut self, package: &PackageName, extra: &ExtraName) {
         let extra = encode_package_extra(package, extra);
         self.marker = self
             .marker
@@ -198,7 +224,7 @@ impl UniversalMarker {
     ///
     /// This may simplify the conflicting marker component of this universal
     /// marker.
-    pub(crate) fn assume_group(&mut self, package: &PackageName, group: &GroupName) {
+    fn assume_group(&mut self, package: &PackageName, group: &GroupName) {
         let extra = encode_package_group(package, group);
         self.marker = self
             .marker
@@ -210,7 +236,7 @@ impl UniversalMarker {
     ///
     /// This may simplify the conflicting marker component of this universal
     /// marker.
-    pub(crate) fn assume_not_group(&mut self, package: &PackageName, group: &GroupName) {
+    fn assume_not_group(&mut self, package: &PackageName, group: &GroupName) {
         let extra = encode_package_group(package, group);
         self.marker = self
             .marker
@@ -254,6 +280,7 @@ impl UniversalMarker {
     pub(crate) fn evaluate<P, E, G>(
         self,
         env: &MarkerEnvironment,
+        projects: impl Iterator<Item = P>,
         extras: impl Iterator<Item = (P, E)>,
         groups: impl Iterator<Item = (P, G)>,
     ) -> bool
@@ -262,12 +289,18 @@ impl UniversalMarker {
         E: Borrow<ExtraName>,
         G: Borrow<GroupName>,
     {
+        let projects = projects.map(|package| encode_project(package.borrow()));
         let extras =
             extras.map(|(package, extra)| encode_package_extra(package.borrow(), extra.borrow()));
         let groups =
             groups.map(|(package, group)| encode_package_group(package.borrow(), group.borrow()));
-        self.marker
-            .evaluate(env, &extras.chain(groups).collect::<Vec<ExtraName>>())
+        self.marker.evaluate(
+            env,
+            &projects
+                .chain(extras)
+                .chain(groups)
+                .collect::<Vec<ExtraName>>(),
+        )
     }
 
     /// Returns the internal marker that combines both the PEP 508
@@ -355,8 +388,18 @@ impl ConflictMarker {
         match *item.kind() {
             ConflictKind::Extra(ref extra) => ConflictMarker::extra(item.package(), extra),
             ConflictKind::Group(ref group) => ConflictMarker::group(item.package(), group),
-            ConflictKind::Project => ConflictMarker::TRUE,
+            ConflictKind::Project => ConflictMarker::project(item.package()),
         }
+    }
+
+    /// Create a conflict marker that is true only when the production
+    /// dependencies for the given package are activated.
+    pub fn project(package: &PackageName) -> ConflictMarker {
+        let operator = uv_pep508::ExtraOperator::Equal;
+        let name = uv_pep508::MarkerValueExtra::Extra(encode_project(package));
+        let expr = uv_pep508::MarkerExpression::Extra { operator, name };
+        let marker = MarkerTree::expression(expr);
+        ConflictMarker { marker }
     }
 
     /// Create a conflict marker that is true only when the given extra for the
@@ -522,8 +565,17 @@ fn encode_package_group(package: &PackageName, group: &GroupName) -> ExtraName {
     ExtraName::from_owned(format!("group-{package_len}-{package}-{group}")).unwrap()
 }
 
+/// Encodes the given project package name into a valid `extra` value in a PEP
+/// 508 marker.
+fn encode_project(package: &PackageName) -> ExtraName {
+    // See `encode_package_extra`, the same considerations apply here.
+    let package_len = package.as_str().len();
+    ExtraName::from_owned(format!("project-{package_len}-{package}")).unwrap()
+}
+
 #[derive(Debug)]
 enum ParsedRawExtra<'a> {
+    Project { package: &'a str },
     Extra { package: &'a str, extra: &'a str },
     Group { package: &'a str, group: &'a str },
 }
@@ -540,13 +592,13 @@ impl<'a> ParsedRawExtra<'a> {
         let Some((kind, tail)) = raw.split_once('-') else {
             return Err(mkerr(
                 raw_extra,
-                "expected to find leading `extra-` or `group-`",
+                "expected to find leading `package`, `extra-` or `group-`",
             ));
         };
         let Some((len, tail)) = tail.split_once('-') else {
             return Err(mkerr(
                 raw_extra,
-                "expected to find `{number}-` after leading `extra-` or `group-`",
+                "expected to find `{number}-` after leading `package-`, `extra-` or `group-`",
             ));
         };
         let len = len.parse::<usize>().map_err(|_| {
@@ -564,22 +616,28 @@ impl<'a> ParsedRawExtra<'a> {
                 ),
             ));
         };
-        if !tail.starts_with('-') {
-            return Err(mkerr(
-                raw_extra,
-                format!("expected `-` after package name `{package}`"),
-            ));
-        }
-        let tail = &tail[1..];
         match kind {
-            "extra" => Ok(ParsedRawExtra::Extra {
-                package,
-                extra: tail,
-            }),
-            "group" => Ok(ParsedRawExtra::Group {
-                package,
-                group: tail,
-            }),
+            "project" => Ok(ParsedRawExtra::Project { package }),
+            "extra" | "group" => {
+                if !tail.starts_with('-') {
+                    return Err(mkerr(
+                        raw_extra,
+                        format!("expected `-` after package name `{package}`"),
+                    ));
+                }
+                let tail = &tail[1..];
+                if kind == "extra" {
+                    Ok(ParsedRawExtra::Extra {
+                        package,
+                        extra: tail,
+                    })
+                } else {
+                    Ok(ParsedRawExtra::Group {
+                        package,
+                        group: tail,
+                    })
+                }
+            }
             _ => Err(mkerr(
                 raw_extra,
                 format!("unrecognized kind `{kind}` (must be `extra` or `group`)"),
@@ -595,6 +653,7 @@ impl<'a> ParsedRawExtra<'a> {
             }
         })?;
         match *self {
+            ParsedRawExtra::Project { .. } => Ok(ConflictItem::from(package)),
             ParsedRawExtra::Extra { extra, .. } => {
                 let extra = ExtraName::from_str(extra).map_err(|name_error| {
                     ResolveError::InvalidValueInConflictMarker {
@@ -618,6 +677,7 @@ impl<'a> ParsedRawExtra<'a> {
 
     fn package(&self) -> &'a str {
         match *self {
+            ParsedRawExtra::Project { package, .. } => package,
             ParsedRawExtra::Extra { package, .. } => package,
             ParsedRawExtra::Group { package, .. } => package,
         }
--- crates/uv/tests/it/lock.rs
+++ crates/uv/tests/it/lock.rs
@@ -2704,7 +2704,7 @@ fn lock_dependency_non_existent_extra() -> Result<()> {
 /// This tests a "basic" case for specifying a group that conflicts with the
 /// project itself.
 #[test]
-fn lock_conflicting_project_basic() -> Result<()> {
+fn lock_conflicting_project_basic1() -> Result<()> {
     let context = TestContext::new("3.12");
 
     // First we test that resolving a group with a dependency that conflicts
@@ -2787,9 +2787,6 @@ fn lock_conflicting_project_basic() -> Result<()> {
         version = 1
         revision = 2
         requires-python = ">=3.12"
-        resolution-markers = [
-            "python_version < '0'",
-        ]
         conflicts = [[
             { package = "project", group = "foo" },
             { package = "project" },
@@ -2803,7 +2800,7 @@ fn lock_conflicting_project_basic() -> Result<()> {
         version = "0.1.0"
         source = { editable = "." }
         dependencies = [
-            { name = "sortedcontainers", version = "2.3.0", source = { registry = "https://pypi.org/simple" } },
+            { name = "sortedcontainers", version = "2.3.0", source = { registry = "https://pypi.org/simple" }, marker = "extra == 'project-7-project'" },
         ]
 
         [package.dev-dependencies]
@@ -2840,14 +2837,12 @@ fn lock_conflicting_project_basic() -> Result<()> {
 
     // Re-run with `--locked`.
     uv_snapshot!(context.filters(), context.lock().arg("--locked"), @r"
-    success: false
-    exit_code: 1
+    success: true
+    exit_code: 0
     ----- stdout -----
 
     ----- stderr -----
-    warning: Ignoring existing lockfile due to fork markers not covering the supported environments: `python_version < '0'` vs `python_full_version >= '3.12'`
     Resolved 3 packages in [TIME]
-    The lockfile at `uv.lock` needs to be updated, but `--locked` was provided. To update the lockfile, run `uv lock`.
     ");
 
     // Install from the lockfile.
@@ -2887,6 +2882,190 @@ fn lock_conflicting_project_basic() -> Result<()> {
      - sortedcontainers==2.3.0
      + sortedcontainers==2.4.0
     "###);
+
+    Ok(())
+}
+
+/// This tests another "basic" case for specifying a group that conflicts with
+/// the project itself.
+#[test]
+fn lock_conflicting_project_basic2() -> Result<()> {
+    let context = TestContext::new("3.12");
+
+    let pyproject_toml = context.temp_dir.child("pyproject.toml");
+    pyproject_toml.write_str(
+        r#"
+        [project]
+        name = "example"
+        version = "0.1.0"
+        description = "Add your description here"
+        readme = "README.md"
+        requires-python = ">=3.12"
+        dependencies = [
+            "anyio>=4.2.0",
+        ]
+
+        [dependency-groups]
+        foo = [
+            "anyio<4.2.0",
+        ]
+
+        [tool.uv]
+        conflicts = [
+          [
+            { group = "foo" },
+            { package = "example" },
+          ],
+        ]
+
+        [build-system]
+        requires = ["setuptools>=42"]
+        build-backend = "setuptools.build_meta"
+        "#,
+    )?;
+
+    uv_snapshot!(context.filters(), context.lock(), @r"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Resolved 5 packages in [TIME]
+    ");
+
+    let lock = context.read("uv.lock");
+
+    insta::with_settings!({
+        filters => context.filters(),
+    }, {
+        assert_snapshot!(
+            lock, @r#"
+        version = 1
+        revision = 2
+        requires-python = ">=3.12"
+        conflicts = [[
+            { package = "example", group = "foo" },
+            { package = "example" },
+        ]]
+
+        [options]
+        exclude-newer = "2024-03-25T00:00:00Z"
+
+        [[package]]
+        name = "anyio"
+        version = "4.1.0"
+        source = { registry = "https://pypi.org/simple" }
+        dependencies = [
+            { name = "idna" },
+            { name = "sniffio" },
+        ]
+        sdist = { url = "https://files.pythonhosted.org/packages/6e/57/075e07fb01ae2b740289ec9daec670f60c06f62d04b23a68077fd5d73fab/anyio-4.1.0.tar.gz", hash = "sha256:5a0bec7085176715be77df87fc66d6c9d70626bd752fcc85f57cdbee5b3760da", size = 155773, upload-time = "2023-11-22T23:23:54.066Z" }
+        wheels = [
+            { url = "https://files.pythonhosted.org/packages/85/4f/d010eca6914703d8e6be222165d02c3e708ed909cdb2b7af3743667f302e/anyio-4.1.0-py3-none-any.whl", hash = "sha256:56a415fbc462291813a94528a779597226619c8e78af7de0507333f700011e5f", size = 83924, upload-time = "2023-11-22T23:23:52.595Z" },
+        ]
+
+        [[package]]
+        name = "anyio"
+        version = "4.3.0"
+        source = { registry = "https://pypi.org/simple" }
+        dependencies = [
+            { name = "idna", marker = "extra == 'project-7-example'" },
+            { name = "sniffio", marker = "extra == 'project-7-example'" },
+        ]
+        sdist = { url = "https://files.pythonhosted.org/packages/db/4d/3970183622f0330d3c23d9b8a5f52e365e50381fd484d08e3285104333d3/anyio-4.3.0.tar.gz", hash = "sha256:f75253795a87df48568485fd18cdd2a3fa5c4f7c5be8e5e36637733fce06fed6", size = 159642, upload-time = "2024-02-19T08:36:28.641Z" }
+        wheels = [
+            { url = "https://files.pythonhosted.org/packages/14/fd/2f20c40b45e4fb4324834aea24bd4afdf1143390242c0b33774da0e2e34f/anyio-4.3.0-py3-none-any.whl", hash = "sha256:048e05d0f6caeed70d731f3db756d35dcc1f35747c8c403364a8332c630441b8", size = 85584, upload-time = "2024-02-19T08:36:26.842Z" },
+        ]
+
+        [[package]]
+        name = "example"
+        version = "0.1.0"
+        source = { editable = "." }
+        dependencies = [
+            { name = "anyio", version = "4.3.0", source = { registry = "https://pypi.org/simple" }, marker = "extra == 'project-7-example'" },
+        ]
+
+        [package.dev-dependencies]
+        foo = [
+            { name = "anyio", version = "4.1.0", source = { registry = "https://pypi.org/simple" } },
+        ]
+
+        [package.metadata]
+        requires-dist = [{ name = "anyio", specifier = ">=4.2.0" }]
+
+        [package.metadata.requires-dev]
+        foo = [{ name = "anyio", specifier = "<4.2.0" }]
+
+        [[package]]
+        name = "idna"
+        version = "3.6"
+        source = { registry = "https://pypi.org/simple" }
+        sdist = { url = "https://files.pythonhosted.org/packages/bf/3f/ea4b9117521a1e9c50344b909be7886dd00a519552724809bb1f486986c2/idna-3.6.tar.gz", hash = "sha256:9ecdbbd083b06798ae1e86adcbfe8ab1479cf864e4ee30fe4e46a003d12491ca", size = 175426, upload-time = "2023-11-25T15:40:54.902Z" }
+        wheels = [
+            { url = "https://files.pythonhosted.org/packages/c2/e7/a82b05cf63a603df6e68d59ae6a68bf5064484a0718ea5033660af4b54a9/idna-3.6-py3-none-any.whl", hash = "sha256:c05567e9c24a6b9faaa835c4821bad0590fbb9d5779e7caa6e1cc4978e7eb24f", size = 61567, upload-time = "2023-11-25T15:40:52.604Z" },
+        ]
+
+        [[package]]
+        name = "sniffio"
+        version = "1.3.1"
+        source = { registry = "https://pypi.org/simple" }
+        sdist = { url = "https://files.pythonhosted.org/packages/a2/87/a6771e1546d97e7e041b6ae58d80074f81b7d5121207425c964ddf5cfdbd/sniffio-1.3.1.tar.gz", hash = "sha256:f4324edc670a0f49750a81b895f35c3adb843cca46f0530f79fc1babb23789dc", size = 20372, upload-time = "2024-02-25T23:20:04.057Z" }
+        wheels = [
+            { url = "https://files.pythonhosted.org/packages/e9/44/75a9c9421471a6c4805dbf2356f7c181a29c1879239abab1ea2cc8f38b40/sniffio-1.3.1-py3-none-any.whl", hash = "sha256:2f6da418d1f1e0fddd844478f41680e794e6051915791a034ff65e5f100525a2", size = 10235, upload-time = "2024-02-25T23:20:01.196Z" },
+        ]
+        "#
+        );
+    });
+
+    // Re-run with `--locked`.
+    uv_snapshot!(context.filters(), context.lock().arg("--locked"), @r"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Resolved 5 packages in [TIME]
+    ");
+
+    // Install from the lockfile.
+    uv_snapshot!(context.filters(), context.sync().arg("--frozen"), @r"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Prepared 4 packages in [TIME]
+    Installed 4 packages in [TIME]
+     + anyio==4.3.0
+     + example==0.1.0 (from file://[TEMP_DIR]/)
+     + idna==3.6
+     + sniffio==1.3.1
+    ");
+    // Another install, but with the group enabled, which
+    // should fail because it conflicts with the project.
+    uv_snapshot!(context.filters(), context.sync().arg("--frozen").arg("--group=foo"), @r"
+    success: false
+    exit_code: 2
+    ----- stdout -----
+
+    ----- stderr -----
+    error: Group `foo` and the project are incompatible with the declared conflicts: {`example:foo`, the project}
+    ");
+    // Another install, but this time with `--only-group=foo`,
+    // which excludes the project and is thus okay.
+    uv_snapshot!(context.filters(), context.sync().arg("--frozen").arg("--only-group=foo"), @r"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Prepared 1 package in [TIME]
+    Uninstalled 2 packages in [TIME]
+    Installed 1 package in [TIME]
+     - anyio==4.3.0
+     + anyio==4.1.0
+     - example==0.1.0 (from file://[TEMP_DIR]/)
+    ");
 
     Ok(())
 }
