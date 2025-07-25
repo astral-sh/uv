f01e8d40ceaddfb8917e3a01f74afb85094c6fc1
konstin <konstin@mailbox.org>
2025-07-25 21:34:44 +0200

    Commit package for torch test script

--- test_torch.sh
+++ test_torch.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 cargo build
-uv=target/uv/debug
+uv="$(pwd)/target/debug/uv"
 unset VIRTUAL_ENV
 export RUST_LOG=uv_distribution_types=debug,uv_distribution::distribution_database=debug
 
 # Three cases: Sync (new/updated lock), Sync (fresh lock), Sync (noop)
-( cd debug && uv venv -p 3.13 -c -q && rm -f uv.lock && ${uv} sync )
-( cd debug && uv venv -p 3.13 -c -q && ${uv} sync )
-( cd debug && ${uv} sync )
+( cd scripts/packages/torch_user && ${uv} venv -p 3.13 -c -q && rm -f uv.lock && ${uv} sync )
+( cd scripts/packages/torch_user && ${uv} venv -p 3.13 -c -q && ${uv} sync )
+( cd scripts/packages/torch_user && ${uv} sync )
 
 export NV_VARIANT_PROVIDER_FORCE_CUDA_DRIVER_VERSION=12.8
 export NV_VARIANT_PROVIDER_FORCE_SM_ARCH=9.0
 
-( cd debug && uv venv -p 3.13 -c -q && rm -f uv.lock && ${uv} sync )
-( cd debug && uv venv -p 3.13 -c -q && ${uv} sync )
-( cd debug && ${uv} sync )
+( cd scripts/packages/torch_user && ${uv} venv -p 3.13 -c -q && rm -f uv.lock && ${uv} sync )
+( cd scripts/packages/torch_user && ${uv} venv -p 3.13 -c -q && ${uv} sync )
+( cd scripts/packages/torch_user && ${uv} sync )
 
 unset NV_VARIANT_PROVIDER_FORCE_CUDA_DRIVER_VERSION
 unset NV_VARIANT_PROVIDER_FORCE_SM_ARCH
 
-uv venv --clear -q -p 3.13 &&  echo "torch" | ${uv} pip compile - --index https://variants-index.wheelnext.dev --no-progress --no-annotate # --no-cache
+${uv} venv --clear -q -p 3.13 &&  echo "torch" | ${uv} pip compile - --index https://variants-index.wheelnext.dev --no-progress --no-annotate # --no-cache
 
 export NV_VARIANT_PROVIDER_FORCE_CUDA_DRIVER_VERSION=12.8
 export NV_VARIANT_PROVIDER_FORCE_SM_ARCH=9.0
-uv venv --clear -q -p 3.13 && echo "torch" | ${uv} pip compile - --index https://variants-index.wheelnext.dev --no-annotate # --no-cache
+${uv} venv --clear -q -p 3.13 && echo "torch" | ${uv} pip compile - --index https://variants-index.wheelnext.dev --no-annotate # --no-cache
