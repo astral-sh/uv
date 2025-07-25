9778b2445fe93e11e4bc0025a9abc4d3ff97d1a7
konstin <konstin@mailbox.org>
2025-07-25 21:28:34 +0200

    Support replacing the binary in test scripts

--- test_torch.sh
+++ test_torch.sh
@@ -3,30 +3,27 @@
 set -e
 
 cargo build
+uv=target/uv/debug
 unset VIRTUAL_ENV
 export RUST_LOG=uv_distribution_types=debug,uv_distribution::distribution_database=debug
 
 # Three cases: Sync (new/updated lock), Sync (fresh lock), Sync (noop)
-( cd debug && uv venv -p 3.13 -c -q && rm -f uv.lock && cargo run -q sync )
-( cd debug && uv venv -p 3.13 -c -q && cargo run -q sync )
-( cd debug && cargo run -q sync )
+( cd debug && uv venv -p 3.13 -c -q && rm -f uv.lock && ${uv} sync )
+( cd debug && uv venv -p 3.13 -c -q && ${uv} sync )
+( cd debug && ${uv} sync )
 
 export NV_VARIANT_PROVIDER_FORCE_CUDA_DRIVER_VERSION=12.8
 export NV_VARIANT_PROVIDER_FORCE_SM_ARCH=9.0
 
-( cd debug && uv venv -p 3.13 -c -q && rm -f uv.lock && cargo run -q sync )
-( cd debug && uv venv -p 3.13 -c -q && cargo run -q sync )
-( cd debug && cargo run -q sync )
+( cd debug && uv venv -p 3.13 -c -q && rm -f uv.lock && ${uv} sync )
+( cd debug && uv venv -p 3.13 -c -q && ${uv} sync )
+( cd debug && ${uv} sync )
 
 unset NV_VARIANT_PROVIDER_FORCE_CUDA_DRIVER_VERSION
 unset NV_VARIANT_PROVIDER_FORCE_SM_ARCH
 
-uv venv --clear -q -p 3.13 &&  echo "torch" | cargo run -- pip compile - --index https://variants-index.wheelnext.dev --no-progress --no-annotate # --no-cache
+uv venv --clear -q -p 3.13 &&  echo "torch" | ${uv} pip compile - --index https://variants-index.wheelnext.dev --no-progress --no-annotate # --no-cache
 
 export NV_VARIANT_PROVIDER_FORCE_CUDA_DRIVER_VERSION=12.8
 export NV_VARIANT_PROVIDER_FORCE_SM_ARCH=9.0
-uv venv --clear -q -p 3.13 && echo "torch" | cargo run -- pip compile - --index https://variants-index.wheelnext.dev --no-annotate # --no-cache
-
-# For user testing
-# cargo run pip install torch --index https://download.pytorch.org/whl/test/variant/ --no-cache
-
+uv venv --clear -q -p 3.13 && echo "torch" | ${uv} pip compile - --index https://variants-index.wheelnext.dev --no-annotate # --no-cache
