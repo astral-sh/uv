5004cbc72a01c27e749512f50ede9c63b0040bbd
Zanie Blue <contact@zanie.dev>
2025-07-30 08:46:40 -0500

    Remove test case for `cargo dev generate-all`
    This means that CI tests fail in a way that is redundant with the dedicated CI job which can obscure signal on whether actual tests are failing

--- crates/uv-dev/src/generate_options_reference.rs
+++ crates/uv-dev/src/generate_options_reference.rs
@@ -387,26 +387,3 @@ impl Visit for CollectOptionsVisitor {
         self.fields.push((name.to_owned(), field));
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use std::env;
-
-    use anyhow::Result;
-
-    use uv_static::EnvVars;
-
-    use crate::generate_all::Mode;
-
-    use super::{Args, main};
-
-    #[test]
-    fn test_generate_options_reference() -> Result<()> {
-        let mode = if env::var(EnvVars::UV_UPDATE_SCHEMA).as_deref() == Ok("1") {
-            Mode::Write
-        } else {
-            Mode::Check
-        };
-        main(&Args { mode })
-    }
-}
