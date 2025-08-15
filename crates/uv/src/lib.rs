80e3e96cc4eaf2a98acbe05bd7b3dc0ff3761272
Wade Roberts <wade.roberts@centralsquare.com>
2025-08-15 11:58:53 -0600

    arg was mixed up

--- crates/uv/src/lib.rs
+++ crates/uv/src/lib.rs
@@ -1316,8 +1316,8 @@ async fn run(mut cli: Cli) -> Result<ExitStatus> {
                 args.show_paths,
                 args.show_version_specifiers,
                 args.show_with,
-                args.show_python,
                 args.show_extras,
+                args.show_python,
                 &cache,
                 printer,
             )
