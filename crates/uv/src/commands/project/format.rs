d949171c2272493305359826b6a36853a8482750
Jorge Hermo <jorgehermogonzalez@gmail.com>
2025-09-05 19:26:36 +0200

    feat: address PR comments

--- crates/uv/src/commands/project/format.rs
+++ crates/uv/src/commands/project/format.rs
@@ -39,20 +39,21 @@ pub(crate) async fn format(
     }
 
     let workspace_cache = WorkspaceCache::default();
-    let project =
-        VirtualProject::discover(project_dir, &DiscoveryOptions::default(), &workspace_cache).await;
-    let working_dir = match project {
-        // If we found a project, we use the project root
-        Ok(proj) => proj.root().to_owned(),
-        // If there is a problem finding a project, we just use the provided directory
-        // e.g. unmanaged projects
-        Err(
-            WorkspaceError::MissingPyprojectToml
-            | WorkspaceError::MissingProject(_)
-            | WorkspaceError::NonWorkspace(_),
-        ) => project_dir.to_owned(),
-        Err(err) => return Err(err.into()),
-    };
+    let target_dir =
+        match VirtualProject::discover(project_dir, &DiscoveryOptions::default(), &workspace_cache)
+            .await
+        {
+            // If we found a project, we use the project root
+            Ok(proj) => proj.root().to_owned(),
+            // If there is a problem finding a project, we just use the provided directory
+            // e.g. unmanaged projects
+            Err(
+                WorkspaceError::MissingPyprojectToml
+                | WorkspaceError::MissingProject(_)
+                | WorkspaceError::NonWorkspace(_),
+            ) => project_dir.to_owned(),
+            Err(err) => return Err(err.into()),
+        };
 
     // Parse version if provided
     let version = version.as_deref().map(Version::from_str).transpose()?;
@@ -73,7 +74,7 @@ pub(crate) async fn format(
         .with_context(|| format!("Failed to install ruff {version}"))?;
 
     let mut command = Command::new(&ruff_path);
-    command.current_dir(working_dir);
+    command.current_dir(target_dir);
     command.arg("format");
 
     if check {
