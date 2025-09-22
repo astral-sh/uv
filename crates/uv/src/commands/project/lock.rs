3941ca648f03253b3b1ced6d0ed7bf89dccb9a5c
11happy <soni5happy@gmail.com>
2025-09-22 21:56:29 +0530

    refactor: correct force implementation
    Signed-off-by: 11happy <soni5happy@gmail.com>

--- crates/uv/src/commands/project/lock.rs
+++ crates/uv/src/commands/project/lock.rs
@@ -777,9 +777,7 @@ async fn do_lock(
     let database = DistributionDatabase::new(&client, &build_dispatch, concurrency.downloads);
 
     // If any of the resolution-determining settings changed, invalidate the lock.
-    let existing_lock = if force {
-        None
-    } else if let Some(existing_lock) = existing_lock {
+    let existing_lock = if let Some(existing_lock) = existing_lock {
         match ValidatedLock::validate(
             existing_lock,
             target.install_path(),
@@ -805,6 +803,7 @@ async fn do_lock(
             state.index(),
             &database,
             printer,
+            force,
         )
         .await
         {
@@ -1024,11 +1023,16 @@ impl ValidatedLock {
         index: &InMemoryIndex,
         database: &DistributionDatabase<'_, Context>,
         printer: Printer,
+        force: bool,
     ) -> Result<Self, ProjectError> {
         // Perform checks in a deliberate order, such that the most extreme conditions are tested
         // first (i.e., every check that returns `Self::Unusable`, followed by every check that
         // returns `Self::Versions`, followed by every check that returns `Self::Preferable`, and
         // finally `Self::Satisfies`).
+        if force {
+            return Ok(Self::Preferable(lock));
+        }
+        // Start with the most severe condition: a fundamental option changed between resolutions.
         if lock.resolution_mode() != options.resolution_mode {
             let _ = writeln!(
                 printer.stderr(),
