astral-sh/uv

commit fa1498396e25382d932191c6cd0cdab7d2487bc6

Treat missing `top_level.txt` as non-fatal (#6881)
## Summary

Closes https://github.com/astral-sh/uv/issues/6872.

crates/uv/src/commands/pip/operations.rs
@@ -463,7 +463,15 @@ pub(crate) async fn install(
                     install_wheel_rs::Error::MissingRecord(_),
                 )) => {
                     warn_user!(
-                        "Failed to uninstall package at {} due to missing RECORD file. Installation may result in an incomplete environment.",
+                        "Failed to uninstall package at {} due to missing `RECORD` file. Installation may result in an incomplete environment.",
+                        dist_info.path().user_display().cyan(),
+                    );
+                }
+                Err(uv_installer::UninstallError::Uninstall(
+                    install_wheel_rs::Error::MissingTopLevel(_),
+                )) => {
+                    warn_user!(
+                        "Failed to uninstall package at {} due to missing `top-level.txt` file. Installation may result in an incomplete environment.",
                         dist_info.path().user_display().cyan(),
                     );
                 }

crates/uv/tests/pip_install.rs
@@ -672,7 +672,7 @@ fn reinstall_incomplete() -> Result<()> {
     ----- stderr -----
     Resolved 3 packages in [TIME]
     Prepared 1 package in [TIME]
-    warning: Failed to uninstall package at [SITE_PACKAGES]/anyio-3.7.0.dist-info due to missing RECORD file. Installation may result in an incomplete environment.
+    warning: Failed to uninstall package at [SITE_PACKAGES]/anyio-3.7.0.dist-info due to missing `RECORD` file. Installation may result in an incomplete environment.
     Uninstalled 1 package in [TIME]
     Installed 1 package in [TIME]
      - anyio==3.7.0
@@ -6454,4 +6454,41 @@ fn stale_egg_info() -> Result<()> {
     );
 
     Ok(())
+}
+
+/// `suds-community` has an incorrect layout whereby the wheel includes `suds_community.egg-info` at
+/// the top-level. We're then under the impression that `suds` is installed twice, but when we go to
+/// uninstall the second "version", we can't find the `egg-info` directory.
+#[test]
+fn missing_top_level() {
+    let context = TestContext::new("3.12");
+
+    uv_snapshot!(context.filters(), context.pip_install()
+        .arg("suds-community==0.8.5"), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Resolved 1 package in [TIME]
+    Prepared 1 package in [TIME]
+    Installed 1 package in [TIME]
+     + suds-community==0.8.5
+    "###
+    );
+
+    uv_snapshot!(context.filters(), context.pip_install()
+        .arg("suds-community==0.8.5"), @r###"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    Resolved 1 package in [TIME]
+    warning: Failed to uninstall package at [SITE_PACKAGES]/suds_community.egg-info due to missing `top-level.txt` file. Installation may result in an incomplete environment.
+    Uninstalled 2 packages in [TIME]
+    Installed 1 package in [TIME]
+     ~ suds-community==0.8.5
+    "###
+    );
 }

commit 57f56467d96f29ecfee219978a8e218ec361f317

Fix typo in MissingTopLevel warning (#16351)
<!--
Thank you for contributing to uv! To help us out with reviewing, please
consider the following:

- Does this pull request include a summary of the change? (See below.)
- Does this pull request include a descriptive title?
- Does this pull request include references to any relevant issues?
-->

## Summary

<!-- What's the purpose of the change? What does it do, and why? -->
The warning shown when `egg-info` lacks `top_level.txt` incorrectly
warns about missing `top-level.txt`

https://github.com/astral-sh/uv/blob/ee2649feaa78b847ebcd44daebdce28c2e974842/crates/uv-install-wheel/src/uninstall.rs#L179-L188

https://github.com/astral-sh/uv/blob/ee2649feaa78b847ebcd44daebdce28c2e974842/crates/uv/src/commands/pip/operations.rs#L687-L693

The non fatal warning with incorrect filename was introduced in
https://github.com/astral-sh/uv/pull/6881 which changed previous fatal
error https://github.com/astral-sh/uv/issues/6872 with correct
`top_level.txt` output.

## Test Plan

<!-- How was it tested? -->
Updated unit test to reflect change in warning

crates/uv/src/commands/pip/operations.rs
@@ -688,7 +688,7 @@ async fn execute_plan(
                     uv_install_wheel::Error::MissingTopLevel(_),
                 )) => {
                     warn_user!(
-                        "Failed to uninstall package at {} due to missing `top-level.txt` file. Installation may result in an incomplete environment.",
+                        "Failed to uninstall package at {} due to missing `top_level.txt` file. Installation may result in an incomplete environment.",
                         dist_info.install_path().user_display().cyan(),
                     );
                 }

crates/uv/tests/it/pip_install.rs
@@ -9019,7 +9019,7 @@ fn missing_top_level() {
 
     ----- stderr -----
     Resolved 1 package in [TIME]
-    warning: Failed to uninstall package at [SITE_PACKAGES]/suds_community.egg-info due to missing `top-level.txt` file. Installation may result in an incomplete environment.
+    warning: Failed to uninstall package at [SITE_PACKAGES]/suds_community.egg-info due to missing `top_level.txt` file. Installation may result in an incomplete environment.
     Uninstalled 2 packages in [TIME]
     Installed 1 package in [TIME]
      ~ suds-community==0.8.5