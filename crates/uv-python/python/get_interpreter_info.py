b044f90ccd5660ea359ca208cf241c962b654754
konstin <konstin@mailbox.org>
2025-11-11 20:05:28 +0100

    Latest ruff

--- crates/uv-python/python/get_interpreter_info.py
+++ crates/uv-python/python/get_interpreter_info.py
@@ -665,7 +665,7 @@ def main() -> None:
         "debug_enabled": bool(sysconfig.get_config_var("Py_DEBUG")),
         # Determine if the interpreter is 32-bit or 64-bit.
         # https://github.com/python/cpython/blob/b228655c227b2ca298a8ffac44d14ce3d22f6faa/Lib/venv/__init__.py#L136
-        "pointer_size": "64" if sys.maxsize > 2 ** 32 else "32",
+        "pointer_size": "64" if sys.maxsize > 2**32 else "32",
     }
     print(json.dumps(interpreter_info))
 
