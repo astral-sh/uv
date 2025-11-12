90b93bb40aa235920b75e63afd3e19e918492296
Zsolt Dollenstein <zsol.zsol@gmail.com>
2025-11-12 11:25:44 +0000

    Stop pinning SHELL to bash on windows

--- crates/uv/tests/it/common/mod.rs
+++ crates/uv/tests/it/common/mod.rs
@@ -896,6 +896,12 @@ impl TestContext {
         ))
         .unwrap();
 
+        // Ensure the tests aren't sensitive to the running user's shell without forcing
+        // `bash` on Windows
+        if cfg!(not(windows)) {
+            command.env(EnvVars::SHELL, "bash");
+        }
+
         command
             // When running the tests in a venv, ignore that venv, otherwise we'll capture warnings.
             .env_remove(EnvVars::VIRTUAL_ENV)
@@ -906,8 +912,6 @@ impl TestContext {
             .env(EnvVars::COLUMNS, "100")
             .env(EnvVars::PATH, path)
             .env(EnvVars::HOME, self.home_dir.as_os_str())
-            // Ensure the tests aren't sensitive to the running user's shell
-            .env(EnvVars::SHELL, "bash")
             .env(EnvVars::APPDATA, self.home_dir.as_os_str())
             .env(EnvVars::USERPROFILE, self.home_dir.as_os_str())
             .env(
