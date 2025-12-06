d90e641adc8df05e08f5fbae00cc8e3025d63c11
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-12-06 14:49:05 +0000

    Vendor code from diskus

--- crates/uv/src/commands/cache_size.rs
+++ crates/uv/src/commands/cache_size.rs
@@ -1,8 +1,12 @@
+use std::collections::HashSet;
 use std::fmt::Write;
 use std::num::NonZero;
+use std::path::{Path, PathBuf};
+use std::thread;
 
 use anyhow::Result;
-use diskus::{FilesizeType, Walk};
+use crossbeam_channel as channel;
+use rayon::{self, prelude::*};
 
 use crate::commands::{ExitStatus, human_readable_bytes};
 use crate::printer::Printer;
@@ -42,10 +46,7 @@ pub(crate) fn cache_size(
 
     tracing::info!("Using {} threads to calculate cache size", num_threads);
 
-    let root_path_buf = &[cache.root().to_path_buf()];
-
-    let walk = Walk::new(root_path_buf, num_threads, FilesizeType::ApparentSize);
-    let (total_bytes, _) = walk.run();
+    let total_bytes = calculate_cache_size(num_threads, cache.root());
 
     if human_readable {
         let (bytes, unit) = human_readable_bytes(total_bytes);
@@ -55,4 +56,88 @@ pub(crate) fn cache_size(
     }
 
     Ok(ExitStatus::Success)
+}
+
+/// Calculate the total size of the cache using multiple threads.
+///
+/// Vendored from <https://github.com/sharkdp/diskus>.
+fn calculate_cache_size(num_threads: usize, path: &Path) -> u64 {
+    let (tx, rx) = channel::unbounded();
+
+    let receiver_thread = thread::spawn(move || {
+        let mut total = 0;
+        let mut ids = HashSet::new();
+        for (unique_id, size) in rx {
+            if let Some(unique_id) = unique_id {
+                if ids.insert(unique_id) {
+                    total += size;
+                }
+            } else {
+                total += size;
+            }
+        }
+        total
+    });
+
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(num_threads)
+        .build()
+        .unwrap();
+    pool.install(|| walk(tx, &[path.to_path_buf()]));
+
+    receiver_thread.join().unwrap()
+}
+
+fn walk(tx: channel::Sender<(Option<UniqueID>, u64)>, entries: &[PathBuf]) {
+    entries.into_par_iter().for_each_with(tx, |tx_ref, entry| {
+        if let Ok(metadata) = entry.symlink_metadata() {
+            let unique_id = generate_unique_id(&metadata);
+
+            let size = &metadata.len();
+
+            tx_ref.send((unique_id, *size)).unwrap();
+
+            if metadata.is_dir() {
+                let mut children = vec![];
+                if let Ok(child_entries) = fs_err::read_dir(entry) {
+                    for child_entry in child_entries.flatten() {
+                        children.push(child_entry.path());
+                    }
+                }
+
+                walk(tx_ref.clone(), &children[..]);
+            }
+        }
+    });
+}
+
+#[derive(Eq, PartialEq, Hash)]
+struct UniqueID {
+    device: u64,
+    inode: u64,
+}
+
+#[cfg(not(windows))]
+fn generate_unique_id(metadata: &std::fs::Metadata) -> Option<UniqueID> {
+    use std::os::unix::fs::MetadataExt;
+    // If the entry has more than one hard link, generate
+    // a unique ID consisting of device and inode in order
+    // not to count this entry twice.
+    if metadata.is_file() && metadata.nlink() > 1 {
+        Some(UniqueID {
+            device: metadata.dev(),
+            inode: metadata.ino(),
+        })
+    } else {
+        None
+    }
+}
+
+#[cfg(windows)]
+fn generate_unique_id(_metadata: &std::fs::Metadata) -> Option<UniqueID> {
+    // Windows-internal tools such as Powershell, Explorer or `dir` are not respecting hardlinks
+    // or junction points when determining the size of a directory. `diskus` does the same and
+    // counts such entries multiple times (on Unix systems, multiple hardlinks to a single file are
+    // counted just once).
+    None
 }
