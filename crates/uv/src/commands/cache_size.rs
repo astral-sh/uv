2ced2167b932fadb2b8a6673cabf5aadcd6e2b35
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-12-07 14:10:21 +0000

    Use diskus again

--- crates/uv/src/commands/cache_size.rs
+++ crates/uv/src/commands/cache_size.rs
@@ -1,12 +1,7 @@
-use std::collections::HashSet;
 use std::fmt::Write;
-use std::num::NonZero;
-use std::path::{Path, PathBuf};
-use std::thread;
 
 use anyhow::Result;
-use crossbeam_channel as channel;
-use rayon::{self, prelude::*};
+use diskus::DiskUsage;
 
 use crate::commands::{ExitStatus, human_readable_bytes};
 use crate::printer::Printer;
@@ -38,15 +33,14 @@ pub(crate) fn cache_size(
         return Ok(ExitStatus::Success);
     }
 
-    let num_threads = threads.unwrap_or_else(|| {
-        std::thread::available_parallelism()
-            .map(NonZero::get)
-            .unwrap_or(1)
-    });
+    let mut disk_usage = DiskUsage::new(vec![cache.root().to_path_buf()]);
 
-    tracing::info!("Using {} threads to calculate cache size", num_threads);
+    if let Some(n) = threads {
+        tracing::info!("Using {} threads to calculate cache size", n);
+        disk_usage = disk_usage.num_workers(n);
+    }
 
-    let total_bytes = calculate_cache_size(num_threads, cache.root());
+    let total_bytes = disk_usage.count_ignoring_errors();
 
     if human_readable {
         let (bytes, unit) = human_readable_bytes(total_bytes);
@@ -56,88 +50,4 @@ pub(crate) fn cache_size(
     }
 
     Ok(ExitStatus::Success)
-}
-
-/// Calculate the total size of the cache using multiple threads.
-///
-/// Vendored from <https://github.com/sharkdp/diskus>.
-fn calculate_cache_size(num_threads: usize, path: &Path) -> u64 {
-    let (tx, rx) = channel::unbounded();
-
-    let receiver_thread = thread::spawn(move || {
-        let mut total = 0;
-        let mut ids = HashSet::new();
-        for (unique_id, size) in rx {
-            if let Some(unique_id) = unique_id {
-                if ids.insert(unique_id) {
-                    total += size;
-                }
-            } else {
-                total += size;
-            }
-        }
-        total
-    });
-
-    let pool = rayon::ThreadPoolBuilder::new()
-        .num_threads(num_threads)
-        .build()
-        .unwrap();
-    pool.install(|| walk(tx, &[path.to_path_buf()]));
-
-    receiver_thread.join().unwrap()
-}
-
-fn walk(tx: channel::Sender<(Option<UniqueID>, u64)>, entries: &[PathBuf]) {
-    entries.into_par_iter().for_each_with(tx, |tx_ref, entry| {
-        if let Ok(metadata) = entry.symlink_metadata() {
-            let unique_id = generate_unique_id(&metadata);
-
-            let size = &metadata.len();
-
-            tx_ref.send((unique_id, *size)).unwrap();
-
-            if metadata.is_dir() {
-                let mut children = vec![];
-                if let Ok(child_entries) = fs_err::read_dir(entry) {
-                    for child_entry in child_entries.flatten() {
-                        children.push(child_entry.path());
-                    }
-                }
-
-                walk(tx_ref.clone(), &children[..]);
-            }
-        }
-    });
-}
-
-#[derive(Eq, PartialEq, Hash)]
-struct UniqueID {
-    device: u64,
-    inode: u64,
-}
-
-#[cfg(not(windows))]
-fn generate_unique_id(metadata: &std::fs::Metadata) -> Option<UniqueID> {
-    use std::os::unix::fs::MetadataExt;
-    // If the entry has more than one hard link, generate
-    // a unique ID consisting of device and inode in order
-    // not to count this entry twice.
-    if metadata.is_file() && metadata.nlink() > 1 {
-        Some(UniqueID {
-            device: metadata.dev(),
-            inode: metadata.ino(),
-        })
-    } else {
-        None
-    }
-}
-
-#[cfg(windows)]
-fn generate_unique_id(_metadata: &std::fs::Metadata) -> Option<UniqueID> {
-    // Windows-internal tools such as Powershell, Explorer or `dir` are not respecting hardlinks
-    // or junction points when determining the size of a directory. `diskus` does the same and
-    // counts such entries multiple times (on Unix systems, multiple hardlinks to a single file are
-    // counted just once).
-    None
 }
