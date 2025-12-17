6fa8204efeb2ebf246caba436f9a240fb8d61493
Charlie Marsh <charlie.r.marsh@gmail.com>
2025-12-17 11:01:59 -0500

    Avoid enforcing incorrect hash in mixed-hash settings (#17157)
    ## Summary
    
    Right now, when we return a `Dist` from a lockfile, we concatenate all
    hashes for all distributions for a given package. In the case of
    https://github.com/astral-sh/uv/issues/17143, I think that means we'll
    return the SHA256 from the sdist, plus the SHA512 from the wheel. If the
    wheel was previously installed (i.e., it's in the cache), and we
    computed the SHA256 at that point in time, then `Hashed::has_digests`
    would return `true` because we have _at least_ one SHA256. We now limit
    the hashes to the distribution that we expect to install.
    
    Closes https://github.com/astral-sh/uv/issues/17143.

--- crates/uv-resolver/src/lock/installable.rs
+++ crates/uv-resolver/src/lock/installable.rs
@@ -16,7 +16,7 @@ use uv_normalize::{ExtraName, GroupName, PackageName};
 use uv_platform_tags::Tags;
 use uv_pypi_types::ResolverMarkerEnvironment;
 
-use crate::lock::{LockErrorKind, Package, TagPolicy};
+use crate::lock::{HashedDist, LockErrorKind, Package, TagPolicy};
 use crate::{Lock, LockError};
 
 pub trait Installable<'lock> {
@@ -527,18 +527,14 @@ pub trait Installable<'lock> {
         marker_env: &ResolverMarkerEnvironment,
         build_options: &BuildOptions,
     ) -> Result<Node, LockError> {
-        let dist = package.to_dist(
-            self.install_path(),
-            TagPolicy::Required(tags),
-            build_options,
-            marker_env,
-        )?;
+        let tag_policy = TagPolicy::Required(tags);
+        let HashedDist { dist, hashes } =
+            package.to_dist(self.install_path(), tag_policy, build_options, marker_env)?;
         let version = package.version().cloned();
         let dist = ResolvedDist::Installable {
             dist: Arc::new(dist),
             version,
         };
-        let hashes = package.hashes();
         Ok(Node::Dist {
             dist,
             hashes,
@@ -553,7 +549,7 @@ pub trait Installable<'lock> {
         tags: &Tags,
         marker_env: &ResolverMarkerEnvironment,
     ) -> Result<Node, LockError> {
-        let dist = package.to_dist(
+        let HashedDist { dist, .. } = package.to_dist(
             self.install_path(),
             TagPolicy::Preferred(tags),
             &BuildOptions::default(),
