1fa655e3efbd5877d4f58d07d224cb27dfca2e74
ryanking13 <def6488@gmail.com>
2026-01-23 23:22:30 +0900

    Debug mount

--- crates/uv-python/src/interpreter.rs
+++ crates/uv-python/src/interpreter.rs
@@ -956,7 +956,7 @@ impl InterpreterInfo {
         // modifications, and then (2) adding the path containing our query script to the front of
         // `sys.path` so that we can import it.
         let script = format!(
-            r#"import sys; sys.path = ["{}"] + sys.path; print("DEBUG: sys.meta_path:", sys.meta_path, file=sys.stderr); print("DEBUG: sys.executable:", sys.executable, file=sys.stderr); print("DEBUG: interpreter path:", {}, file=sys.stderr); from python.get_interpreter_info import main; main()"#,
+            r#"import sys; import os; sys.path = ["{}"] + sys.path; print("DEBUG: sys.meta_path:", sys.meta_path, file=sys.stderr); print("DEBUG: sys.executable:", sys.executable, file=sys.stderr); print("DEBUG: interpreter path:", {}, file=sys.stderr); print("DEBUG: os.listdir('/'):", os.listdir('/'), file=sys.stderr); from python.get_interpreter_info import main; main()"#,
             tempdir.path().escape_for_python(),
             format!("'{}'", interpreter.display()).replace('\\', "\\\\")
         );
