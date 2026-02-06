692a598ece8fb036b06e65b638b9ba3e0528ea3b
Tomasz (Tom) Kramkowski <tom@astral.sh>
2026-02-06 13:00:44 +0000

    Add some missing doc comments

--- crates/uv-settings/src/settings.rs
+++ crates/uv-settings/src/settings.rs
@@ -664,6 +664,7 @@ impl ResolverInstallerSchema {
         })
     }
 
+    /// Return the priority ordered list of indexes
     pub fn indexes(&self) -> impl Iterator<Item = Index> {
         self.index
             .iter()
