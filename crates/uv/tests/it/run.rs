b270921c189362b3617bb2831414108aa24e55f5
Anders Kaseorg <andersk@mit.edu>
2026-02-06 16:05:23 -0800

    Improve run_target_workspace_discovery test to catch reading cwd
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- crates/uv/tests/it/run.rs
+++ crates/uv/tests/it/run.rs
@@ -6494,6 +6494,8 @@ fn run_target_workspace_discovery() -> Result<()> {
         [build-system]
         requires = ["uv_build>=0.7,<10000"]
         build-backend = "uv_build"
+
+        [tool.uv.workspace]
         "#
     })?;
     workspace
@@ -6522,6 +6524,11 @@ fn run_target_workspace_discovery() -> Result<()> {
         import iniconfig
     ModuleNotFoundError: No module named 'iniconfig'
     "#);
+
+    // Write invalid configuration files to the cwd to verify that the
+    // target-workspace-discovery feature skips parsing them.
+    context.temp_dir.child("uv.toml").write_str("bad")?;
+    context.temp_dir.child("pyproject.toml").write_str("bad")?;
 
     // With the preview feature, the workspace is discovered from the target's directory.
     uv_snapshot!(context.filters(), context.run().arg("--preview-features").arg("target-workspace-discovery").arg("project/script.py").env_remove(EnvVars::VIRTUAL_ENV), @"
