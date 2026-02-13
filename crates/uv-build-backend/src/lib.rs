bb8ddcfe6d55a70614230df62f8092adc5255e74
Tomasz (Tom) Kramkowski <tom@astral.sh>
2026-02-13 16:38:02 +0000

    Revert "Update Rust crate flate2 to v1.1.9 (#17686)"
    This reverts commit b1f647f3e97b2821804a3a4813d26af92a616e46.
    
    Looks like it's causing some odd failure in CI?
    
    https://github.com/astral-sh/uv/actions/runs/21994267967/job/63549829432?pr=18012

--- crates/uv-build-backend/src/lib.rs
+++ crates/uv-build-backend/src/lib.rs
@@ -670,7 +670,7 @@ mod tests {
         // Check that the source dist is reproducible across platforms.
         assert_snapshot!(
             format!("{:x}", sha2::Sha256::digest(fs_err::read(&source_dist_path).unwrap())),
-            @"8bed1f7a8059064bcbeedb61a867cca7f63a474306011d0114280de631ac705e"
+            @"bb74bff575b135bb39e5c9bce56349441fb0923bb8857e32a5eaf34ec1843967"
         );
         // Check both the files we report and the actual files
         assert_snapshot!(format_file_list(build.source_dist_list_files, src.path()), @"
