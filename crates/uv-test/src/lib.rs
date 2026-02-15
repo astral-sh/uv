839643aab9b8e4180328f64679988217ee6807e1
Tomasz Kramkowski <tomasz@kramkow.ski>
2026-02-15 00:32:00 +0000

    Set the ruff UV_EXCLUDE_NEWER date to today
    To avoid future ruff releases between now and next month from causing
    the tests to break again.

--- crates/uv-test/src/lib.rs
+++ crates/uv-test/src/lib.rs
@@ -1288,7 +1288,7 @@ impl TestContext {
         command.arg("format");
         self.add_shared_options(&mut command, false);
         // Override to a more recent date for ruff version resolution
-        command.env(EnvVars::UV_EXCLUDE_NEWER, "2026-03-01T00:00:00Z");
+        command.env(EnvVars::UV_EXCLUDE_NEWER, "2026-02-15T00:00:00Z");
         command
     }
 
