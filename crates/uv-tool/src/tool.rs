7ba19eae360d4881c9c6d5414059c0544c0d94d2
Brad Cowie <brad@cowie.nz>
2026-02-15 17:58:07 +1300

    Make access to system site packages directory configureable for tools
    Add --system-site-packages cli arg for uv tool install which is used
    when creating the tool venv. Store this value in the tool receipt so
    that when the tool is upgraded the same value can be used.

--- crates/uv-tool/src/tool.rs
+++ crates/uv-tool/src/tool.rs
@@ -25,6 +25,8 @@ pub struct Tool {
     excludes: Vec<PackageName>,
     /// The build constraints requested by the user during installation.
     build_constraints: Vec<Requirement>,
+    /// The system site packages access requested by the user during installation.
+    system_site_packages: bool,
     /// The Python requested by the user during installation.
     python: Option<PythonRequest>,
     /// A mapping of entry point names to their metadata.
@@ -46,6 +48,8 @@ struct ToolWire {
     excludes: Vec<PackageName>,
     #[serde(default)]
     build_constraint_dependencies: Vec<Requirement>,
+    #[serde(default)]
+    system_site_packages: bool,
     python: Option<PythonRequest>,
     entrypoints: Vec<ToolEntrypoint>,
     #[serde(default)]
@@ -74,6 +78,7 @@ impl From<Tool> for ToolWire {
             overrides: tool.overrides,
             excludes: tool.excludes,
             build_constraint_dependencies: tool.build_constraints,
+            system_site_packages: tool.system_site_packages,
             python: tool.python,
             entrypoints: tool.entrypoints,
             options: tool.options,
@@ -98,6 +103,7 @@ impl TryFrom<ToolWire> for Tool {
             overrides: tool.overrides,
             excludes: tool.excludes,
             build_constraints: tool.build_constraint_dependencies,
+            system_site_packages: tool.system_site_packages,
             python: tool.python,
             entrypoints: tool.entrypoints,
             options: tool.options,
@@ -174,6 +180,7 @@ impl Tool {
         overrides: Vec<Requirement>,
         excludes: Vec<PackageName>,
         build_constraints: Vec<Requirement>,
+        system_site_packages: bool,
         python: Option<PythonRequest>,
         entrypoints: impl IntoIterator<Item = ToolEntrypoint>,
         options: ToolOptions,
@@ -186,6 +193,7 @@ impl Tool {
             overrides,
             excludes,
             build_constraints,
+            system_site_packages,
             python,
             entrypoints,
             options,
@@ -312,6 +320,17 @@ impl Tool {
             });
         }
 
+        if self.system_site_packages {
+            let system_site_packages = self.system_site_packages;
+            table.insert(
+                "system-site-packages",
+                value(serde::Serialize::serialize(
+                    &system_site_packages,
+                    toml_edit::ser::ValueSerializer::new(),
+                )?),
+            );
+        }
+
         if let Some(ref python) = self.python {
             table.insert(
                 "python",
@@ -368,6 +387,10 @@ impl Tool {
 
     pub fn build_constraints(&self) -> &[Requirement] {
         &self.build_constraints
+    }
+
+    pub fn system_site_packages(&self) -> &bool {
+        &self.system_site_packages
     }
 
     pub fn python(&self) -> &Option<PythonRequest> {
