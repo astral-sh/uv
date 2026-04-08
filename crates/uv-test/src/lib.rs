323d46298fbc12575f0e5cf095fe9418409e4f8f
zanieb <2586601+zanieb@users.noreply.github.com>
2026-04-08 12:34:33 +0000

    Sync latest Python releases

--- crates/uv-test/src/lib.rs
+++ crates/uv-test/src/lib.rs
@@ -40,9 +40,9 @@ pub const PACKSE_VERSION: &str = "0.3.59";
 pub const DEFAULT_PYTHON_VERSION: &str = "3.12";
 
 // The expected latest patch version for each Python minor version.
-pub const LATEST_PYTHON_3_15: &str = "3.15.0a7";
-pub const LATEST_PYTHON_3_14: &str = "3.14.3";
-pub const LATEST_PYTHON_3_13: &str = "3.13.12";
+pub const LATEST_PYTHON_3_15: &str = "3.15.0a8";
+pub const LATEST_PYTHON_3_14: &str = "3.14.4";
+pub const LATEST_PYTHON_3_13: &str = "3.13.13";
 pub const LATEST_PYTHON_3_12: &str = "3.12.13";
 pub const LATEST_PYTHON_3_11: &str = "3.11.15";
 pub const LATEST_PYTHON_3_10: &str = "3.10.20";
