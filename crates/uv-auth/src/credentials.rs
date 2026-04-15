1f0240353b4be3184fa210708eb297e542d2a978
Zanie Blue <contact@zanie.dev>
2026-04-15 10:06:00 -0500

    Add locking and legacy migration

--- crates/uv-auth/src/credentials.rs
+++ crates/uv-auth/src/credentials.rs
@@ -165,7 +165,7 @@ impl Credentials {
         }
     }
 
-    fn to_username(&self) -> Username {
+    pub(crate) fn to_username(&self) -> Username {
         match self {
             Self::Basic { username, .. } => username.clone(),
             Self::Bearer { .. } => Username::none(),
