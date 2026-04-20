5d8dd436cacc873096e6f03ed449b86520d99199
apaz-cli <aarpazdera@gmail.com>
2026-04-20 05:43:53 +0000

    Use fs_err over std::fs to appease clippy

--- crates/uv-distribution/src/distribution_database.rs
+++ crates/uv-distribution/src/distribution_database.rs
@@ -946,7 +946,7 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
                 let mut hasher = uv_extract::hash::HashReader::new(reader.compat(), &mut hashers);
 
                 // Open the partial entry (temp file), truncating if not resuming.
-                let temp_file = std::fs::OpenOptions::new()
+                let temp_file = fs_err::OpenOptions::new()
                     .create(true)
                     .write(true)
                     .append(resuming)
@@ -954,7 +954,7 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
                     .open(partial_entry.path())
                     .map_err(Error::CacheWrite)?;
                 let mut writer = tokio::io::BufWriter::new(fs_err::tokio::File::from_std(
-                    fs_err::File::from_parts(temp_file, partial_entry.path()),
+                    fs_err::File::from_parts(temp_file.into(), partial_entry.path()),
                 ));
 
                 match progress {
@@ -1053,7 +1053,7 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
         };
 
         let partial_len = || {
-            std::fs::metadata(partial_entry.path())
+            fs_err::metadata(partial_entry.path())
                 .map(|m| m.len())
                 .unwrap_or(0)
         };
