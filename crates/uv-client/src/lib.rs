3bbbf0f225dc7e96db03a6c77291592071f18c60
Charlie Marsh <charlie.r.marsh@gmail.com>
2026-05-18 12:03:09 +0100

    Hide uv-client linehaul internals

--- crates/uv-client/src/lib.rs
+++ crates/uv-client/src/lib.rs
@@ -7,7 +7,6 @@ pub use base_client::{
 pub use cached_client::{CacheControl, CachedClient, CachedClientError, DataWithCachePolicy};
 pub use error::{Error, ErrorKind, ProblemDetails, WrappedReqwestError};
 pub use flat_index::{FlatIndexClient, FlatIndexEntries, FlatIndexEntry, FlatIndexError};
-pub use linehaul::LineHaul;
 pub use registry_client::{
     Connectivity, MetadataFormat, RegistryClient, RegistryClientBuilder, SimpleDetailMetadata,
     SimpleDetailMetadatum, SimpleIndexMetadata, VersionFiles,
