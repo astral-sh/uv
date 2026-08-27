c4c2c6c217f0da54e07f89a18ded61c43216360a
Zanie Blue <contact@zanie.dev>
2026-08-27 08:50:36 -0500

    Use pip-specific build hash environment variable

--- crates/uv-cli/src/lib.rs
+++ crates/uv-cli/src/lib.rs
@@ -2005,7 +2005,7 @@ pub struct PipSyncArgs {
     pub hash_checking: HashCheckingArgs,
 
     /// Require hashes for all dependencies installed into isolated build environments.
-    #[arg(long, env = EnvVars::UV_REQUIRE_BUILD_HASHES, value_parser = clap::builder::BoolishValueParser::new())]
+    #[arg(long, env = EnvVars::UV_PIP_REQUIRE_BUILD_HASHES, value_parser = clap::builder::BoolishValueParser::new())]
     pub require_build_hashes: bool,
 
     /// The Python interpreter into which packages should be installed.
@@ -2350,7 +2350,7 @@ pub struct PipInstallArgs {
     pub hash_checking: HashCheckingArgs,
 
     /// Require hashes for all dependencies installed into isolated build environments.
-    #[arg(long, env = EnvVars::UV_REQUIRE_BUILD_HASHES, value_parser = clap::builder::BoolishValueParser::new())]
+    #[arg(long, env = EnvVars::UV_PIP_REQUIRE_BUILD_HASHES, value_parser = clap::builder::BoolishValueParser::new())]
     pub require_build_hashes: bool,
 
     /// The Python interpreter into which packages should be installed.
