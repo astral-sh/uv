1f076f2b249dc00cfd8bc91764d99e8a9ee2fece
Charlie Marsh <charlie.r.marsh@gmail.com>
2026-09-14 16:24:20 -0400

    Use libc for required environment constraints

--- crates/uv-resolver/src/lock/mod.rs
+++ crates/uv-resolver/src/lock/mod.rs
@@ -3478,8 +3478,7 @@ impl Lock {
                 ..environment
             })
             .filter(|environment| {
-                environment.minimum_libc_version.is_some()
-                    || environment.marker.contents().is_some()
+                environment.libc.is_some() || environment.marker.contents().is_some()
             })
             .collect()
     }
