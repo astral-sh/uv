f44d1e688e257e7c3c0dece5dc458829b5658cd6
konstin <konstin@mailbox.org>
2026-09-15 10:05:49 +0200

    Make flat index entry construction private

--- crates/uv-resolver/src/flat_index.rs
+++ crates/uv-resolver/src/flat_index.rs
@@ -47,7 +47,7 @@ impl FlatIndex {
 
     /// Collect all files from a `--find-links` target into a [`FlatIndex`].
     #[instrument(skip_all)]
-    pub(crate) fn from_entries(entries: FlatIndexEntries) -> Self {
+    fn from_entries(entries: FlatIndexEntries) -> Self {
         let mut index = FxHashMap::<PackageName, Vec<FlatIndexEntry>>::default();
         let (entries, offline) = entries.into_parts();
 
